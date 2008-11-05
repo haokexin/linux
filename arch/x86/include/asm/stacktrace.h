8e79b26e6ea5ef71082497824e11158de96f123e
Dave Lerner <Dave.Lerner@windriver.com>
2008-11-05 17:10:49 -0600

    oprofile backtrace across system calls 32/64 bit x86 kernels
    Adds systemcall backtracing, from kernel into user space.
    
    The system call assembler routines are modified when oprofile
    is configured into the kernel, such that a full, rather than partial,
    pt_regs structure is pushed onto the stack. This struct includes
    the frame pointer prior to the system call.  The new stack layout
    allows oprofile backtrace to get to the user space frame pointer.
    
    In dumpstack_32/64.c, dump_trace_extended() uses new stacktrace_ops
    callback that returns both pc and sp.  Oprofile x86_backtrace()
    implements the new callback to reconstruct the pt_regs, and get to
    the user frame pointer.
    
    Signed-off-by: Dave Lerner <dave.lerner@windriver.com>
    Signed-off-by: Zwane Mwaikambo <zwane.mwaikambo@windriver.com>
    Integrated-by: Liming Wang <liming.wang@windriver.com>

--- arch/x86/include/asm/stacktrace.h
+++ arch/x86/include/asm/stacktrace.h
@@ -12,19 +12,22 @@ typedef unsigned long (*walk_stack_t)(struct thread_info *tinfo,
 				      const struct stacktrace_ops *ops,
 				      void *data,
 				      unsigned long *end,
-				      int *graph);
+				      int *graph,
+				      int with_sp);
 
 extern unsigned long
 print_context_stack(struct thread_info *tinfo,
 		    unsigned long *stack, unsigned long bp,
 		    const struct stacktrace_ops *ops, void *data,
-		    unsigned long *end, int *graph);
+		    unsigned long *end, int *graph,
+		    int with_sp);
 
 extern unsigned long
 print_context_stack_bp(struct thread_info *tinfo,
 		       unsigned long *stack, unsigned long bp,
 		       const struct stacktrace_ops *ops, void *data,
-		       unsigned long *end, int *graph);
+		       unsigned long *end, int *graph,
+		       int with_sp);
 
 /* Generic stack tracer with callbacks */
 
@@ -33,6 +36,9 @@ struct stacktrace_ops {
 	/* msg must contain %s for the symbol */
 	void (*warning_symbol)(void *data, char *msg, unsigned long symbol);
 	void (*address)(void *data, unsigned long address, int reliable);
+	/* returns return address and sp for this address  */
+	void (*address_with_sp)(void *data, unsigned long address,
+		unsigned long sp);
 	/* On negative return stop dumping */
 	int (*stack)(void *data, char *name);
 	walk_stack_t	walk_stack;
@@ -41,5 +47,11 @@ struct stacktrace_ops {
 void dump_trace(struct task_struct *tsk, struct pt_regs *regs,
 		unsigned long *stack, unsigned long bp,
 		const struct stacktrace_ops *ops, void *data);
+
+/* extends the callbacks to include address_with_sp() */
+void dump_trace_extended(struct task_struct *tsk, struct pt_regs *regs,
+		unsigned long *stack, unsigned long bp,
+		const struct stacktrace_ops *ops, void *data,
+		unsigned int with_stack);
 
 #endif /* _ASM_X86_STACKTRACE_H */
--- arch/x86/oprofile/backtrace.c
+++ arch/x86/oprofile/backtrace.c
@@ -14,6 +14,25 @@
 #include <asm/ptrace.h>
 #include <asm/uaccess.h>
 #include <asm/stacktrace.h>
+#include <asm/proto.h>
+
+struct backtrace_data {
+	/* user input to enable kernel-syscall-userland tracing */
+	unsigned int trace_thru_syscall;
+
+	/* user frame pointer after return from the system call
+	 * frame adjusted by system_call_frame_test */
+	unsigned long bp;
+
+	/* handler to determine if unwind handler is system call frame */
+	void (*syscall_frame_test)(
+		void (*addr)(void),
+		unsigned long sp,
+		struct backtrace_data *backtrace_data);
+
+	/* backtrace depth, 0 implies no callstack tracing */
+	unsigned int depth;
+};
 
 static void backtrace_warning_symbol(void *data, char *msg,
 				     unsigned long symbol)
@@ -28,16 +47,27 @@ static void backtrace_warning(void *data, char *msg)
 
 static int backtrace_stack(void *data, char *name)
 {
-	/* Yes, we want all stacks */
-	return 0;
+	struct backtrace_data *bt_data = (struct backtrace_data *) data;
+	return bt_data->depth;
 }
 
 static void backtrace_address(void *data, unsigned long addr, int reliable)
 {
-	unsigned int *depth = data;
-
-	if ((*depth)--)
+	struct backtrace_data *bt_data = (struct backtrace_data *) data;
+	if (bt_data->depth--)
 		oprofile_add_trace(addr);
+}
+
+static void backtrace_address_with_sp(void *data, unsigned long addr,
+		unsigned long sp)
+{
+	struct backtrace_data *bt_data = (struct backtrace_data *) data;
+
+	/* check if return is from a system call */
+	bt_data->syscall_frame_test((void (*)(void)) addr, sp , bt_data);
+
+	/* log the address */
+	backtrace_address(data, addr, 1);
 }
 
 static struct stacktrace_ops backtrace_ops = {
@@ -46,6 +76,7 @@ static struct stacktrace_ops backtrace_ops = {
 	.stack		= backtrace_stack,
 	.address	= backtrace_address,
 	.walk_stack	= print_context_stack,
+	.address_with_sp = backtrace_address_with_sp,
 };
 
 struct frame_head {
@@ -53,12 +84,24 @@ struct frame_head {
 	unsigned long ret;
 } __attribute__((packed));
 
+/* for 32 bit addresses on a 64 bit kernel */
 struct frame_head32 {
 	unsigned int bp;
 	unsigned int ret;
 } __attribute__((packed));
 
-#if CONFIG_IA32_EMULATION
+
+#if defined(CONFIG_IA32_EMULATION)
+#define SYSCALL_FRAME_TEST (test_thread_flag(TIF_32BIT) ? \
+	in_ia32_syscall_frame : \
+	in_x86_64_syscall_frame);
+#elif defined(CONFIG_X86_64)
+#define SYSCALL_FRAME_TEST in_x86_64_syscall_frame;
+#else /* X86-32 only */
+#define SYSCALL_FRAME_TEST in_x86_32_syscall_frame;
+#endif
+
+#if defined(CONFIG_IA32_EMULATION)
 #define OP_FRAME_SIZE (test_thread_flag(TIF_32BIT) ? \
 	sizeof(struct frame_head32) : \
 	sizeof(struct frame_head))
@@ -74,6 +117,62 @@ struct frame_head32 {
 #define OP_FRAME_SIZE (sizeof(struct frame_head))
 #define GET_FRAME_RET(frame) (((struct frame_head *)frame)->ret)
 #define GET_FRAME_BP(frame) (((struct frame_head *)frame)->bp)
+#endif
+
+/*
+ * The system call routines have setup a pt_regs structure
+ * after the return address currently pointed to by sp.
+ */
+static inline void set_basepointer(unsigned long sp,
+	struct backtrace_data *bt_data)
+{
+	struct pt_regs *regs;
+	sp += sizeof(void *);
+	regs = (struct pt_regs *)sp;
+	bt_data->bp = frame_pointer(regs);
+}
+
+#if defined(CONFIG_OPROFILE_MODULE)
+/*
+ * This causes system call traversal to fail, but the only other choice
+ * for oprofile as module is to export some dangerous symbols
+ */
+void system_call_done(void) {}
+void tracesys_done(void) {}
+void ia32_syscall_done(void) {}
+void ia32_sysenter_done(void) {}
+void ia32_cstar_done(void) {}
+#endif
+
+#if defined(CONFIG_X86_64)
+static inline void in_x86_64_syscall_frame(
+	void (*addr)(void), unsigned long sp,
+	struct backtrace_data *bt_data)
+{
+	if ((addr == system_call_done) || (addr == tracesys_done))
+		set_basepointer(sp, bt_data);
+}
+#endif
+
+#if defined(CONFIG_IA32_EMULATION)
+static inline void in_ia32_syscall_frame(
+	void (*addr)(void), unsigned long sp,
+	struct backtrace_data *bt_data)
+{
+	if ((addr == ia32_syscall_done) || (addr == ia32_cstar_done)
+		|| (addr == ia32_sysenter_done))
+		set_basepointer(sp, bt_data);
+}
+#endif
+
+#if defined(CONFIG_X86_32)
+static inline void in_x86_32_syscall_frame(
+	void (*addr)(void), unsigned long sp,
+	struct backtrace_data *bt_data)
+{
+	if ((addr == system_call_done) || (addr == ia32_sysenter_done))
+		set_basepointer(sp, bt_data);
+}
 #endif
 
 static struct frame_head *dump_user_backtrace(struct frame_head *head)
@@ -101,13 +200,25 @@ void
 x86_backtrace(struct pt_regs * const regs, unsigned int depth)
 {
 	struct frame_head *head = (struct frame_head *)frame_pointer(regs);
+	int trace_syscall = oprofile_get_trace_thru_syscall();
+	struct backtrace_data bt_data = {
+		.depth = depth,
+		.bp = 0,
+	};
+
+	bt_data.syscall_frame_test = SYSCALL_FRAME_TEST;
 
 	if (!user_mode_vm(regs)) {
 		unsigned long stack = kernel_stack_pointer(regs);
-		if (depth)
-			dump_trace(NULL, regs, (unsigned long *)stack, 0,
-				   &backtrace_ops, &depth);
-		return;
+		unsigned long bp = frame_pointer(regs);
+		/* if trace-syscall set, trace thru syscall
+		 * using callbacks that include sp with pc */
+		dump_trace_extended(NULL, regs, (unsigned long *)stack, bp,
+			   &backtrace_ops, &bt_data, trace_syscall);
+		if (bt_data.bp && oprofile_syscall_trace_boundary())
+			head = (struct frame_head *)bt_data.bp;
+		else
+			return;
 	}
 
 	while (depth-- && head)
