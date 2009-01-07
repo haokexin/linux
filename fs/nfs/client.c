bdc5394b9c0dae3428ffb2f6dfd8bd600840b014
Jason Wessel <jason.wessel@windriver.com>
2009-01-07 00:59:33 -0500

    NFS: allow nfs root mount to use alternate rpc ports
    Allow an nfs root mount to use alternate RPC ports for mountd and nfsd.
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>
    [forward port to 2.6.33+]
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- fs/nfs/client.c
+++ fs/nfs/client.c
@@ -99,7 +99,7 @@ static const struct rpc_version *nfs_version[5] = {
 #endif
 };
 
-const struct rpc_program nfs_program = {
+struct rpc_program nfs_program = {
 	.name			= "nfs",
 	.number			= NFS_PROGRAM,
 	.nrvers			= ARRAY_SIZE(nfs_version),
@@ -136,6 +136,7 @@ struct nfs_client_initdata {
 	int proto;
 	u32 minorversion;
 	struct net *net;
+	int nfs_prog;
 };
 
 /*
@@ -154,6 +155,7 @@ static struct nfs_client *nfs_alloc_client(const struct nfs_client_initdata *cl_
 		goto error_0;
 
 	clp->rpc_ops = cl_init->rpc_ops;
+	clp->nfs_prog = cl_init->nfs_prog;
 
 	atomic_set(&clp->cl_count, 1);
 	clp->cl_cons_state = NFS_CS_INITING;
@@ -494,6 +496,9 @@ static struct nfs_client *nfs_match_client(const struct nfs_client_initdata *dat
 		/* Match nfsv4 minorversion */
 		if (clp->cl_minorversion != data->minorversion)
 			continue;
+		if (clp->nfs_prog != data->nfs_prog)
+			continue;
+
 		/* Match the full socket address */
 		if (!nfs_sockaddr_cmp(sap, clap))
 			continue;
@@ -678,6 +683,10 @@ static int nfs_create_rpc_client(struct nfs_client *clp,
 	if (!IS_ERR(clp->cl_rpcclient))
 		return 0;
 
+	if (clp->nfs_prog)
+		nfs_program.number = clp->nfs_prog;
+	else
+		nfs_program.number = NFS_PROGRAM;
 	clnt = rpc_create(&args);
 	if (IS_ERR(clnt)) {
 		dprintk("%s: cannot create RPC client. Error = %ld\n",
@@ -850,6 +859,7 @@ static int nfs_init_server(struct nfs_server *server,
 		.rpc_ops = &nfs_v2_clientops,
 		.proto = data->nfs_server.protocol,
 		.net = data->net,
+		.nfs_prog = data->nfs_prog,
 	};
 	struct rpc_timeout timeparms;
 	struct nfs_client *clp;
