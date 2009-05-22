504fc29af4130044151fc8fcce11a08302c39b55
Benjamin Walsh <benjamin.walsh@windriver.com>
2009-05-22 14:28:38 +0800

    powerpc: fix kexec bug when enable high mem support in FSL_BOOKE
    In FSL_BOOKE platform, relocate_new_kernel only map the 0~768M low
    memory when starting new kernel. So we should limit both the kexec
    source and destination page under 0x2fffffff.
    
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>
    Integrated-by: Benjamin Walsh <benjamin.walsh@windriver.com>

--- arch/powerpc/include/asm/kexec.h
+++ arch/powerpc/include/asm/kexec.h
@@ -6,13 +6,21 @@
  * Maximum page that is mapped directly into kernel memory.
  * XXX: Since we copy virt we can use any page we allocate
  */
+#ifdef CONFIG_FSL_BOOKE
+#define KEXEC_SOURCE_MEMORY_LIMIT 0x2fffffff
+#else
 #define KEXEC_SOURCE_MEMORY_LIMIT (-1UL)
+#endif
 
 /*
  * Maximum address we can reach in physical address mode.
  * XXX: I want to allow initrd in highmem. Otherwise set to rmo on LPAR.
  */
+#ifdef CONFIG_FSL_BOOKE
+#define KEXEC_DESTINATION_MEMORY_LIMIT 0x2fffffff
+#else
 #define KEXEC_DESTINATION_MEMORY_LIMIT (-1UL)
+#endif
 
 /* Maximum address we can use for the control code buffer */
 #ifdef __powerpc64__
