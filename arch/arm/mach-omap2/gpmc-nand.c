a04cf13df493a910d5ad41d7e7c2bc3c2d5123e3
Stanley.Miao <stanley.miao@windriver.com>
2010-03-18 15:45:57 +0800

    OMAP: fix a gpmc nand problem
    If gpmc_t isn't given, we don't need to set timing for gpmc, or it will cause
    a Oops.
    
    Signed-off-by: Stanley.Miao <stanley.miao@windriver.com>

--- arch/arm/mach-omap2/gpmc-nand.c
+++ arch/arm/mach-omap2/gpmc-nand.c
@@ -42,6 +42,9 @@ static int omap2_nand_gpmc_retime(void)
 	if (!gpmc_nand_data->gpmc_t)
 		return 0;
 
+	if (!gpmc_nand_data->gpmc_t)
+		return 0;
+
 	memset(&t, 0, sizeof(t));
 	t.sync_clk = gpmc_round_ns_to_ticks(gpmc_nand_data->gpmc_t->sync_clk);
 	t.cs_on = gpmc_round_ns_to_ticks(gpmc_nand_data->gpmc_t->cs_on);
