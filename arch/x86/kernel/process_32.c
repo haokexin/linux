dc4b12f5bcfe3467c75e4746e992cc9fb95efd7c
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2010-05-13 19:28:25 -0400

    idle-notifier-x86_32-fix-export-symbol
    idle notifier x86_32 fix export symbol
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- arch/x86/kernel/process_32.c
+++ arch/x86/kernel/process_32.c
@@ -74,6 +74,7 @@ void enter_idle(void)
 	trace_pm_idle_entry();
 	notify_idle(IDLE_START);
 }
+EXPORT_SYMBOL_GPL(enter_idle);
 
 void __exit_idle(void)
 {
@@ -82,6 +83,7 @@ void __exit_idle(void)
 	notify_idle(IDLE_END);
 	trace_pm_idle_exit();
 }
+EXPORT_SYMBOL_GPL(__exit_idle);
 
 /* Called from interrupts to signify idle end */
 void exit_idle(void)
@@ -91,6 +93,7 @@ void exit_idle(void)
 		return;
 	__exit_idle();
 }
+EXPORT_SYMBOL_GPL(exit_idle);
 
 /*
  * Return saved PC of a blocked thread.
--- arch/x86/kernel/process_64.c
+++ arch/x86/kernel/process_64.c
@@ -72,6 +72,7 @@ void enter_idle(void)
 	trace_pm_idle_entry();
 	notify_idle(IDLE_START);
 }
+EXPORT_SYMBOL_GPL(enter_idle);
 
 void __exit_idle(void)
 {
@@ -80,6 +81,7 @@ void __exit_idle(void)
 	notify_idle(IDLE_END);
 	trace_pm_idle_exit();
 }
+EXPORT_SYMBOL_GPL(__exit_idle);
 
 /* Called from interrupts to signify idle end */
 void exit_idle(void)
@@ -89,6 +91,7 @@ void exit_idle(void)
 		return;
 	__exit_idle();
 }
+EXPORT_SYMBOL_GPL(exit_idle);
 
 #ifndef CONFIG_SMP
 static inline void play_dead(void)
