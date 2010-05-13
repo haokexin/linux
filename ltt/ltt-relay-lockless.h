aa975b42811dda8bff0983d62c7cde02631c5e03
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2010-05-13 19:27:24 -0400

    lttng-relay-lockless-remove-wmb-from-fast-path
    LTTng relay lockless remove wmb from fast path
    
    We can use a remote wmb to speed up fast path by promoting a compiler barrier
    into a full smp_wmb() when the reader needs it.
    
    Should increase speed of architectures with out-of-order stores.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- ltt/ltt-relay-lockless.h
+++ ltt/ltt-relay-lockless.h
@@ -400,8 +400,16 @@ static __inline__ void ltt_commit_slot(
 	long endidx = SUBBUF_INDEX(offset_end - 1, rchan);
 	long commit_count;
 
-	/* Must write slot data before incrementing commit count */
+#ifdef LTT_NO_IPI_BARRIER
 	smp_wmb();
+#else
+	/*
+	 * Must write slot data before incrementing commit count.
+	 * This compiler barrier is upgraded into a smp_mb() by the IPI
+	 * sent by get_subbuf().
+	 */
+	barrier();
+#endif
 	local_add(slot_size, &ltt_buf->commit_count[endidx]);
 	/*
 	 * commit count read can race with concurrent OOO commit count updates.
