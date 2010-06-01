df17f7e45572bd948aa49e03b044b06f848ea4a4
Zumeng Chen <zumeng.chen@windriver.com>
2010-06-01 19:21:39 +0800

    kvm profiling x86 compiling error
    Add CONFIG_PROFILING definition to avoid compiling error.
    
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4370,7 +4370,11 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 	/*
 	 * Profile KVM exit RIPs:
 	 */
+#ifdef CONFIG_PROFILING
 	if (unlikely(imv_read(prof_on) == KVM_PROFILING)) {
+#else
+ 	if (unlikely(prof_on == KVM_PROFILING)) {
+#endif
 		unsigned long rip = kvm_rip_read(vcpu);
 		profile_hit(KVM_PROFILING, (void *)rip);
 	}
