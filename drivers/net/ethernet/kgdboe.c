9e8b4b79a4bf68e00b28ca78c155c6dd7d1b193b
Dongdong Deng <dongdong.deng@windriver.com>
2010-09-08 06:40:18 -0700

    kgdboe: check kgdboe whether initialized or not when cleanup kgdboe
    If the kgdboe didn't be initialized, the cleanup_kgdboe() should
    do nothing.
    
    Signed-off-by: Dongdong Deng <dongdong.deng@windriver.com>
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>

--- drivers/net/ethernet/kgdboe.c
+++ drivers/net/ethernet/kgdboe.c
@@ -212,9 +212,11 @@ static int init_kgdboe(void)
 
 static void cleanup_kgdboe(void)
 {
+	if (configured != 2)
+		return;
 	netpoll_cleanup(&np);
-	configured = 0;
 	kgdb_unregister_io_module(&local_kgdb_io_ops);
+	configured = 0;
 }
 
 static int param_set_kgdboe_var(const char *kmessage, struct kernel_param *kp)
