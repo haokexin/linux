dc854e83ce7b93ab6e87b4c0e13f1e5a0d1af54e
Wu Zhangjin <zhangjin.wu@windriver.com>
2010-09-20 15:24:55 +0800

    tracing/trace-clock: Add trace_clock_read64_ns()
    Thanks to trace_clock_cyc2ns(), this patch adds trace_clock_read64_ns()
    to allow the users to get the nanosecs directly.
    
    Signed-off-by: Wu Zhangjin <zhangjin.wu@windriver.com>

--- include/linux/trace-clock.h
+++ include/linux/trace-clock.h
@@ -14,4 +14,12 @@
 #else
 #include <asm-generic/trace-clock.h>
 #endif /* CONFIG_HAVE_TRACE_CLOCK */
+
+extern u64 notrace trace_clock_cyc2ns(u64 cycles);
+
+static inline u64 notrace trace_clock_read64_ns(void)
+{
+	return trace_clock_cyc2ns(trace_clock_read64());
+}
+
 #endif /* _LINUX_TRACE__CLOCK_H */
