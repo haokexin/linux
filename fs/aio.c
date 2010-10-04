574a877848c9070a69c0ebdc44fa738160e75fc7
Michel Thebeau <michel.thebeau@windriver.com>
2010-10-04 07:29:37 -0700

    FS Compat: Relocate IOV MAX check to precede access_ok
    Ensures that compat readv and writev system calls return EINVAL
    when vector length (vlen) is greater than IOV MAX (UIO_MAXIOV).
    This is consistent with the non-compat readv and writev system
    calls.  System calls affected include compat_readv and
    compat_writev.
    
    Previously, these two system calls would return EFAULT when the vlen
    passed to kernel was larger than the largest signed integer. (I.e.,
    cram a negative int into an unsigned long); because access_ok would
    fail to access such a large vector.  It was premature to check
    access_ok before checking the vector length.
    
    The check for UIO_MAXIOV is bubbled up out of
    compat_rw_copy_check_uvector function into compat_do_readv_writev
    so that it will precede the call to access_ok.
    aio_setup_vectored_rw in fs/aio.c also calls
    compat_rw_copy_check_uvector so move the IOV MAX check into there as
    well.
    
    In both cases listed above it is either redundant or unnecessary to
    check for vector lengths less than zero, so that part is dropped
    during the copy.
    
    Signed-off-by: Michel Thebeau <michel.thebeau@windriver.com>

--- fs/aio.c
+++ fs/aio.c
@@ -1401,12 +1401,16 @@ static ssize_t aio_setup_vectored_rw(int type, struct kiocb *kiocb, bool compat)
 	ssize_t ret;
 
 #ifdef CONFIG_COMPAT
-	if (compat)
+	if (compat) {
+		ret = -EINVAL;
+		if (kiocb->ki_nbytes > UIO_MAXIOV)
+			goto out;
+
 		ret = compat_rw_copy_check_uvector(type,
 				(struct compat_iovec __user *)kiocb->ki_buf,
 				kiocb->ki_nbytes, 1, &kiocb->ki_inline_vec,
 				&kiocb->ki_iovec);
-	else
+	} else
 #endif
 		ret = rw_copy_check_uvector(type,
 				(struct iovec __user *)kiocb->ki_buf,
