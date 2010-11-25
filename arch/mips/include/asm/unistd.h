0e8bf35aabe438a73f394d82d39b196cecc64a6a
Yang Shi <yang.shi@windriver.com>
2010-11-25 16:03:09 +0800

    WRHV: MIPS: Update system call numbers
    Added three new system calls for VB control, so update Linux
    system call number.
    
    Signed-off-by: Yang Shi <yang.shi@windriver.com>

--- arch/mips/include/asm/unistd.h
+++ arch/mips/include/asm/unistd.h
@@ -362,16 +362,25 @@
 #define __NR_sched_getparam_ex		(__NR_Linux + 339)
 #define __NR_sched_wait_interval	(__NR_Linux + 340)
 #define __NR_setns                      (__NR_Linux + 341)
+#define __NR_vbi_mem			(__NR_Linux + 342)
+#define __NR_vbi_activate_vb		(__NR_Linux + 343)
+#define __NR_vbi_control		(__NR_Linux + 344)
+
+#define VBI_MEM_READ			0x0010
+#define VBI_MEM_WRITE			0x0100
+#define SYS_VBI_VB_SUSPEND		0x10001
+#define SYS_VBI_VB_RESUME		0x20002
+#define SYS_VBI_VB_RESTART		0x40004
 
 /*
  * Offset of the last Linux o32 flavoured syscall
  */
-#define __NR_Linux_syscalls		341
+#define __NR_Linux_syscalls		344
 
 #endif /* _MIPS_SIM == _MIPS_SIM_ABI32 */
 
 #define __NR_O32_Linux			4000
-#define __NR_O32_Linux_syscalls		341
+#define __NR_O32_Linux_syscalls		344
 
 #if _MIPS_SIM == _MIPS_SIM_ABI64
 
@@ -680,16 +689,25 @@
 #define __NR_sched_getparam_ex		(__NR_Linux + 298)
 #define __NR_sched_wait_interval	(__NR_Linux + 299)
 #define __NR_setns                      (__NR_Linux + 300)
+#define __NR_vbi_mem			(__NR_Linux + 301)
+#define __NR_vbi_activate_vb		(__NR_Linux + 302)
+#define __NR_vbi_control		(__NR_Linux + 303)
+
+#define VBI_MEM_READ			0x0010
+#define VBI_MEM_WRITE			0x0100
+#define SYS_VBI_VB_SUSPEND		0x10001
+#define SYS_VBI_VB_RESUME		0x20002
+#define SYS_VBI_VB_RESTART		0x40004
 
 /*
  * Offset of the last Linux 64-bit flavoured syscall
  */
-#define __NR_Linux_syscalls		300
+#define __NR_Linux_syscalls		303
 
 #endif /* _MIPS_SIM == _MIPS_SIM_ABI64 */
 
 #define __NR_64_Linux			5000
-#define __NR_64_Linux_syscalls		300
+#define __NR_64_Linux_syscalls		303
 
 #if _MIPS_SIM == _MIPS_SIM_NABI32
 
@@ -1003,16 +1021,25 @@
 #define __NR_sched_getparam_ex		(__NR_Linux + 303)
 #define __NR_sched_wait_interval	(__NR_Linux + 304)
 #define __NR_setns                      (__NR_Linux + 305)
+#define __NR_vbi_mem			(__NR_Linux + 306)
+#define __NR_vbi_activate_vb		(__NR_Linux + 307)
+#define __NR_vbi_control		(__NR_Linux + 308)
+
+#define VBI_MEM_READ			0x0010
+#define VBI_MEM_WRITE			0x0100
+#define SYS_VBI_VB_SUSPEND		0x10001
+#define SYS_VBI_VB_RESUME		0x20002
+#define SYS_VBI_VB_RESTART		0x40004
 
 /*
  * Offset of the last N32 flavoured syscall
  */
-#define __NR_Linux_syscalls		305
+#define __NR_Linux_syscalls		308
 
 #endif /* _MIPS_SIM == _MIPS_SIM_NABI32 */
 
 #define __NR_N32_Linux			6000
-#define __NR_N32_Linux_syscalls		305
+#define __NR_N32_Linux_syscalls		308
 
 #ifdef __KERNEL__
 
