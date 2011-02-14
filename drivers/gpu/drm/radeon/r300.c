9f7216f7a8a63c7d15ca4bd5f2d4b1c070c42a64
Marek Olšák <maraeo@gmail.com>
2011-02-14 01:01:10 +0100

    drm/radeon/kms: check AA resolve registers on r300
    Commit fff1ce4dc6113b6fdc4e3a815ca5fd229408f8ef upstream.
    
    This is an important security fix because we allowed arbitrary values
    to be passed to AARESOLVE_OFFSET. This also puts the right buffer address
    in the register.
    Signed-off-by: Marek Olšák <maraeo@gmail.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Integrated-by: Gao Wei <wei.gao2@windriver.com>

--- drivers/gpu/drm/radeon/r300.c
+++ drivers/gpu/drm/radeon/r300.c
@@ -1051,6 +1051,27 @@ static int r300_packet0_check(struct radeon_cs_parser *p,
 		/* ZB_BW_CNTL */
 		track->zb_cb_clear = !!(idx_value & (1 << 5));
 		break;
+	case R300_RB3D_AARESOLVE_OFFSET:
+		r = r100_cs_packet_next_reloc(p, &reloc);
+		if (r) {
+			DRM_ERROR("No reloc for ib[%d]=0x%04X\n",
+				  idx, reg);
+			r100_cs_dump_packet(p, pkt);
+			return r;
+		}
+		track->aa.robj = reloc->robj;
+		track->aa.offset = idx_value;
+		track->aa_dirty = true;
+		ib[idx] = idx_value + ((u32)reloc->lobj.gpu_offset);
+		break;
+	case R300_RB3D_AARESOLVE_PITCH:
+		track->aa.pitch = idx_value & 0x3FFE;
+		track->aa_dirty = true;
+		break;
+	case R300_RB3D_AARESOLVE_CTL:
+		track->aaresolve = idx_value & 0x1;
+		track->aa_dirty = true;
+		break;
 	case 0x4e04:
 		/* RB3D_BLENDCNTL */
 		track->blend_read_enable = !!(idx_value & (1 << 2));
