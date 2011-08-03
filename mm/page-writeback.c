17410561904ddaadfc1e5d259e15249d7d8bd4ab
Xufeng Zhang <xufeng.zhang@windriver.com>
2011-08-03 10:29:01 +0800

    SD/MMC: add VFS(ext2/ext4) performance optimization support
    Improve ext2/ext4 performance. Distinguish the usage of write operation.
    used_for_format in struct inode will be indicated whether data is used
    to format device. If both data is not used to format disk and backing
    device is pointed to a real block device, it means write-back mechanism
    write redundant data, and these write operations will take bad effect to
    I/O performance. So, page cache write operation (do_writepages) will skip
    these operations.
    
    Extracted from vendor drop QorIQ-NONDPAA-SDK-V1-20110429_ltib.iso
    linux-2.6.35-qoriq-SD-MMC-add-VFS-ext2-ext4-performance-op.patch and
    0046-SD-MMC-fix-the-race-condition-flaw-when-format-SSD-d.patch.
    
    Signed-off-by: Qiang Liu <b32616@freescale.com>
    Integrated-by: Xufeng Zhang <xufeng.zhang@windriver.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -1013,12 +1013,24 @@ int do_writepages(struct address_space *mapping, struct writeback_control *wbc)
 {
 	int ret;
 
+#ifdef CONFIG_OPTIMIZE_SD_PERFORMANCE
+	/* no need to write device if the operation is not used to format device */
+	if (imajor(mapping->host) &&
+			(wbc->sync_mode == WB_SYNC_NONE) &&
+			!mapping->host->used_for_format)
+		return 0;
+#endif
 	if (wbc->nr_to_write <= 0)
 		return 0;
 	if (mapping->a_ops->writepages)
 		ret = mapping->a_ops->writepages(mapping, wbc);
 	else
 		ret = generic_writepages(mapping, wbc);
+#ifdef CONFIG_OPTIMIZE_SD_PERFORMANCE
+	/* recover initial state of this inode */
+	if (mapping->host->used_for_format && (wbc->sync_mode == WB_SYNC_ALL))
+		mapping->host->used_for_format = 0;
+#endif
 	return ret;
 }
 
