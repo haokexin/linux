8e7a33b6c7bda8e04ea0f7dbe0e5d4662d450fd6
Geoff Thorpe <Geoff.Thorpe@freescale.com>
2012-03-14 04:49:52 +0000

    fsl_usdpaa: extend interface for multi-process support
    This change enhances the ioctl interface of the fsl_usdpaa device to
    support multi-process USDPAA scenarios. Multiple DMA regions can be
    mapped, with support for process-private (and unnamed) maps and
    cross-process (and named) maps. The latter also supports cross-process,
    per-region locks to allow user-space processes to synchronise their
    manipulations of shared regions (the user-space drivers use this
    facility for implementing buffer allocators within such regions).
    
    This change also updates the "hugetlb-like" TLB1 hack to support such
    configurations. In particular, if the boot-arg has a comma-separated
    second parameter, this will instruct the driver to reserve more than one
    TLB1 entry for its fault-handling. Eg. with "usdpaa_mem=64M,4" a 64M
    region will be reserved and 4 TLB1 entries will be available to map it
    or subsets of it, potentially from multiple processes. More than 4
    distinct mappings could be made simultaneously (from one process or
    many), at the risk of significant performance degradation unless no more
    than 4 mappings were being regularly accessed by software.
    
    Signed-off-by: Geoff Thorpe <Geoff.Thorpe@freescale.com>
    [Kevin: Original patch taken from FSL
    QorIQ-SDK-V1.2-SOURCE-20120614-yocto.iso image]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- include/linux/fsl_usdpaa.h
+++ include/linux/fsl_usdpaa.h
@@ -27,10 +27,6 @@ enum usdpaa_id_type {
 	usdpaa_id_max /* <-- not a valid type, represents the number of types */
 };
 #define USDPAA_IOCTL_MAGIC 'u'
-struct usdpaa_ioctl_get_region {
-	uint64_t phys_start;
-	uint64_t phys_len;
-};
 struct usdpaa_ioctl_id_alloc {
 	uint32_t base; /* Return value, the start of the allocated range */
 	enum usdpaa_id_type id_type; /* what kind of resource(s) to allocate */
@@ -44,28 +40,70 @@ struct usdpaa_ioctl_id_release {
 	uint32_t base;
 	uint32_t num;
 };
-#define USDPAA_IOCTL_GET_PHYS_BASE \
-	_IOR(USDPAA_IOCTL_MAGIC, 0x01, struct usdpaa_ioctl_get_region)
+/* Maximum length for a map name, including NULL-terminator */
+#define USDPAA_DMA_NAME_MAX 16
+/* Flags for requesting DMA maps. Maps are private+unnamed or sharable+named.
+ * For a sharable and named map, specify _SHARED (whether creating one or
+ * binding to an existing one). If _SHARED is specified and _CREATE is not, then
+ * the mapping must already exist. If _SHARED and _CREATE are specified and the
+ * mapping doesn't already exist, it will be created. If _SHARED and _CREATE are
+ * specified and the mapping already exists, the mapping will fail unless _LAZY
+ * is specified. When mapping to a pre-existing sharable map, the length must be
+ * an exact match. Lengths must be a power-of-4 multiple of page size.
+ *
+ * Note that this does not actually map the memory to user-space, that is done
+ * by a subsequent mmap() using the page offset returned from this ioctl(). The
+ * ioctl() is what gives the process permission to do this, and a page-offset
+ * with which to do so.
+ */
+#define USDPAA_DMA_FLAG_SHARE    0x01
+#define USDPAA_DMA_FLAG_CREATE   0x02
+#define USDPAA_DMA_FLAG_LAZY     0x04
+struct usdpaa_ioctl_dma_map {
+	/* If the map succeeds, pa_offset is returned and can be used in a
+	 * subsequent call to mmap(). */
+	uint64_t pa_offset;
+	/* Input parameter, the length of the region to be created (or if
+	 * mapping an existing region, this must match it). Must be a power-of-4
+	 * multiple of page size. */
+	uint64_t len;
+	/* Input parameter, the USDPAA_DMA_FLAG_* settings. */
+	uint32_t flags;
+	/* If _FLAG_SHARE is specified, the name of the region to be created (or
+	 * of the existing mapping to use). */
+	char name[USDPAA_DMA_NAME_MAX];
+	/* If this ioctl() creates the mapping, this is an input parameter
+	 * stating whether the region supports locking. If mapping an existing
+	 * region, this is a return value indicating the same thing. */
+	int has_locking;
+	/* In the case of a successful map with _CREATE and _LAZY, this return
+	 * value indicates whether we created the mapped region or whether it
+	 * already existed. */
+	int did_create;
+};
 #define USDPAA_IOCTL_ID_ALLOC \
-	_IOWR(USDPAA_IOCTL_MAGIC, 0x02, struct usdpaa_ioctl_id_alloc)
+	_IOWR(USDPAA_IOCTL_MAGIC, 0x01, struct usdpaa_ioctl_id_alloc)
 #define USDPAA_IOCTL_ID_RELEASE \
-	_IOW(USDPAA_IOCTL_MAGIC, 0x03, struct usdpaa_ioctl_id_release)
+	_IOW(USDPAA_IOCTL_MAGIC, 0x02, struct usdpaa_ioctl_id_release)
+#define USDPAA_IOCTL_DMA_MAP \
+	_IOWR(USDPAA_IOCTL_MAGIC, 0x03, struct usdpaa_ioctl_dma_map)
+/* We implement a cross-process locking scheme per DMA map. Call this ioctl()
+ * with a mmap()'d address, and the process will (interruptible) sleep if the
+ * lock is already held by another process. Process destruction will
+ * automatically clean up any held locks. */
+#define USDPAA_IOCTL_DMA_LOCK \
+	_IOW(USDPAA_IOCTL_MAGIC, 0x04, unsigned char)
+#define USDPAA_IOCTL_DMA_UNLOCK \
+	_IOW(USDPAA_IOCTL_MAGIC, 0x05, unsigned char)
 
 #ifdef __KERNEL__
 
-/* Physical address range */
-extern u64 usdpaa_phys_start;
-extern u64 usdpaa_phys_size;
-
-/* PFN versions */
-extern unsigned long usdpaa_pfn_start;
-extern unsigned long usdpaa_pfn_len;
-
-/* TLB1 index */
-extern unsigned int usdpaa_tlbcam_index;
-
 /* Early-boot hook */
 void __init fsl_usdpaa_init_early(void);
+
+/* Fault-handling in arch/powerpc/mm/mem.c gives USDPAA an opportunity to detect
+ * faults within its ranges via this hook. */
+int usdpaa_test_fault(unsigned long pfn, u64 *phys_addr, u64 *size);
 
 #endif /* __KERNEL__ */
 
