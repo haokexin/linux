11f39c92b2b968cdaea4f1a0297a88108a1812a3
Srikanth Srinivasan <srikanth.srinivasan@freescale.com>
2012-03-29 19:01:41 +0800

    powerpc/85xx: Initroduce support for the P5040 DS reference board
    This patch includes preliminary code for platform,
    the dts files, and the defconfig. In its current
    form, the kernel can boot with a ramdisk fs
    
    Interfaces including pcie, usb, sdhc, espi are tested
    
    Signed-off-by: Srikanth Srinivasan <srikanth.srinivasan@freescale.com>
    Signed-off-by: Timur Tabi <timur@freescale.com>
    Signed-off-by: Roy Zang <tie-fei.zang@freescale.com>
    Signed-off-by: Pan Jiafei <Jiafei.Pan@freescale.com>
    Signed-off-by: Jiang Yutang <b14898@freescale.com>
    [Kevin: The main changes include:
    * Remove the parts for FSL hypervisor support.
    * Using a monolithic dts for p5040ds.
    * Make the qbman portal device node compatible with the SDK 1.1 qbman
      driver.]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

---
+++ arch/powerpc/platforms/85xx/p5040_ds.c
@@ -0,0 +1,82 @@
+/*
+ * P5040 DS Setup
+ *
+ * Copyright 2012 Freescale Semiconductor Inc.
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#include <linux/kernel.h>
+#include <linux/pci.h>
+#include <linux/phy.h>
+
+#include <asm/time.h>
+#include <asm/machdep.h>
+#include <asm/pci-bridge.h>
+#include <asm/udbg.h>
+#include <asm/mpic.h>
+
+#include <linux/of_platform.h>
+#include <sysdev/fsl_soc.h>
+#include <sysdev/fsl_pci.h>
+
+#include "corenet_ds.h"
+
+/*
+ * Called very early, device-tree isn't unflattened
+ */
+static int __init p5040_ds_probe(void)
+{
+	unsigned long root = of_get_flat_dt_root();
+
+	if (of_flat_dt_is_compatible(root, "fsl,P5040DS"))
+		return 1;
+
+	return 0;
+}
+
+#if defined(CONFIG_PHYLIB) && defined(CONFIG_VITESSE_PHY)
+int vsc824x_add_skew(struct phy_device *phydev);
+#define PHY_ID_VSC8244                  0x000fc6c0
+static int __init board_fixups(void)
+{
+	phy_register_fixup_for_uid(PHY_ID_VSC8244, 0xfffff, vsc824x_add_skew);
+
+	return 0;
+}
+machine_device_initcall(p5040_ds, board_fixups);
+#endif
+
+define_machine(p5040_ds) {
+	.name			= "P5040 DS",
+	.probe			= p5040_ds_probe,
+	.setup_arch		= corenet_ds_setup_arch,
+	.init_IRQ		= corenet_ds_pic_init,
+#ifdef CONFIG_PCI
+	.pcibios_fixup_bus	= fsl_pcibios_fixup_bus,
+#endif
+/* coreint doesn't play nice with lazy EE, use legacy mpic for now */
+#ifdef CONFIG_PPC64
+	.get_irq		= mpic_get_irq,
+#else
+	.get_irq		= mpic_get_coreint_irq,
+#endif
+	.restart		= fsl_rstcr_restart,
+	.calibrate_decr		= generic_calibrate_decr,
+	.progress		= udbg_progress,
+#ifdef CONFIG_PPC64
+	.power_save		= book3e_idle,
+#else
+	.power_save		= e500_idle,
+#endif
+	.init_early		= corenet_ds_init_early,
+};
+
+machine_device_initcall(p5040_ds, declare_of_platform_devices);
+
+#ifdef CONFIG_SWIOTLB
+machine_arch_initcall(p5040_ds, swiotlb_setup_bus_notifier);
+#endif
