7dc892860983832959cae205ea3e17768b13f1ff
Tiejun Chen <tiejun.chen@windriver.com>
2012-04-23 15:08:36 +0800

    fsl_usdpaa_shmem: reworked from old "fsl_shmem" driver.
    Extracted from QorIQ-DPAA-SDK-v1.1-20111026-systembuilder.iso
    
    This the old "fsl_shmem" driver, renamed to "fsl_usdpaa_shmem", with a
    variety of updates that make it USDPAA-specific.
    
    The support for a "fsl-shmem" device-tree node specifying the memory
    region is removed, as this required the manual coordination of too many
    hard-coded constants. (User-space address-handling, the "mem=" boot
    parameter to the kernel, the buffer-pool seeding in the device-tree, and
    the "TLB1 hack" in the kernel memory handling.) Note also that the
    original authors of fsl_shmem, who believed it to be removed for good,
    were not keen to see it revived in its existing form.
    
    This change uses a Kconfig setting to trigger an early-boot allocation of
    a physically-contiguous memory range, size is also controlled by a
    Kconfig setting (default: 256MB).
    
    Also, the old "TLB1 hack" is included and has been updated to match
    against the early-boot allocation, rather than assuming hard-coded
    addresses. This also means the driver cannot be built as a loadable
    module, it must be built into the kernel. The driver's early-boot hook
    will attempt to find an unused region of suitably-aligned memory, by
    searching from the high-end of the memory range downwards, though due to a
    conflict with ramdisk handling this logic intentionally limits itself to
    the first 6GB. It also reserves a "tlbcam_index" value to ensure other
    uses of TLB1 (HugeTLB, kvm, ...) don't conflict.
    
    The resulting character device "/dev/fsl-usdpaa-shmem" (renamed) supports
    a new ioctl() to allow the application to retrieve the physical (DMA)
    base-address and size of the memory region. (So this change is inter-
    dependent on a matching change in the user-space "dma_mem" driver.)
    
    Signed-off-by: Geoff Thorpe <Geoff.Thorpe@freescale.com>
    Signed-off-by: Becky Bruce <beckyb@kernel.crashing.org>
    Signed-off-by: Emil Medve <Emilian.Medve@Freescale.com>
    Signed-off-by: Mahima Verma <Mahima.Verma@freescale.com>
    Signed-off-by: Haiying Wang <Haiying.Wang@freescale.com>

---
+++ include/linux/fsl_usdpaa.h
@@ -0,0 +1,54 @@
+/* Copyright 2011 Freescale Semiconductor, Inc.
+ *
+ * This file is licensed under the terms of the GNU General Public License
+ * version 2.  This program is licensed "as is" without any warranty of any
+ * kind, whether express or implied.
+ */
+
+#ifndef FSL_USDPAA_H
+#define FSL_USDPAA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <linux/uaccess.h>
+#include <linux/ioctl.h>
+
+#ifdef CONFIG_FSL_USDPAA_SHMEM
+
+/* Character-device interface. NB: these definitions need to be duplicated in
+ * user-space. It is all temporary until being replaced by HugeTLB. */
+#define USDPAA_IOCTL_MAGIC 'u'
+struct usdpaa_ioctl_get_region {
+	uint64_t phys_start;
+	uint64_t phys_len;
+};
+#define USDPAA_IOCTL_GET_PHYS_BASE \
+	_IOR(USDPAA_IOCTL_MAGIC, 0x01, struct usdpaa_ioctl_get_region)
+
+#ifdef __KERNEL__
+
+/* Physical address range */
+extern u64 usdpaa_phys_start;
+extern u64 usdpaa_phys_size;
+
+/* PFN versions */
+extern unsigned long usdpaa_pfn_start;
+extern unsigned long usdpaa_pfn_len;
+
+/* TLB1 index */
+extern unsigned int usdpaa_tlbcam_index;
+
+/* Early-boot hook */
+void __init fsl_usdpaa_shmem_init_early(void);
+
+#endif /* __KERNEL__ */
+
+#endif /* CONFIG_FSL_USDPAA_SHMEM */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FSL_USDPAA_H */
