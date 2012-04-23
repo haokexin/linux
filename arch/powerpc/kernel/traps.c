5d7cec7b07556bba5ea518616d16a819e88d7f75
Tiejun Chen <tiejun.chen@windriver.com>
2012-04-23 13:59:34 +0800

    powerpc/booke: Re-organize debug code
    Extracted from QorIQ-DPAA-SDK-v1.1-20111026-systembuilder.iso
    
    * set_dabr/do_dabr are no longer used when CNFIG_PPC_ADV_DEBUG_REGS is set
      refactor code a bit such that we only build the dabr code for
      !CONFIG_PPC_ADV_DEBUG_REGS and removed some CONFIG_PPC_ADV_DEBUG_REGS
      code in set_dabr that would never get built.
    
    * Move do_send_trap into traps.c as its only used there
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>
    [Modify for current kernel version context]
    Integrated-by: Tiejun Chen <tiejun.chen@windriver.com>

--- arch/powerpc/kernel/traps.c
+++ arch/powerpc/kernel/traps.c
@@ -1246,6 +1246,24 @@ void SoftwareEmulation(struct pt_regs *regs)
 #endif /* CONFIG_8xx */
 
 #ifdef CONFIG_PPC_ADV_DEBUG_REGS
+static void do_send_trap(struct pt_regs *regs, unsigned long address,
+		  unsigned long error_code, int signal_code, int breakpt)
+{
+	siginfo_t info;
+
+	current->thread.trap_nr = signal_code;
+	if (notify_die(DIE_DABR_MATCH, "dabr_match", regs, error_code,
+			11, SIGSEGV) == NOTIFY_STOP)
+		return;
+
+	/* Deliver the signal to userspace */
+	info.si_signo = SIGTRAP;
+	info.si_errno = breakpt;	/* breakpoint or watchpoint id */
+	info.si_code = signal_code;
+	info.si_addr = (void __user *)address;
+	force_sig_info(SIGTRAP, &info, current);
+}
+
 static void handle_debug(struct pt_regs *regs, unsigned long debug_status)
 {
 	int changed = 0;
