c302daf071985bb810f94222beaba3e9c86ad247
Tiejun Chen <tiejun.chen@windriver.com>
2012-04-23 14:04:27 +0800

    mmc: Use mmc_delay() instead of mdelay() for time delay
    Extracted from QorIQ-DPAA-SDK-v1.1-20111026-systembuilder.iso
    
    The mmc_delay() is a wrapper function for mdelay() and msleep().
    
        o mdelay() -- block the system when busy-waiting.
        o msleep() -- suspend the currently running task to enable CPU
                      to process other tasks, so it is non-blocking
                      regarding the whole system.
    
    When the desired delay time is more than a period of timer interrupt,
    just use msleep(). Change mdelay() to mmc_delay() to avoid chewing
    CPU when busy wait.
    
    Signed-off-by: Shengzhou Liu <b36685@freescale.com>
    Signed-off-by: Chunhe Lan <Chunhe.Lan@freescale.com>
    Cc: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -211,7 +211,7 @@ static void sdhci_reset(struct sdhci_host *host, u8 mask)
 			return;
 		}
 		timeout--;
-		mdelay(1);
+		mmc_delay(1);
 	}
 
 	if (host->ops->platform_reset_exit)
@@ -1001,7 +1001,7 @@ static void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 			return;
 		}
 		timeout--;
-		mdelay(1);
+		mmc_delay(1);
 	}
 
 	mod_timer(&host->timer, jiffies + 10 * HZ);
@@ -1182,7 +1182,7 @@ static void sdhci_set_clock(struct sdhci_host *host, unsigned int clock)
 			return;
 		}
 		timeout--;
-		mdelay(1);
+		mmc_delay(1);
 	}
 
 	clk |= SDHCI_CLOCK_CARD_EN;
@@ -1268,7 +1268,7 @@ static int sdhci_set_power(struct sdhci_host *host, unsigned short power)
 	 * can apply clock after applying power
 	 */
 	if (host->quirks & SDHCI_QUIRK_DELAY_AFTER_POWER)
-		mdelay(10);
+		mmc_delay(1);
 
 	return power;
 }
@@ -1866,7 +1866,7 @@ static int sdhci_execute_tuning(struct mmc_host *mmc, u32 opcode)
 		ctrl = sdhci_readw(host, SDHCI_HOST_CONTROL2);
 		tuning_loop_counter--;
 		timeout--;
-		mdelay(1);
+		mmc_delay(1);
 	} while (ctrl & SDHCI_CTRL_EXEC_TUNING);
 
 	/*
