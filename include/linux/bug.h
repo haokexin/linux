e4db940c927b41cc2317c7b97290e462defe3fc6
Yang Shi <yang.shi@windriver.com>
2012-05-10 14:19:29 -0700

    introduce-build-runtime-bug-on
    Introduce BUILD_RUNTIME_BUG_ON()
    
    BUILD_RUNTIME_BUG_ON - check condition at build (if constant) or runtime
    
    If the condition is a constant and true, the compiler will generate a
    build error. If the condition is not constant, a BUG will be triggered
    at runtime if the condition is ever true. If the condition is constant
    and false, no code is emitted.
    
    Useful to add as sanity check on the input received by API such as
    align.h, which typically receive constant parameters which need to be
    power of two. Having a build failure is expected when an incorrect
    parameter is passed. However, triggering a build failure is not always
    appropriate, because the parameter could sometimes be non-constant
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- include/linux/bug.h
+++ include/linux/bug.h
@@ -72,6 +72,23 @@ extern int __build_bug_on_failed;
 
 #endif	/* __CHECKER__ */
 
+/**
+ * BUILD_RUNTIME_BUG_ON - check condition at build (if constant) or runtime
+ * @condition: the condition which should be false.
+ *
+ * If the condition is a constant and true, the compiler will generate a build
+ * error. If the condition is not constant, a BUG will be triggered at runtime
+ * if the condition is ever true. If the condition is constant and false, no
+ * code is emitted.
+ */
+#define BUILD_RUNTIME_BUG_ON(condition)                                \
+	do {                                                    \
+		if (__builtin_constant_p(condition))            \
+			BUILD_BUG_ON(condition);                \
+		else                                            \
+			BUG_ON(condition);                      \
+	} while (0)
+
 #ifdef CONFIG_GENERIC_BUG
 #include <asm-generic/bug.h>
 
