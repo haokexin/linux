50bc6a56da91aea0e58a0b97e7f7d815f5ceffa2
Liming Wang <liming.wang@windriver.com>
2012-07-19 12:14:13 +0800

    SPEAr: add pmu platform data into initialization
    Oprofile needs pmu to collect data, so add pmu platform data
    to ensure pmu initialization when the kernel boots.
    
    Signed-off-by: Liming Wang <liming.wang@windriver.com>

--- arch/arm/mach-spear13xx/spear1310_evb.c
+++ arch/arm/mach-spear13xx/spear1310_evb.c
@@ -127,6 +127,7 @@ static struct platform_device *plat_devs[] __initdata = {
 	&spear13xx_smi_device,
 	&spear13xx_thermal_device,
 	&spear13xx_wdt_device,
+	&spear13xx_pmu_device,
 
 	/* spear1310 specific devices */
 	&spear1310_plgpio_device,
