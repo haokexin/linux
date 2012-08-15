e79e6cb59747e7a51a200ea752f0a198fd96e08b
Kevin Hao <kexin.hao@windriver.com>
2012-08-15 14:33:22 +0800

    Revert "powerpc/law: correct LAW size"
    We will apply a more latest fix for this errata.
    
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- arch/powerpc/sysdev/fsl_pamu.c
+++ arch/powerpc/sysdev/fsl_pamu.c
@@ -471,7 +471,7 @@ static int __init create_csd(phys_addr_t phys, size_t size)
 
 			law_start = make64(law[i].lawbarh, law[i].lawbarl);
 			law_end = law_start +
-				(1ULL << ((law[i].lawar & LAWAR_SIZE_MASK) + 1));
+				(1ULL << (law[i].lawar & LAWAR_SIZE_MASK));
 
 			if (law_start <= phys && phys < law_end) {
 				law_target = law[i].lawar & LAWAR_TARGET_MASK;
