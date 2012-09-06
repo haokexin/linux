59a41e27734b7e4fc33fb507fef6459b442fe977
Scott Wood <scottwood@freescale.com>
2012-09-06 08:19:30 +0000

    mtd/fsl-nand: set NAND_NO_SUBPAGE_WRITE
    These controllers only do ECC on full-page accesses, even though the ECC
    consists of multiple steps.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    [Kevin: Original patch taken from FSL
    QorIQ-SDK-V1.3-SOURCE-20121114-yocto.iso tarball. Just minor
    context mods in order to port to 3.4 kernel.]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- drivers/mtd/nand/fsl_elbc_nand.c
+++ drivers/mtd/nand/fsl_elbc_nand.c
@@ -795,7 +795,8 @@ static int fsl_elbc_chip_init(struct fsl_elbc_mtd *priv)
 	chip->bbt_md = &bbt_mirror_descr;
 
 	/* set up nand options */
-	chip->options = NAND_NO_READRDY | NAND_NO_AUTOINCR;
+	chip->options = NAND_NO_READRDY | NAND_NO_AUTOINCR |
+			NAND_NO_SUBPAGE_WRITE;
 	chip->bbt_options = NAND_BBT_USE_FLASH;
 
 	chip->controller = &elbc_fcm_ctrl->controller;
