333a3b48a417780e234f9be4d42de2c0494a33e8
Jiang Lu <lu.jiang@windriver.com>
2012-10-22 20:37:56 +0800

    powerpc/elbc: add missing compatible for p3041
    The elbc compatible for p3041 is missing in driver, this will cause lbc
    on p3041 can not be probed, so add the compatible for p3041
    
    Signed-off-by: Shaohui Xie <Shaohui.Xie@freescale.com>
    
    [Extracted from FSL QorIQ-SDK-V1.2-SOURCE-20120614-yocto.iso image]
    Signed-off-by:Jiang Lu <lu.jiang@windriver.com>
    [Rebase on sdkv1.3 context.]
    Signed-off-by: Tiejun Chen <tiejun.chen@windriver.com>

--- arch/powerpc/sysdev/fsl_lbc.c
+++ arch/powerpc/sysdev/fsl_lbc.c
@@ -49,6 +49,7 @@ u32 fsl_lbc_addr(phys_addr_t addr_base)
 	u32 addr = addr_base & 0xffff8000;
 
 	if (of_device_is_compatible(np, "fsl,elbc") ||
+			of_device_is_compatible(np, "fsl,p3041-rev1.0-elbc") ||
 			of_device_is_compatible(np, "fsl,p5020-rev1.0-elbc") ||
 			of_device_is_compatible(np, "fsl,p5040-rev1.0-elbc"))
 		return addr;
@@ -201,6 +202,7 @@ static int __devinit fsl_lbc_ctrl_init(struct fsl_lbc_ctrl *ctrl,
 
 	/* Set the monitor timeout value to the maximum for erratum A001 */
 	if (of_device_is_compatible(node, "fsl,elbc") ||
+		of_device_is_compatible(node, "fsl,p3041-rev1.0-elbc") ||
 		of_device_is_compatible(node, "fsl,p5020-rev1.0-elbc") ||
 		of_device_is_compatible(node, "fsl,p5040-rev1.0-elbc"))
 		clrsetbits_be32(&lbc->lbcr, LBCR_BMT, LBCR_BMTPS);
