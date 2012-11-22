131456d876be978ec2924cb93f98abf3904aedd1
Kevin Hao <kexin.hao@windriver.com>
2012-11-22 10:02:25 +0800

    powerpc/corenet_ds: add the pcie v2.3 & v2.4 to matches table
    Extracted from the patch
    (0805-powerpc-85xx-Add-support-for-P5040DS-board.patch) in FSK SDK 1.3.
    
    Some SoCs such as p5040 integrate a new version pcie controller,
    so add the compatible string of them to matches table.
    
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- arch/powerpc/platforms/85xx/corenet_ds.c
+++ arch/powerpc/platforms/85xx/corenet_ds.c
@@ -76,6 +76,8 @@ void __init corenet_ds_setup_arch(void)
 static struct of_device_id __initdata corenetds_pci_ids[] = {
 	{ .compatible = "fsl,p4080-pcie", },
 	{ .compatible = "fsl,qoriq-pcie-v2.2", },
+	{ .compatible = "fsl,qoriq-pcie-v2.4", },
+	{ .compatible = "fsl,qoriq-pcie-v2.3", },
 	{},
 };
 
