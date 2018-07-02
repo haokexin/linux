964ec422f5cd54e3e1335b0a02cf3e519c373570
James Morse <james.morse@arm.com>
2018-07-02 11:15:31 +0100

    arm64: cpufeature: discover CPU support for MPAM
    commit a7bbf2a759f15a5291d5966018944e0a189cae12 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    ARMv8.4 adds support for 'Memory Partitioning And Monitoring' (MPAM)
    which describes an interface to cache and bandwidth controls wherever
    they appear in the system.
    
    Add support to detect MPAM. Like SVE, MPAM has an extra id register that
    describes the virtualisation support, which is optional. Detect this
    separately so we can detect mismatched/insane systems, but still use
    MPAM on the host even if the virtualisation support is missing.
    
    MPAM needs enabling at the highest implemented exception level, otherwise
    the register accesses trap. The 'enabled' flag is accessible to lower
    exception levels, but its in a register that traps when MPAM isn't enabled.
    The cpufeature 'matches' hook is extended to test this on one of the
    CPUs, so that firwmare can emulate MPAM as disabled if it is reserved
    for use by secure world.
    
    (If you have a boot failure that bisects here its likely your CPUs
    advertise MPAM in the id registers, but firmware failed to either enable
    or MPAM, or emulate the trap as if it were disabled)
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- arch/arm64/include/asm/cpu.h
+++ arch/arm64/include/asm/cpu.h
@@ -69,6 +69,9 @@ struct cpuinfo_arm64 {
 
 	/* pseudo-SMCR for recording maximum SMCR_EL1 LEN value: */
 	u64		reg_smcr;
+
+	/* pseudo-MPAMIDR for recording maximum PARTID and PMG values: */
+	u64		reg_mpamidr;
 };
 
 DECLARE_PER_CPU(struct cpuinfo_arm64, cpu_data);
