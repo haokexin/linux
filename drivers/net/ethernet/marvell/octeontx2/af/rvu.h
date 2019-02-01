7aae15941099272979df58c6ea4b05caf0f4f10d
Pavan Nikhilesh <pbhagavatula@marvell.com>
2019-02-01 14:49:15 +0530

    octeontx2-af: Add TIM unit support.
    commit d35591d560f3f6fe3b11d24e8cc31bda918a141e from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Add TIM (Timer) unit support to AF driver that involves initializing and
    configuring TIM and its rings through mailbox.
    
    Change-Id: Ib42edb126554a5cc78af14f96fa966d5d2202fad
    Signed-off-by: Andrew Pinski <apinski@marvell.com>
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -871,6 +871,24 @@ void rvu_program_channels(struct rvu *rvu);
 /* CN10K RVU - LMT*/
 void rvu_reset_lmt_map_tbl(struct rvu *rvu, u16 pcifunc);
 
+/* TIM APIs */
+int rvu_tim_init(struct rvu *rvu);
+int rvu_mbox_handler_tim_lf_alloc(struct rvu *rvu,
+				  struct tim_lf_alloc_req *req,
+				  struct tim_lf_alloc_rsp *rsp);
+int rvu_mbox_handler_tim_lf_free(struct rvu *rvu,
+				 struct tim_ring_req *req,
+				 struct msg_rsp *rsp);
+int rvu_mbox_handler_tim_config_ring(struct rvu *rvu,
+				     struct tim_config_req *req,
+				     struct msg_rsp *rsp);
+int rvu_mbox_handler_tim_enable_ring(struct rvu *rvu,
+				     struct tim_ring_req *req,
+				     struct tim_enable_rsp *rsp);
+int rvu_mbox_handler_tim_disable_ring(struct rvu *rvu,
+				      struct tim_ring_req *req,
+				      struct msg_rsp *rsp);
+
 #ifdef CONFIG_DEBUG_FS
 void rvu_dbg_init(struct rvu *rvu);
 void rvu_dbg_exit(struct rvu *rvu);
