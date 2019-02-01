d64f1ba72c93d7a5f3366003dbade01284dfa70a
Pavan Nikhilesh <pbhagavatula@marvell.com>
2019-02-01 18:38:42 +0530

    octeontx2-af: manually dain partially consumed TAQ buffers
    commit d650bc3c0469323f06313f9fd9452f0d4de7bb62 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    When work is added to a HWGRP it is first buffered in TOAQ and based on
    GETWORK pressure the work is moved along the pipeline. Each TAQ buffers
    eleven addwork entries. Each HWGRP hold the TAQ buffer until with it
    till it is completely used.
    SSO doesn't release the partially consumed TAQ buffer used by HWGRP
    when HWGRP is reset (SSO_AF_LF_HWGRP_RST). Workaround is to manually drain
    the TAQ buffer through addwork-getwork loop till TAQ buffer is completely
    consumed.
    
    Change-Id: Ifda82a4317e7a66d4c61b4b7f665fca72a02956d
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    [RQ: Replace is_rvu_9xxx_A0() with is_rvu_96xx_A0().]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_reg.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_reg.h
@@ -429,6 +429,9 @@
 #define NIX_PRIV_LFX_INT_CFG		(0x8000020)
 #define NIX_AF_RVU_LF_CFG_DEBUG		(0x8000030)
 
+#define NIX_AF_LF_CFG_SHIFT		17
+#define NIX_AF_LF_SSO_PF_FUNC_SHIFT	16
+
 #define NIX_AF_LINKX_BASE_MASK		GENMASK_ULL(11, 0)
 #define NIX_AF_LINKX_RANGE_MASK		GENMASK_ULL(19, 16)
 
@@ -582,6 +585,7 @@
 #define SSOW_LF_GWS_INT			(0x100ull)
 #define SSOW_LF_GWS_INT_ENA_W1C		(0x118ull)
 #define SSOW_LF_GWS_TAG			(0x200ull)
+#define SSOW_LF_GWS_WQP			(0x210ull)
 #define SSOW_LF_GWS_OP_GET_WORK		(0x600ull)
 #define SSOW_LF_GWS_OP_SWTAG_FLUSH	(0x800ull)
 #define SSOW_LF_GWS_OP_DESCHED		(0x880ull)
@@ -608,6 +612,9 @@
 #define TIM_AF_FLAGS_REG_ENA_TIM	BIT_ULL(0)
 #define TIM_AF_RINGX_CTL1_ENA		BIT_ULL(47)
 #define TIM_AF_RINGX_CTL1_RCF_BUSY	BIT_ULL(50)
+
+#define TIM_AF_RING_GMCTL_SHIFT		3
+#define TIM_AF_RING_SSO_PF_FUNC_SHIFT	0
 
 /* CPT */
 #define CPT_AF_CONSTANTS0               (0x0000)
@@ -698,6 +705,9 @@
 #define CPT_LF_Q_GRP_PTR                0x120
 
 #define NPC_AF_BLK_RST                  (0x00040)
+
+#define CPT_AF_LF_CTL2_SHIFT		3
+#define CPT_AF_LF_SSO_PF_FUNC_SHIFT	32
 
 /* NPC */
 #define NPC_AF_CFG			(0x00000)
