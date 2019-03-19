7d995c94e7d2d4838656f1cf697c3019c7cca8e1
James Morse <james.morse@arm.com>
2019-03-19 17:39:31 +0000

    x86/resctrl: Split arch and fs resctrl locks
    commit 5e2196185f7a4dfa2d7a6c2171e0d902992f25f4 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    resctrl has one mutex that is taken by the arch code, and the fs parts.
    Lets split this up so we can't accidentally have conflicting dependencies
    between architectures.
    
    The arch and fs code interact via cpuhp where the arch code updates
    the domain list. Create a new domain_list_lock for arch code updates
    to the domain list.
    
    It is now no-longer enough for resctrl to take the rdtgroup_mutex to
    block changes to this list, use cpus_read_lock() for that. Taking
    cpus_read_lock() in rdtgroup_kn_lock_live() covers most of these,
    add lockdep_assert_cpus_held() in the cases where the
    rdtgroup_kn_lock_live() call isn't obvious.
    
    Our domain online/offline calls now need to take the rdtgroup_mutex
    themselves.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -25,8 +25,13 @@
 #include <asm/resctrl.h>
 #include "internal.h"
 
-/* Mutex to protect rdtgroup access. */
-DEFINE_MUTEX(rdtgroup_mutex);
+/*
+ * rdt_domain structures are kfree()d when their last cpu goes offline,
+ * and allocated when the first cpu in a new domain comes online.
+ * The rdt_resource's domain list is updated when this happens. The domain
+ * list is protected by cpuhp as a rwlock, writers also take this mutex.
+ */
+DEFINE_MUTEX(domain_list_lock);
 
 /*
  * The cached resctrl_pqr_state is strictly per CPU and can never be
@@ -586,11 +591,11 @@ static int resctrl_arch_online_cpu(unsigned int cpu)
 {
 	struct rdt_resource *r;
 
-	mutex_lock(&rdtgroup_mutex);
+	mutex_lock(&domain_list_lock);
 	for_each_capable_rdt_resource(r)
 		domain_add_cpu(cpu, r);
 	clear_closid_rmid(cpu);
-	mutex_unlock(&rdtgroup_mutex);
+	mutex_unlock(&domain_list_lock);
 
 	return resctrl_online_cpu(cpu);
 }
@@ -601,11 +606,11 @@ static int resctrl_arch_offline_cpu(unsigned int cpu)
 
 	resctrl_offline_cpu(cpu);
 
-	mutex_lock(&rdtgroup_mutex);
+	mutex_lock(&domain_list_lock);
 	for_each_capable_rdt_resource(r)
 		domain_remove_cpu(cpu, r);
 	clear_closid_rmid(cpu);
-	mutex_unlock(&rdtgroup_mutex);
+	mutex_unlock(&domain_list_lock);
 
 	return 0;
 }
