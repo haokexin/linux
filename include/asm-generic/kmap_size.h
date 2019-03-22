f7fe9823d4dbbec18dd536aaa374ac5d02b7db5f
John Jacques <john.jacques@intel.com>
2019-03-22 17:11:41 +0200

    ARM: kmap: axxia: Prevent overlap for 16th core
    commit 9b15bbc552b3523b4fa263e94c980a53ed03c94e from
    https://github.com/axxia/linux-yocto.git v5.10/standard/axxia-dev/base.
    
    Prevent overlap between fixmap mapping and CPU vector
    page for 16th core if SMP enabled.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>
    Signed-off-by: Charlie Paul <charlie.paul@windriver.com>
    Signed-off-by: Daniel Dragomir <daniel.dragomir@windriver.com>
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- include/asm-generic/kmap_size.h
+++ include/asm-generic/kmap_size.h
@@ -2,11 +2,20 @@
 #ifndef _ASM_GENERIC_KMAP_SIZE_H
 #define _ASM_GENERIC_KMAP_SIZE_H
 
+#if defined(CONFIG_ARCH_AXXIA) && defined(CONFIG_SMP)
+#if (CONFIG_NR_CPUS > 15)
+/* Prevent overlap between fixmap mapping and CPU vector page for 16th core */
+#define KM_MAX_IDX 15
+#else
+ #define KM_MAX_IDX 16
+#endif
+#else
 /* For debug this provides guard pages between the maps */
 #ifdef CONFIG_DEBUG_KMAP_LOCAL
 # define KM_MAX_IDX	33
 #else
 # define KM_MAX_IDX	16
+#endif
 #endif
 
 #endif
