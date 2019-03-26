4c4805f9f1713dd7eb225cb94c19965655285a55
Pavan Nikhilesh <pbhagavatula@marvell.com>
2019-03-26 12:07:32 +0530

    octeontx2-af: add sso error af interrupt handlers
    commit 909dfd0cfaadd0db39e8c46a45b251f1c61f872c from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Added debug messages for SSO_AF_ERR0, SSO_AF_ERR2 and SSO_AF_RAS SSO AF
    error interrupts.
    
    Change-Id: I8c15fb93807903d9192e0d951ceffc3c95f2a8ac
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -2997,6 +2997,7 @@ static void rvu_unregister_interrupts(struct rvu *rvu)
 {
 	int irq;
 
+	rvu_sso_unregister_interrupts(rvu);
 	rvu_cpt_unregister_interrupts(rvu);
 	rvu_ree_unregister_interrupts(rvu);
 
@@ -3209,6 +3210,10 @@ static int rvu_register_interrupts(struct rvu *rvu)
 		goto fail;
 	}
 	rvu->irq_allocated[offset] = true;
+
+	ret = rvu_sso_register_interrupts(rvu);
+	if (ret)
+		goto fail;
 
 	ret = rvu_cpt_register_interrupts(rvu);
 	if (ret)
