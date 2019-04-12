02c8bf585a4b91604b121cad6953fb4ba0254118
Subbaraya Sundeep <sbhatta@marvell.com>
2019-04-12 15:22:35 +0530

    octeontx2-pf: add support for stripping STAG/CTAG
    commit bd2892394658f238be94d85c2f519819459f08ea from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Add support for stripping VLAN tag. In case double tagged packet
    is received only the outer VLAN gets stripped off.
    And  VLAN offload can be done installed with VTAG action.
    
    Change-Id: Id5a35348ec325eb79d15f5632822e310cc9d8287
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Tomasz Duszynski <tduszynski@marvell.com>
    [Kevin: Drop the change for otx2_set_features()]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>
    [RQ: Replace inline function otx2_mbox_lock/otx2_mbox_unlock with mutex_lock/mutex_unlock.]
    Signed-off-by: Ruiqiang Hao <ruiqiang.hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -251,6 +251,8 @@ struct otx2_nic {
 
 	/* Block address of NIX either BLKADDR_NIX0 or BLKADDR_NIX1 */
 	int			nix_blkaddr;
+	u16			rxvlan_entry;
+	bool			rxvlan_alloc;
 };
 
 static inline bool is_otx2_lbkvf(struct pci_dev *pdev)
@@ -613,6 +615,8 @@ int otx2_update_rq_stats(struct otx2_nic *pfvf, int qidx);
 int otx2_update_sq_stats(struct otx2_nic *pfvf, int qidx);
 void otx2_set_ethtool_ops(struct net_device *netdev);
 void otx2vf_set_ethtool_ops(struct net_device *netdev);
+int otx2_install_rxvlan_offload_flow(struct otx2_nic *pfvf);
+int otx2_delete_rxvlan_offload_flow(struct otx2_nic *pfvf);
 
 int otx2_open(struct net_device *netdev);
 int otx2_stop(struct net_device *netdev);
