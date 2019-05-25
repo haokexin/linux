7c6260828fb1783f9dfb3ad7ec96912055df091b
Stanislaw Kardach <skardach@marvell.com>
2019-05-25 14:20:06 -0400

    octeontx2-af: add TIM LF teardown
    commit 68a4a4790f18128bf03df64cd955ea2417eb2dc3 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    TIM LF teardown was missing from the code which resulted in LF mapping
    registers not being cleared.
    
    Change-Id: I71b4fbf2c70fef6ddaa997270aae93b1fb5e3d05
    Signed-off-by: Stanislaw Kardach <skardach@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/9956
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -847,11 +847,6 @@ void rvu_sso_deinit_xaq_aura(struct rvu *rvu, int blkaddr, int npa_blkaddr,
 			     int aura, int lf);
 void rvu_sso_xaq_aura_write(struct rvu *rvu, int lf, u64 val);
 
-/* TIM APIs */
-int rvu_tim_init(struct rvu *rvu);
-int rvu_lf_lookup_tim_errata(struct rvu *rvu, struct rvu_block *block,
-		u16 pcifunc, int slot);
-
 /* NPA APIs */
 int rvu_npa_init(struct rvu *rvu);
 void rvu_npa_freemem(struct rvu *rvu);
@@ -975,6 +970,12 @@ void rvu_program_channels(struct rvu *rvu);
 
 /* CN10K RVU - LMT*/
 void rvu_reset_lmt_map_tbl(struct rvu *rvu, u16 pcifunc);
+
+/* TIM APIs */
+int rvu_tim_init(struct rvu *rvu);
+int rvu_tim_lf_teardown(struct rvu *rvu, u16 pcifunc, int lf, int slot);
+int rvu_lf_lookup_tim_errata(struct rvu *rvu, struct rvu_block *block,
+		u16 pcifunc, int slot);
 
 #ifdef CONFIG_DEBUG_FS
 void rvu_dbg_init(struct rvu *rvu);
