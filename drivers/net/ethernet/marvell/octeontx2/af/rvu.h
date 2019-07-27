dd5e08ea632aa106b473ab6aff1108f57f7d0ed3
Sunil Goutham <sgoutham@marvell.com>
2019-07-27 13:34:47 +0530

    octeontx2-af: Apply relevant HW issue workarounds for 96xx B0 silicon
    commit b8a531980931575d9a3c95341572ff129d9426e1 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Made changes to apply relevant workarounds for HW issues which
    are present in 96xx B0 silicon. Also moved some silicon checks to
    a common place to reduce clutter.
    
    Note: Only picked code related to TIM.
    
    Change-Id: I09b7d3812c9a14c0a574ed4bb4bbbdf133323e2a
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/13140
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -608,7 +608,8 @@ static inline bool is_rvu_96xx_B0(struct rvu *rvu)
 {
 	struct pci_dev *pdev = rvu->pdev;
 
-	return (pdev->revision == 0x00) || (pdev->revision == 0x01);
+	return ((pdev->revision == 0x00) || (pdev->revision == 0x01)) &&
+		(pdev->subsystem_device == PCI_SUBSYS_DEVID_96XX);
 }
 
 static inline bool is_rvu_95xx_A0(struct rvu *rvu)
@@ -986,8 +987,6 @@ void rvu_reset_lmt_map_tbl(struct rvu *rvu, u16 pcifunc);
 /* TIM APIs */
 int rvu_tim_init(struct rvu *rvu);
 int rvu_tim_lf_teardown(struct rvu *rvu, u16 pcifunc, int lf, int slot);
-int rvu_lf_lookup_tim_errata(struct rvu *rvu, struct rvu_block *block,
-		u16 pcifunc, int slot);
 
 #ifdef CONFIG_DEBUG_FS
 void rvu_dbg_init(struct rvu *rvu);
@@ -996,6 +995,10 @@ void rvu_dbg_exit(struct rvu *rvu);
 static inline void rvu_dbg_init(struct rvu *rvu) {}
 static inline void rvu_dbg_exit(struct rvu *rvu) {}
 #endif
+
+/* HW workarounds/fixes */
+int rvu_tim_lookup_rsrc(struct rvu *rvu, struct rvu_block *block,
+			u16 pcifunc, int slot);
 
 int rvu_ndc_fix_locked_cacheline(struct rvu *rvu, int blkaddr);
 
@@ -1013,5 +1016,4 @@ int rvu_get_hwvf(struct rvu *rvu, int pcifunc);
 int rvu_mcs_init(struct rvu *rvu);
 int rvu_mcs_flr_handler(struct rvu *rvu, u16 pcifunc);
 void rvu_mcs_exit(struct rvu *rvu);
-
 #endif /* RVU_H */
