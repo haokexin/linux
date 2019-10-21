c514f029cac8bb99ead6cb7ddd3b07ffa4b50edd
Harman Kalra <hkalra@marvell.com>
2019-10-21 20:46:14 +0530

    octeontx2-pf: notify VF about ptp event
    commit 1f56892993fb7d9b4aa13ed8481e3422e3d87ab1 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    In case of PTP enable/disable event, PF should inform its VFs
    about the event. So that VFs can adjust the shifted data offset
    (viz 8 bytes on PTP enable).
    
    Change-Id: I83d45441ee18c53bad3b68249c32cdc273cf4fc9
    Signed-off-by: Harman Kalra <hkalra@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/17618
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -13,6 +13,7 @@
 
 #include <linux/pci.h>
 #include <linux/iommu.h>
+#include <linux/ptp_clock_kernel.h>
 
 #include <mbox.h>
 #include "otx2_reg.h"
@@ -198,6 +199,7 @@ struct otx2_hw {
 struct otx2_vf_config {
 	struct otx2_nic *pf;
 	struct delayed_work link_event_work;
+	struct delayed_work ptp_info_work;
 	bool intf_down; /* interface was either configured or not */
 	u8 mac[ETH_ALEN];
 	u16 vlan;
@@ -219,7 +221,15 @@ struct otx2_mac_table {
 	bool inuse;
 };
 
-struct otx2_ptp;
+struct otx2_ptp {
+	struct ptp_clock_info ptp_info;
+	struct ptp_clock *ptp_clock;
+	struct otx2_nic *nic;
+
+	struct cyclecounter cycle_counter;
+	struct timecounter time_counter;
+	bool ptp_en;
+};
 
 struct otx2_nic {
 	void __iomem		*reg_base;
