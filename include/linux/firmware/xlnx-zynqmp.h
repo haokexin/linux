30b09a3bbf7bd55b4e36bcef7df054c153d50742
Michal Simek <michal.simek@xilinx.com>
2019-12-03 01:03:44 -0800

    firmware: xilinx: Add ULPI reset support
    commit  df1f6fd6c9cc18dd883b8006bb4a16e1067872ed from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.4
    
    Add missing IOCTLs for ULPI reset.
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>
    [Quanyang: Because of mainline commit 426c8d85df7a ("firmware: xilinx: Use APIs instead of IOCTLs"),
    use the new API zynqmp_pm_ulpi_reset instead of eemi_ops->ioctl]

--- include/linux/firmware/xlnx-zynqmp.h
+++ include/linux/firmware/xlnx-zynqmp.h
@@ -151,6 +151,8 @@ enum pm_ioctl_id {
 	IOCTL_READ_GGS = 13,
 	IOCTL_WRITE_PGGS = 14,
 	IOCTL_READ_PGGS = 15,
+	/* IOCTL for ULPI reset */
+	IOCTL_ULPI_RESET,
 	/* Set healthy bit value */
 	IOCTL_SET_BOOT_HEALTH_STATUS = 17,
 	IOCTL_AFI,
@@ -481,6 +483,7 @@ int zynqmp_pm_read_ggs(u32 index, u32 *value);
 int zynqmp_pm_write_pggs(u32 index, u32 value);
 int zynqmp_pm_read_pggs(u32 index, u32 *value);
 int zynqmp_pm_afi(u32 index, u32 value);
+int zynqmp_pm_ulpi_reset(void);
 int zynqmp_pm_system_shutdown(const u32 type, const u32 subtype);
 int zynqmp_pm_set_boot_health_status(u32 value);
 #else
@@ -626,6 +629,10 @@ static inline int zynqmp_pm_read_pggs(u32 index, u32 *value)
 	return -ENODEV;
 }
 static inline int zynqmp_pm_afi(u32 index, u32 value)
+{
+	return -ENODEV;
+}
+static inline int zynqmp_pm_ulpi_reset(void)
 {
 	return -ENODEV;
 }
