b470c4e025f7aa4fb087242e589f8762a0869019
Subbaraya Sundeep <sbhatta@marvell.com>
2019-12-15 14:31:56 +0530

    octeontx2-af: Assign NIX block addresses
    commit 67c1d821e7ec6bee1c682377270bda1da2d6ea6f from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    For CGX mapped PFs attach the NIX block address
    as either NIX0 or NIX1 based on the info set
    by firmware. For LBK VFs assign NIX0 for even
    numbered VFs and NIX1 for odd numbered VFs.
    
    Change-Id: I4ed0e10cad109ece7fe8d933af8d13ac37f4faae
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/27986
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_npc.c
@@ -37,6 +37,7 @@ static void npc_mcam_free_all_counters(struct rvu *rvu, struct npc_mcam *mcam,
 static int npc_mcam_verify_channel(struct rvu *rvu, u16 pcifunc, u8 intf,
 				   u16 channel)
 {
+	int max_lbkid = rvu->hw->lbk_links - 1;
 	int pf = rvu_get_pf(pcifunc);
 	u8 cgx_id, lmac_id;
 	int base = 0, end;
@@ -48,6 +49,7 @@ static int npc_mcam_verify_channel(struct rvu *rvu, u16 pcifunc, u8 intf,
 		end = rvu_get_num_lbk_chans();
 		if (end < 0)
 			return -EINVAL;
+		end = NIX_CHAN_LBK_CHX(max_lbkid, end);
 	} else {
 		rvu_get_cgx_lmac_id(rvu->pf2cgxlmac_map[pf], &cgx_id, &lmac_id);
 		base = NIX_CHAN_CGX_LMAC_CHX(cgx_id, lmac_id, 0x0);
