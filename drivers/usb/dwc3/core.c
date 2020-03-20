3903bc68ce98df0a311d80fe9f30829fbdfd61bb
Li Jun <jun.li@nxp.com>
2020-03-20 14:21:54 +0800

    LF-2345-1 usb: dwc3: core: add a core init flag for device mode resume
    commit 010c1c25b0d34e522ccadc2fce32adfffa755c1d from
    https://github.com/nxp-imx/linux-imx.git
    
    In case system resume by device mode event(e.g, VBUS), runtime resume
    will happen before system resume, to avoid doing device mode resume
    again when system resume, add a core init flag.
    
    Reviewed-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Li Jun <jun.li@nxp.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -840,6 +840,8 @@ static void dwc3_core_exit(struct dwc3 *dwc)
 	dwc3_phy_exit(dwc);
 	dwc3_clk_disable(dwc);
 	reset_control_assert(dwc->reset);
+
+	dwc->core_inited = false;
 }
 
 static bool dwc3_core_is_valid(struct dwc3 *dwc)
@@ -1366,6 +1368,8 @@ static int dwc3_core_init(struct dwc3 *dwc)
 	}
 
 	dwc3_config_threshold(dwc);
+
+	dwc->core_inited = true;
 
 	return 0;
 
@@ -2175,6 +2179,15 @@ static int dwc3_resume_common(struct dwc3 *dwc, pm_message_t msg)
 
 	switch (dwc->current_dr_role) {
 	case DWC3_GCTL_PRTCAP_DEVICE:
+		/*
+		 * system resume may come after runtime resume
+		 * e.g. rpm suspend -> pm suspend -> wakeup
+		 * -> rpm resume -> system resume, so if already
+		 *  runtime resumed, system resume should skip it.
+		 */
+		if (dwc->core_inited)
+			break;
+
 		ret = dwc3_core_init_for_resume(dwc);
 		if (ret)
 			return ret;
