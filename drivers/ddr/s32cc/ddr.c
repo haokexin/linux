bcaeb38b900714a893520d6c6c33ee941e54b16e
Ciprian Marian Costea <ciprianmarian.costea@nxp.com>
2020-09-03 17:01:43 +0300

    s32cc: ddr: Add workaround for Erratum ERR050543
    commit 1fe810f1bc6aa433a856ebb0b348146ea5425b9f from
    https://github.com/nxp-auto-linux/linux
    
    This commit adds a new driver based on Linux Kernel Timers.
    
    The Linux driver operates in two scenarios:
    
    1. Booting at high temp (> 85 C):
    U-boot applies ERR050543 workaround. In this case, the
    Linux driver periodically checks if ERR050543 workaround
    is no longer needed (with a time period of 1 sec).
    If it is not, refresh rate is updated and auto derating
    is turned on (ERR050543 changes are partially removed).
    After this operation, no periodically polling of MR4 register
    is being performed by the driver.
    
    2. Booting at low/room temp (< 85 C):
    U-boot does not apply the ERR050543 workaround. Therefore, no
    polling is being performed from Linux at any point in time,
    in the future.
    
    Issue: ALB-5557
    Issue: ALB-5853
    Issue: ALB-6298
    Issue: ALB-7531
    Issue: ALB-7692
    Issue: ALB-9124
    Issue: ALB-10870
    Signed-off-by: Ciprian Marian Costea <ciprianmarian.costea@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

---
+++ drivers/ddr/s32cc/ddr.c
@@ -0,0 +1,212 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2020-2021 NXP
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/init.h>
+#include <linux/timer.h>
+#include <linux/io.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/init.h>
+#include <linux/processor.h>
+#include <linux/spinlock.h>
+#include "ddr.h"
+
+struct ddr_priv {
+	void __iomem *ddrc_base;
+	void __iomem *perf_base;
+	struct device *dev;
+	struct timer_list timer;
+	bool shutdown;
+
+	/* When the timer is active, reading/writing the 'shutdown'
+	 * flag will be done in a critical section.
+	 * This way the timer cannot be reset before
+	 * this flag is set.
+	 */
+	spinlock_t shutdown_access;
+};
+
+static void reset_timer(struct ddr_priv *data)
+{
+	mod_timer(&data->timer, jiffies +
+			msecs_to_jiffies(MSEC_PER_SEC));
+}
+
+static void cleanup_ddr_errata(struct ddr_priv *data)
+{
+	if (!data)
+		return;
+
+	data->shutdown = true;
+	del_timer(&data->timer);
+}
+
+/* Read lpddr4 mode register with given index */
+u32 read_lpddr4_mr(u8 MR_index, void __iomem *ddrc_base,
+		   void __iomem *perf_base)
+{
+	u32 reg;
+
+	/* Set MRR_DDR_SEL_REG to 0x1 to enable LPDDR4 mode */
+	reg = readl(perf_base + OFFSET_MRR_0_DATA_REG_ADDR);
+	writel((reg | MRR_DDR_SEL_REG), perf_base +
+		OFFSET_MRR_0_DATA_REG_ADDR);
+
+	/*
+	 * Ensure no MR transaction is in progress:
+	 * mr_wr_busy signal must be low
+	 */
+	spin_until_cond((readl(ddrc_base + OFFSET_DDRC_MRSTAT) &
+				DDRC_MRSTAT_MR_WR_FLAG) == 0);
+
+	/* Set MR_TYPE = 0x1 (Read) and MR_RANK = 0x1 (Rank 0) */
+	reg = readl(ddrc_base + OFFSET_DDRC_MRCTRL0);
+	reg |= DDRC_MRCTRL0_MR_TYPE_READ;
+	reg &= ~DDRC_MRCTRL0_MR_RANK_MASK;
+	writel(reg | DDRC_MRCTRL0_MR_RANK_OFF, ddrc_base + OFFSET_DDRC_MRCTRL0);
+
+	/* Configure MR address: MRCTRL1[8:15] */
+	reg = readl(ddrc_base + OFFSET_DDRC_MRCTRL1);
+	reg &= ~DDRC_MRCTRL1_MR_ADDR_MASK;
+	writel(reg | (MR_index << DDRC_MRCTRL1_MR_ADDR_SHIFT),
+	       ddrc_base + OFFSET_DDRC_MRCTRL1);
+
+	/* Initiate MR transaction: MR_WR = 0x1 */
+	reg = readl(ddrc_base + OFFSET_DDRC_MRCTRL0);
+	writel(reg | DDRC_MRCTRL0_MR_WR_MASK, ddrc_base + OFFSET_DDRC_MRCTRL0);
+
+	/* Wait until MR transaction completed */
+	spin_until_cond((readl(ddrc_base + OFFSET_DDRC_MRSTAT) &
+				DDRC_MRSTAT_MR_WR_FLAG) == 0);
+
+	return readl(perf_base + OFFSET_MRR_1_DATA_REG_ADDR);
+}
+
+/*
+ * Read Temperature Update Flag from lpddr4 MR4 register.
+ * This method actually reads the first 3 bits of MR4 (MR4[2:0])
+ * instead of the TUF flag.
+ * The return value is being used in order to determine if the
+ * timing parameters need to be adjusted or not.
+ */
+u8 read_TUF(void __iomem *ddrc_base, void __iomem *perf_base)
+{
+	u32 MR4_val;
+	u8 MR4_die_1, MR4_die_2;
+
+	MR4_val = read_lpddr4_mr(MR4, ddrc_base, perf_base);
+	MR4_die_1 = MR4_val & 0x7;
+	MR4_die_2 = (MR4_val >> 16) & 0x7;
+
+	return MR4_die_1 > MR4_die_2 ? MR4_die_1 : MR4_die_2;
+}
+
+static void execute_polling(struct timer_list *t)
+{
+	struct ddr_priv *data = from_timer(data, t, timer);
+	void __iomem *ddrc_base = data->ddrc_base;
+	void __iomem *perf_base = data->perf_base;
+	int ret;
+
+	spin_lock_bh(&data->shutdown_access);
+	if (data->shutdown) {
+		spin_unlock_bh(&data->shutdown_access);
+		return;
+	}
+
+	ret = poll_derating_temp_errata(ddrc_base, perf_base);
+	if (ret) {
+		cleanup_ddr_errata(data);
+		spin_unlock_bh(&data->shutdown_access);
+	} else {
+		spin_unlock_bh(&data->shutdown_access);
+		reset_timer(data);
+	}
+}
+
+static void __iomem *get_perf_map_by_phandle(struct device *dev)
+{
+	struct device_node *perf_node;
+	void __iomem *perf_regs;
+
+	perf_node = of_parse_phandle(dev->of_node, "perf-phandle", 0);
+	if (!perf_node) {
+		dev_info(dev, "perf-phandle node not found\n");
+		return (void __iomem *)ERR_PTR(-ENODEV);
+	}
+
+	perf_regs = of_iomap(perf_node, 0);
+	if (!perf_regs) {
+		dev_warn(dev, "Cannot map PERF registers\n");
+		return (void __iomem *)ERR_PTR(-ENOMEM);
+	}
+	of_node_put(perf_node);
+
+	return perf_regs;
+}
+
+static int ddr_probe(struct platform_device *pdev)
+{
+	struct ddr_priv *data;
+	struct device *dev = &pdev->dev;
+
+	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
+	if (unlikely(!data))
+		return -ENOMEM;
+
+	data->ddrc_base = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(data->ddrc_base))
+		return PTR_ERR(data->ddrc_base);
+
+	data->perf_base = get_perf_map_by_phandle(dev);
+	data->dev = dev;
+	data->shutdown = false;
+
+	spin_lock_init(&data->shutdown_access);
+
+	/* Start the timer. */
+	timer_setup(&data->timer, execute_polling, 0);
+	reset_timer(data);
+
+	return 0;
+}
+
+static void ddr_remove(struct platform_device *pdev)
+{
+	struct ddr_priv *data = platform_get_drvdata(pdev);
+
+	spin_lock_bh(&data->shutdown_access);
+	if (!data->shutdown)
+		cleanup_ddr_errata(data);
+	spin_unlock_bh(&data->shutdown_access);
+
+	dev_info(data->dev, "device removed\n");
+}
+
+static const struct of_device_id s32cc_ddr_errata_dt_ids[] = {
+	{
+		.compatible = "nxp,s32cc-ddr",
+	}, {}
+};
+MODULE_DEVICE_TABLE(of, s32cc_ddr_errata_dt_ids);
+
+static struct platform_driver s32cc_ddr_errata_driver = {
+	.driver = {
+		.name = KBUILD_MODNAME,
+		.of_match_table = s32cc_ddr_errata_dt_ids,
+	},
+	.probe = ddr_probe,
+	.remove_new = ddr_remove,
+};
+
+module_platform_driver(s32cc_ddr_errata_driver);
+
+MODULE_AUTHOR("NXP");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("NXP S32CC DDR Errata ERR050543 Driver");
