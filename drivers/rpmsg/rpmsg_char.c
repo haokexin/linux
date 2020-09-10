9786bc36104f0b626ecc691724f6e7ea3468c24f
Suman Anna <s-anna@ti.com>
2020-09-10 13:39:41 -0500

    rpmsg: char: Add device id_table for auto-probe
    commit 959bc1fe641a2cf518ed0c9518660c2b963f9eaa from
    git://git.ti.com/ti-linux-kernel/ti-linux-kernel.git
    
    The rpmsg char driver is not auto-probed for any rpmsg device
    without relying on the device's driver_override feature. Add a
    device id_table to the rpmsg-char driver to facilitate this
    auto-probe. Any rpmsg device with the name "rpmsg_chrdev" will
    be auto-probed to begin with.
    
    The driver_override will continue to work just fine as well.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Subhajit Paul <subhajit_paul@ti.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/rpmsg/rpmsg_char.c
+++ drivers/rpmsg/rpmsg_char.c
@@ -470,17 +470,17 @@ static void rpmsg_chrdev_remove(struct rpmsg_device *rpdev)
 		dev_warn(&rpdev->dev, "failed to destroy endpoints: %d\n", ret);
 }
 
-static struct rpmsg_device_id rpmsg_chrdev_id_table[] = {
-	{ .name	= "rpmsg-raw" },
+static const struct rpmsg_device_id rpmsg_char_id_table[] = {
+	{ .name	= "rpmsg_chrdev" },
 	{ },
 };
+MODULE_DEVICE_TABLE(rpmsg, rpmsg_char_id_table);
 
 static struct rpmsg_driver rpmsg_chrdev_driver = {
 	.probe = rpmsg_chrdev_probe,
 	.remove = rpmsg_chrdev_remove,
 	.callback = rpmsg_ept_cb,
-	.id_table = rpmsg_chrdev_id_table,
-	.drv.name = "rpmsg_chrdev",
+	.id_table = rpmsg_char_id_table,
 };
 
 static int rpmsg_chrdev_init(void)
