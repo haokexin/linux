52c3f7202f8d455ef5fd9142e6aa671acc3f252b
Quanyang Wang <quanyang.wang@windriver.com>
2020-09-22 10:28:12 +0800

    PCI: XDMA PL: use pci_parse_request_of_pci_ranges instead of devm_of_pci_get_host_bridge_resources
    Because of mainline commit 3b55809cf91f ("PCI: Make
    devm_of_pci_get_host_bridge_resources() static"), use
    pci_parse_request_of_pci_ranges instead of
    devm_of_pci_get_host_bridge_resources().
    
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@gmail.com>

--- drivers/pci/controller/pcie-xdma-pl.c
+++ drivers/pci/controller/pcie-xdma-pl.c
@@ -832,8 +832,6 @@ static int xilinx_pcie_probe(struct platform_device *pdev)
 	struct pci_bus *child;
 	struct pci_host_bridge *bridge;
 	int err;
-	resource_size_t iobase = 0;
-	LIST_HEAD(res);
 
 	bridge = devm_pci_alloc_host_bridge(dev, sizeof(*port));
 	if (!bridge)
@@ -857,18 +855,13 @@ static int xilinx_pcie_probe(struct platform_device *pdev)
 		return err;
 	}
 
-	err = devm_of_pci_get_host_bridge_resources(dev, 0, 0xff, &res,
-						    &iobase);
+	err = pci_parse_request_of_pci_ranges(dev, &bridge->windows,
+										  &bridge->dma_ranges, NULL);
 	if (err) {
 		dev_err(dev, "Getting bridge resources failed\n");
 		return err;
 	}
 
-	err = devm_request_pci_bus_resources(dev, &res);
-	if (err)
-		goto error;
-
-	list_splice_init(&res, &bridge->windows);
 	bridge->dev.parent = dev;
 	bridge->sysdata = port;
 	bridge->busnr = port->root_busno;
@@ -878,7 +871,7 @@ static int xilinx_pcie_probe(struct platform_device *pdev)
 
 	err = pci_scan_root_bus_bridge(bridge);
 	if (err)
-		goto error;
+		return err;
 
 	bus = bridge->bus;
 
@@ -887,10 +880,6 @@ static int xilinx_pcie_probe(struct platform_device *pdev)
 		pcie_bus_configure_settings(child);
 	pci_bus_add_devices(bus);
 	return 0;
-
-error:
-	pci_free_resource_list(&res);
-	return err;
 }
 
 static const struct of_device_id xilinx_pcie_of_match[] = {
