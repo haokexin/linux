135cdbc08b9416e3de7c0e61d7bd54a9d6637c71
Pavan Nikhilesh <pbhagavatula@marvell.com>
2020-09-30 23:36:59 +0530

    octeontx2-af: cn10K: Set default vWQE timer interval
    commit 59f47497794ac3cfc239643f518b043c21713473 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Set the vWQE timer interval to 102us i.e. the max configurable interval.
    This increases the probability of the vWQE reaching its max size.
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Change-Id: I7c48b23a6a305725b609dfb126b3b906e9a0da57
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/37286
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Tested-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Signed-off-by: Sunil Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.h
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.h
@@ -903,6 +903,9 @@ void rvu_program_channels(struct rvu *rvu);
 /* CN10K RVU - LMT*/
 void rvu_reset_lmt_map_tbl(struct rvu *rvu, u16 pcifunc);
 
+/* CN10K NIX */
+void rvu_nix_block_cn10k_init(struct rvu *rvu, struct nix_hw *nix_hw);
+
 /* TIM APIs */
 int rvu_tim_init(struct rvu *rvu);
 int rvu_tim_lf_teardown(struct rvu *rvu, u16 pcifunc, int lf, int slot);
