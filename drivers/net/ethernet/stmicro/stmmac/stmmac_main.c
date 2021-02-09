9e79efd21f939be85ef902a6c6a27f36b3de41fc
Shenwei Wang <shenwei.wang@nxp.com>
2021-02-09 11:06:48 -0600

    LF-3319 net: stmmac: enable rx_clk when killing the vlan
    commit  2816b3b1ce51135f60ff8fa09eed93b1d04d2129 from
    https://github.com/nxp-imx/linux-imx lf-6.1.y
    
    when killing the vlan, the rx_clk from the phy need to be enabled.
    The patch checks the phy's status before killing the vlan. If the phy
    is stopped, enable it, remove the vlan, and then stop the phy.
    
    Reviewed-by: Frank Li <frank.li@nxp.com>
    Signed-off-by: Shenwei Wang <shenwei.wang@nxp.com>
    Acked-by: Jason Liu <jason.hui.liu@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3837,6 +3837,7 @@ static int __stmmac_open(struct net_device *dev,
 	phylink_start(priv->phylink);
 	/* We may have called phylink_speed_down before */
 	phylink_speed_up(priv->phylink);
+	priv->is_phy_started = true;
 
 	ret = stmmac_request_irq(dev);
 	if (ret)
@@ -3904,6 +3905,7 @@ static int stmmac_release(struct net_device *dev)
 	/* Stop and disconnect the PHY */
 	phylink_stop(priv->phylink);
 	phylink_disconnect_phy(priv->phylink);
+	priv->is_phy_started = false;
 
 	stmmac_disable_all_queues(priv);
 
@@ -6372,7 +6374,15 @@ static int stmmac_vlan_rx_kill_vid(struct net_device *ndev, __be16 proto, u16 vi
 	clear_bit(vid, priv->active_vlans);
 
 	if (priv->hw->num_vlan) {
-		ret = stmmac_del_hw_vlan_rx_fltr(priv, ndev, priv->hw, proto, vid);
+		if (priv->is_phy_started == false) {
+			stmmac_init_phy(ndev);
+			phylink_start(priv->phylink);
+			ret = stmmac_del_hw_vlan_rx_fltr(priv, ndev, priv->hw, proto, vid);
+			phylink_stop(priv->phylink);
+			phylink_disconnect_phy(priv->phylink);
+		} else {
+			ret = stmmac_del_hw_vlan_rx_fltr(priv, ndev, priv->hw, proto, vid);
+		}
 		if (ret)
 			goto del_vlan_error;
 	}
