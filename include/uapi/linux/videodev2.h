104c816dbe838869dda63c337421b55cceffa21e
Sidraya Jayagond <sidraya.bj@pathparntertech.com>
2021-06-30 16:59:12 +0530

    v4l: videodev2: Add 10bit definitions for NV12 and NV16 color formats
    The default color formats support only 8bit color depth. This patch
    adds 10bit definitions for NV12 and NV16.
    
    Signed-off-by: Amit Makani <amit.makani@ti.com>
    Signed-off-by: Sunita Nadampalli <sunitan@ti.com>
    Signed-off-by: Sidraya Jayagond <sidraya.bj@pathparntertech.com>
    [Xulin: Original patch taken from TI PROCESSOR-SDK-LINUX-J721E_08.00.00.08]
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -773,6 +773,8 @@ struct v4l2_pix_format {
 #define V4L2_PIX_FMT_INZI     v4l2_fourcc('I', 'N', 'Z', 'I') /* Intel Planar Greyscale 10-bit and Depth 16-bit */
 #define V4L2_PIX_FMT_CNF4     v4l2_fourcc('C', 'N', 'F', '4') /* Intel 4-bit packed depth confidence information */
 #define V4L2_PIX_FMT_HI240    v4l2_fourcc('H', 'I', '2', '4') /* BTTV 8-bit dithered RGB */
+#define V4L2_PIX_FMT_TI1210   v4l2_fourcc('T', 'I', '1', '2') /* TI NV12 10-bit, two bytes per channel */
+#define V4L2_PIX_FMT_TI1610   v4l2_fourcc('T', 'I', '1', '6') /* TI NV16 10-bit, two bytes per channel */
 #define V4L2_PIX_FMT_QC08C    v4l2_fourcc('Q', '0', '8', 'C') /* Qualcomm 8-bit compressed */
 #define V4L2_PIX_FMT_QC10C    v4l2_fourcc('Q', '1', '0', 'C') /* Qualcomm 10-bit compressed */
 
