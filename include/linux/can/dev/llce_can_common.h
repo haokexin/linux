9fdc5797f50087cfb2da09755404afc7f39f2bf2
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2021-08-04 10:15:47 +0300

    can: llce: logger: Provide the logs over SocketCAN
    commit 10e440cb0f2539f2ca78621330e6fec56d129482 from
    https://github.com/nxp-auto-linux/linux
    
    This changes the design of LLCE Logger from one single instance
    that provides the logs in text mode to one virtual CAN instance for
    each LLCE CAN interface which sends the logs using SoketCAN.
    
    Issue: ALB-7505
    Issue: ALB-10883
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- include/linux/can/dev/llce_can_common.h
+++ include/linux/can/dev/llce_can_common.h
@@ -36,21 +36,6 @@ static inline void unpack_word0(u32 word0, bool *rtr, bool *ide,
 	*rtr = !!(word0 & LLCE_CAN_MB_RTR);
 }
 
-static inline u32 pack_word0(bool rtr, bool ide, u32 id)
-{
-	u32 word0;
-
-	word0 = (rtr << LLCE_CAN_MB_RTR_SHIFT);
-	word0 |= (ide << LLCE_CAN_MB_IDE_SHIFT);
-
-	if (ide)
-		word0 |= (id & CAN_EFF_MASK);
-	else
-		word0 |= (id & CAN_SFF_MASK) <<  LLCE_CAN_MB_IDSTD_SHIFT;
-
-	return word0;
-}
-
 static inline void unpack_word1(u32 word1, bool *fdf, u8 *dlc, bool *brs,
 				bool *esi)
 {
@@ -58,18 +43,6 @@ static inline void unpack_word1(u32 word1, bool *fdf, u8 *dlc, bool *brs,
 	*brs = !!(word1 & LLCE_CAN_MB_BRS);
 	*esi = !!(word1 & LLCE_CAN_MB_ESI);
 	*fdf = !!(word1 & LLCE_CAN_MB_FDF);
-}
-
-static inline u32 pack_word1(bool fdf, u8 dlc, bool brs, bool esi)
-{
-	u32 word1;
-
-	word1 = (brs << LLCE_CAN_MB_BRS_SHIFT);
-	word1 |= (fdf << LLCE_CAN_MB_FDF_SHIFT);
-	word1 |= (esi << LLCE_CAN_MB_ESI_SHIFT);
-	word1 |= dlc;
-
-	return word1;
 }
 
 struct llce_can_dev *init_llce_can_dev(struct device *dev, size_t priv_size,
