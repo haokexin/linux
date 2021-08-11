5426ffb36e3b023c1d59d7d55e6e6a7e0af8482a
Wojciech Bartczak <wbartczak@marvell.com>
2021-08-11 10:35:26 -0700

    driver: soc: Modify error reporting for some SoC drivers
    commit 0b3058c2e474d94ea739b51845ddd3c0664e71a1 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    The change modifies error reporting for CN10KX SoC drivers.
    Warnings for non-supported platforms have been replaced
    with debug information displayed in dmesg.
    
    Signed-off-by: Wojciech Bartczak <wbartczak@marvell.com>
    Change-Id: Ic6067220f333f1166e995f2c67b3018f16454de6
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/58845
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/soc/marvell/cn10k-rpram.c
+++ drivers/soc/marvell/cn10k-rpram.c
@@ -148,7 +148,7 @@ static int __init cn10k_rpram_init(void)
 
 	ret = octeontx_soc_check_smc();
 	if (ret != 2) {
-		pr_info("%s: Not supported\n", __func__);
+		pr_debug("%s: Not supported\n", __func__);
 		return -EPERM;
 	}
 
