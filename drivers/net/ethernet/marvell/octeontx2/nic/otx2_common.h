d15ea6c7244f1ac56d53c7a64292e7339a569773
Subbaraya Sundeep <sbhatta@marvell.com>
2021-09-13 12:45:25 +0530

    octeontx2-pf: Add devlink param to vary rbuf size
    commit 0d1f920f02c79fcc426c606d694fcc77daa1f5d4 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    The size of receive buffers for receiving packets
    is calculated based on the mtu of interface. This
    patch adds devlink parameter to utilize user given
    size for receive buffers instead.
    
    Change-Id: Ibf8bcf40f31c3cbb1474fc3f71c419ac3823e6e0
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/61668
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    [RQ: Drop the parts which have been already applied.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -201,6 +201,7 @@ struct otx2_hw {
 #define OTX2_DEFAULT_RBUF_LEN	2048
 	u16			rbuf_len;
 	u32			xqe_size;
+	u16			rbuf_fixed_size;
 
 	/* NPA */
 	u32			stack_pg_ptrs;  /* No of ptrs per stack page */
