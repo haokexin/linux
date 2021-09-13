832dc64f4fa28c2afd2f3b4efc78a18a2ae82ee2
Subbaraya Sundeep <sbhatta@marvell.com>
2021-09-13 12:45:25 +0530

    octeontx2-pf: Add devlink param to vary rbuf size
    commit 0d1f920f02c79fcc426c606d694fcc77daa1f5d4 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    The size of receive buffers for receiving packets
    is calculated based on the mtu of interface. This
    patch adds devlink parameter to utilize user given
    size for receive buffers instead.
    
    Change-Id: Ibf8bcf40f31c3cbb1474fc3f71c419ac3823e6e0
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/61668
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -68,6 +68,10 @@ static int otx2_change_mtu(struct net_device *netdev, int new_mtu)
 	netdev_info(netdev, "Changing MTU from %d to %d\n",
 		    netdev->mtu, new_mtu);
 	netdev->mtu = new_mtu;
+	/* Modify receive buffer size based on MTU and do not
+	 * use the fixed size set.
+	 */
+	pf->hw.rbuf_fixed_size = 0;
 
 	if (if_up)
 		err = otx2_open(netdev);
@@ -1424,6 +1428,9 @@ static int otx2_get_rbuf_size(struct otx2_nic *pf, int mtu)
 	int frame_size;
 	int total_size;
 	int rbuf_size;
+
+	if (pf->hw.rbuf_fixed_size)
+		return pf->hw.rbuf_fixed_size;
 
 	/* The data transferred by NIX to memory consists of actual packet
 	 * plus additional data which has timestamp and/or EDSA/HIGIG2
