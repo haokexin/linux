868e7a8229c86f6bc002dbf7fae8b2b23709d075
Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
2021-12-14 11:13:25 +0530

    spi: spi-cadence-quadspi: Add Rx periodic Tuning support
    commit 0b9816b0f71af4cbb2f41bfc414e1a7c7a09bc54 from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.15
    
    This patch add support for RX periodic tuning.
    Periodic tuning is recommended based on the expected temperature
    and voltage drift ranges.
    For every 5minutes, driver will schedule the periodic tuning work.
    
    Signed-off-by: Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -1673,6 +1673,8 @@ static int spi_nor_erase(struct mtd_info *mtd, struct erase_info *instr)
 	if (ret)
 		return ret;
 
+	reinit_completion(&nor->spimem->request_completion);
+
 	/* whole-chip erase? */
 	if (len == mtd->size && !(nor->flags & SNOR_F_NO_OP_CHIP_ERASE)) {
 		unsigned long timeout;
@@ -1732,6 +1734,8 @@ static int spi_nor_erase(struct mtd_info *mtd, struct erase_info *instr)
 	ret = spi_nor_write_disable(nor);
 
 erase_err:
+	complete(&nor->spimem->request_completion);
+
 	spi_nor_unlock_and_unprep(nor);
 
 	return ret;
@@ -1926,6 +1930,8 @@ static int spi_nor_read(struct mtd_info *mtd, loff_t from, size_t len,
 	if (ret)
 		return ret;
 
+	reinit_completion(&nor->spimem->request_completion);
+
 	while (len) {
 		loff_t addr = from;
 
@@ -1949,6 +1955,7 @@ static int spi_nor_read(struct mtd_info *mtd, loff_t from, size_t len,
 	ret = 0;
 
 read_err:
+	complete(&nor->spimem->request_completion);
 	spi_nor_unlock_and_unprep(nor);
 	return ret;
 }
@@ -1970,6 +1977,8 @@ static int spi_nor_write(struct mtd_info *mtd, loff_t to, size_t len,
 	ret = spi_nor_lock_and_prep(nor);
 	if (ret)
 		return ret;
+
+	reinit_completion(&nor->spimem->request_completion);
 
 	for (i = 0; i < len; ) {
 		ssize_t written;
@@ -2010,6 +2019,7 @@ static int spi_nor_write(struct mtd_info *mtd, loff_t to, size_t len,
 	}
 
 write_err:
+	complete(&nor->spimem->request_completion);
 	spi_nor_unlock_and_unprep(nor);
 	return ret;
 }
@@ -3322,6 +3332,9 @@ static int spi_nor_probe(struct spi_mem *spimem)
 	nor->spimem = spimem;
 	nor->dev = &spi->dev;
 	spi_nor_set_flash_node(nor, spi->dev.of_node);
+
+	if (nor->spimem)
+		init_completion(&nor->spimem->request_completion);
 
 	spi_mem_set_drvdata(spimem, nor);
 
