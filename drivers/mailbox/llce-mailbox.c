72cb845356b0088c70e850b9fae645fb3e7b04ac
Julia Cartwright <j.cartwright@kodiak.ai>
2022-03-09 15:51:21 -0600

    mailbox: llce: discontinue use of DO_ONCE
    commit dba91d3083b7133705488be2c0b9a99b60e30b66 from
    https://github.com/nxp-auto-linux/linux
    
    DO_ONCE isn't the correct mechanism for _most_ drivers: it's intended
    purpose is to perform a task once _globally_ (not per-device).
    
    In addition, functions invoked with DO_ONCE() are executed with
    interrupts disabled, and so are therefore constrained to the set of
    operations that can be performed (and registering an interrupt is _not_
    one of those operations).
    
    Instead, just open code a check.
    
    This resolves the following lockdep splat:
    
        =============================
        [ BUG: Invalid wait context ]
        -----------------------------
        ffffff8804a23648 (&desc->request_mutex){+.+.}-{3:3}, at: __setup_irq+0xc0/0x760
        other info that might help us debug this:
        context-{4:4}
        2 locks held by systemd-udevd/1203:
         #0: ffffff88029f4988 (&dev->mutex){....}-{3:3}, at: device_driver_attach+0x4c/0xe0
         #1: ffffffc011350008 (once_lock){....}-{2:2}, at: __do_once_start+0x2c/0x74
        stack backtrace:
        CPU: 0 PID: 1203 Comm: systemd-udevd Tainted: G        W  O      5.10.41-4.4.4-rt42 #1
        Hardware name: MicroSys S32G274ASBC (DT)
        Call trace:
         dump_backtrace+0x0/0x1b0
         show_stack+0x24/0x30
         dump_stack+0xec/0x154
         __lock_acquire+0x810/0x2010
         lock_acquire.part.0+0xf8/0x350
         lock_acquire+0x68/0x84
         __mutex_lock+0xa0/0x4e4
         mutex_lock_nested+0x44/0x70
         __setup_irq+0xc0/0x760
         request_threaded_irq+0xe8/0x1b0
         devm_request_threaded_irq+0x84/0x100
         request_llce_pair_irq.isra.0+0x3c/0xb0 [llce_mailbox]
         llce_hif_startup+0x98/0xc0 [llce_mailbox]
         llce_mb_probe+0x2a0/0x420 [llce_mailbox]
         platform_drv_probe+0x60/0xb4
         really_probe+0xec/0x4d4
         driver_probe_device+0x64/0xcc
         device_driver_attach+0xd4/0xe0
         __driver_attach+0x94/0x140
         bus_for_each_dev+0x7c/0xd0
         driver_attach+0x30/0x3c
         bus_add_driver+0x114/0x1fc
         driver_register+0x84/0x140
         __platform_driver_register+0x54/0x60
         llce_mb_driver_init+0x30/0x1000 [llce_mailbox]
         do_one_initcall+0x90/0x450
         do_init_module+0x60/0x284
         load_module+0x2268/0x2950
         __do_sys_finit_module+0xb8/0xfc
         __arm64_sys_finit_module+0x2c/0x40
         el0_svc_common.constprop.0+0x84/0x1e0
         do_el0_svc+0x30/0xa0
         el0_svc+0x20/0x30
         el0_sync_handler+0x1a4/0x1b0
         el0_sync+0x180/0x1c0
    
    Issue: ALB-8732
    
    Signed-off-by: Julia Cartwright <j.cartwright@kodiak.ai>
    Signed-off-by: Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>
    Signed-off-by: Radu Pirea <radu-nicolae.pirea@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- drivers/mailbox/llce-mailbox.c
+++ drivers/mailbox/llce-mailbox.c
@@ -19,7 +19,6 @@
 #include <linux/module.h>
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
-#include <linux/once.h>
 #include <linux/platform_device.h>
 #include <linux/processor.h>
 #include <linux/slab.h>
@@ -78,6 +77,7 @@ struct llce_fifoirq {
 	unsigned int num;
 	const char *name;
 	irq_handler_t handler;
+	bool registered;
 };
 
 struct llce_pair_irq {
@@ -726,12 +726,16 @@ static int request_llce_irq(struct llce_mb *mb, struct llce_fifoirq *fifo_irq)
 {
 	int ret;
 
+	if (fifo_irq->registered)
+		return 0;
+
 	ret = devm_request_irq(mb->dev, fifo_irq->num, fifo_irq->handler,
 			       IRQF_SHARED, fifo_irq->name, mb);
 	if (ret < 0)
 		dev_err(mb->dev, "Failed to register '%s' IRQ\n",
 			fifo_irq->name);
 
+	fifo_irq->registered = true;
 	return ret;
 }
 
@@ -755,7 +759,7 @@ static int llce_hif_startup(struct mbox_chan *chan)
 	struct llce_chan_priv *priv = chan->con_priv;
 	struct llce_mb *mb = priv->mb;
 
-	DO_ONCE(request_llce_pair_irq, mb, &mb->rxin_irqs);
+	request_llce_pair_irq(mb, &mb->rxin_irqs);
 
 	return 0;
 }
@@ -766,7 +770,7 @@ static int llce_rx_startup(struct mbox_chan *chan)
 	struct llce_mb *mb = priv->mb;
 	unsigned long flags;
 
-	DO_ONCE(request_llce_pair_irq, mb, &mb->rxout_irqs);
+	request_llce_pair_irq(mb, &mb->rxout_irqs);
 
 	/* State change must go under the lock protection */
 	spin_lock_irqsave(&priv->lock, flags);
@@ -809,7 +813,7 @@ static int llce_tx_startup(struct mbox_chan *chan)
 	struct llce_mb *mb = priv->mb;
 	unsigned long flags;
 
-	DO_ONCE(request_llce_pair_irq, mb, &mb->txack_irqs);
+	request_llce_pair_irq(mb, &mb->txack_irqs);
 
 	spin_lock_irqsave(&priv->lock, flags);
 	priv->state = LLCE_REGISTERED_CHAN;
@@ -845,7 +849,9 @@ static int llce_logger_startup(struct mbox_chan *chan)
 	unsigned long flags;
 	int ret = 0;
 
-	DO_ONCE(request_llce_irq, mb, &mb->logger_irq);
+	ret = request_llce_irq(mb, &mb->logger_irq);
+	if (ret)
+		return ret;
 
 	spin_lock_irqsave(&priv->lock, flags);
 	priv->state = LLCE_REGISTERED_CHAN;
