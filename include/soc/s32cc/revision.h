e6cd63ad5e77d81e31c610a6a23c6c24805006bc
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-03-28 13:27:41 +0300

    s32cc: nvmem: Refactor SIUL NVMEM to sync with SCMI NVMEM
    commit e4895642a96a72dfeb3bc27f6d676db2b46dc14a from
    https://github.com/nxp-auto-linux/linux
    
    Added implementation in the SIUL NVMEM driver for getting
    "soc_major" and "soc_minor" versions separately, and refactored
    the `s32cc_nvmem_get_soc_revision` to make separate calls to NVMEM
    in order to get both the major and minor versions. The data provided
    by the NVMEM SIUL cells is not shifted anymore now, but rather
    stored in the LSByte of the `u32` buffer returned.
    
    Also, for the `s32cc_nvmem_get_soc_revision` and the
    `s32cc_nvmem_get_pcie_dev_id` helpers, removed the manual passing
    of the cell name, since the functions' names are explicit enough.
    
    Issue: ALB-8078
    Issue: ALB-9139
    Issue: ALB-9554
    Issue: ALB-10857
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- include/soc/s32cc/revision.h
+++ include/soc/s32cc/revision.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2022 NXP
  *
  */
 
@@ -10,39 +10,71 @@
 #include <linux/nvmem-consumer.h>
 #include <soc/s32cc/revision_defs.h>
 
-static inline
-int s32cc_siul2_nvmem_get_soc_revision(struct device *dev,
-				       const char *cname,
-				       struct s32cc_soc_rev *soc_rev)
+static inline char *read_nvmem_cell(struct device *dev,
+				    const char *cname)
 {
 	struct nvmem_cell *cell;
 	ssize_t len;
 	char *buf;
-	int ret = 0;
-
-	if (!soc_rev)
-		return -EINVAL;
 
 	cell = nvmem_cell_get(dev, cname);
 	if (IS_ERR(cell))
-		return PTR_ERR(cell);
+		return ERR_PTR(cell);
 
 	buf = nvmem_cell_read(cell, &len);
 	nvmem_cell_put(cell);
 
 	if (IS_ERR(buf))
-		return PTR_ERR(buf);
+		return buf;
 
 	if (len != sizeof(u32)) {
-		ret = -EOPNOTSUPP;
-		goto out;
+		kfree(buf);
+		return ERR_PTR(-EOPNOTSUPP);
 	}
 
-	soc_rev->minor = ((*(u32 *)buf) >> S32CC_SOC_REV_MINOR_SHIFT) & 0xFF;
-	soc_rev->major = ((*(u32 *)buf) >> S32CC_SOC_REV_MAJOR_SHIFT) & 0xFF;
-
-out:
-	kfree(buf);
-	return ret;
+	return buf;
 }
+
+static inline
+int s32cc_nvmem_get_soc_revision(struct device *dev,
+				 struct s32cc_soc_rev *soc_rev)
+{
+	char *buf_major = NULL, *buf_minor = NULL;
+
+	if (!soc_rev)
+		return -EINVAL;
+
+	buf_major = read_nvmem_cell(dev, "soc_major");
+	if (IS_ERR(buf_major))
+		return PTR_ERR(buf_major);
+
+	buf_minor = read_nvmem_cell(dev, "soc_minor");
+	if (IS_ERR(buf_minor)) {
+		kfree(buf_major);
+		return PTR_ERR(buf_minor);
+	}
+
+	soc_rev->minor = *(u32 *)buf_minor;
+	soc_rev->major = *(u32 *)buf_major;
+
+	kfree(buf_major);
+	kfree(buf_minor);
+	return 0;
+}
+
+static inline
+int s32cc_nvmem_get_pcie_dev_id(struct device *dev, u32 *pcie_variant_bits)
+{
+	char *buf;
+
+	buf = read_nvmem_cell(dev, "pcie_dev_id");
+	if (IS_ERR(buf))
+		return PTR_ERR(buf);
+
+	*pcie_variant_bits = *(u32 *)buf;
+
+	kfree(buf);
+	return 0;
+}
+
 #endif /* __SOC_S32CC_REVISION_H__*/
