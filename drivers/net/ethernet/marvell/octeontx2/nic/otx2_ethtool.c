ebde434cbb6220bc770620219122ef5dd4354b78
Radha Mohan Chintakuntla <radhac@marvell.com>
2022-05-11 13:27:30 -0700

    octeontx2-vf: Add partial ethtool support for SDP VFs
    commit 20340966b52bf3f8eaa8200c00ee4d36d502e931 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    The SDP VFs can support some of the ethtool operations. So enable it and
    return appropriate value for unsupported operations.
    
    Signed-off-by: Radha Mohan Chintakuntla <radhac@marvell.com>
    Change-Id: Idaac387bb9231241f3773e185e448228137b86ea
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/76858
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Satananda Burla <sburla@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_ethtool.c
@@ -311,6 +311,9 @@ static int otx2_set_channels(struct net_device *dev,
 	bool if_up = netif_running(dev);
 	int err = 0, qos_txqs;
 
+	if (is_otx2_sdpvf(pfvf->pdev))
+		return -EOPNOTSUPP;
+
 	if (!channel->rx_count || !channel->tx_count)
 		return -EINVAL;
 
@@ -353,7 +356,7 @@ static void otx2_get_pauseparam(struct net_device *netdev,
 	struct otx2_nic *pfvf = netdev_priv(netdev);
 	struct cgx_pause_frm_cfg *req, *rsp;
 
-	if (is_otx2_lbkvf(pfvf->pdev))
+	if (is_otx2_lbkvf(pfvf->pdev) || is_otx2_sdpvf(pfvf->pdev))
 		return;
 
 	mutex_lock(&pfvf->mbox.lock);
@@ -380,7 +383,7 @@ static int otx2_set_pauseparam(struct net_device *netdev,
 	if (pause->autoneg)
 		return -EOPNOTSUPP;
 
-	if (is_otx2_lbkvf(pfvf->pdev))
+	if (is_otx2_lbkvf(pfvf->pdev) || is_otx2_sdpvf(pfvf->pdev))
 		return -EOPNOTSUPP;
 
 	if (pause->rx_pause)
@@ -943,8 +946,8 @@ static u32 otx2_get_link(struct net_device *netdev)
 {
 	struct otx2_nic *pfvf = netdev_priv(netdev);
 
-	/* LBK link is internal and always UP */
-	if (is_otx2_lbkvf(pfvf->pdev))
+	/* LBK and SDP links are internal and always UP */
+	if (is_otx2_lbkvf(pfvf->pdev) || is_otx2_sdpvf(pfvf->pdev))
 		return 1;
 	return pfvf->linfo.link_up;
 }
@@ -1676,7 +1679,7 @@ static int otx2vf_get_link_ksettings(struct net_device *netdev,
 {
 	struct otx2_nic *pfvf = netdev_priv(netdev);
 
-	if (is_otx2_lbkvf(pfvf->pdev)) {
+	if (is_otx2_lbkvf(pfvf->pdev) || is_otx2_sdpvf(pfvf->pdev)) {
 		cmd->base.duplex = DUPLEX_FULL;
 		cmd->base.speed = SPEED_100000;
 	} else {
