22c648964f210e63374ef3a2b1cbab4983b1a2a4
Quanyang Wang <quanyang.wang@windriver.com>
2022-06-13 12:01:44 +0800

    net: xilinx: fix mixed declarations and code compile warning
    commit 5e0be2b8c0a423b650f4e4d107be37e858d2b9dd from
    https://github.com/Xilinx/linux-xlnx.git xlnx_rebase_v5.15
    
    drivers/net/ethernet/xilinx/xilinx_axienet_main.c:3402:3: warning: ISO C90 forbids mixed declarations and code [-Wdeclaration-after-statement]
     3402 |   struct resource txtsres, rxtsres;
          |   ^~~~~~
    
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>
    State: pending
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/net/ethernet/xilinx/xilinx_axienet_main.c
+++ drivers/net/ethernet/xilinx/xilinx_axienet_main.c
@@ -3202,6 +3202,9 @@ static int axienet_probe(struct platform_device *pdev)
 	struct resource *ethres;
 	u32 value;
 	u16 num_queues = XAE_MAX_QUEUES;
+#ifdef CONFIG_XILINX_AXI_EMAC_HWTSTAMP
+	struct resource txtsres, rxtsres;
+#endif
 
 	ret = of_property_read_u16(pdev->dev.of_node, "xlnx,num-queues",
 				   &num_queues);
@@ -3440,8 +3443,6 @@ static int axienet_probe(struct platform_device *pdev)
 	}
 
 #ifdef CONFIG_XILINX_AXI_EMAC_HWTSTAMP
-		struct resource txtsres, rxtsres;
-
 		/* Find AXI Stream FIFO */
 		np = of_parse_phandle(pdev->dev.of_node, "axififo-connected",
 				      0);
