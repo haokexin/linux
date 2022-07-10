4b5af1332e96354cc141cdb6d984f8fadb38988f
Matt Ranostay <mranostay@ti.com>
2022-07-09 20:55:17 -0700

    PCI: j721e: Add per platform maximum lane settings
    commit 490f0f63a6bbd7a92b2479f14244f9c0e0a1f339 from
    git://git.ti.com/ti-linux-kernel/ti-linux-kernel.git
    
    Various platforms have different maximum amount of lanes that
    can be selected. Add max_lanes to struct j721e_pcie to allow
    for error checking on num-lanes selection from device tree.
    
    Cc: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Matt Ranostay <mranostay@ti.com>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/pci/controller/cadence/pci-j721e.c
+++ drivers/pci/controller/cadence/pci-j721e.c
@@ -67,8 +67,6 @@ enum eoi_reg {
 
 #define GENERATION_SEL_MASK		GENMASK(1, 0)
 
-#define MAX_LANES			2
-
 struct j721e_pcie {
 	struct device		*dev;
 	struct clk		*refclk;
@@ -94,6 +92,7 @@ struct j721e_pcie_data {
 	unsigned int		quirk_detect_quiet_flag:1;
 	u32			linkdown_irq_regfield;
 	unsigned int		byte_access_allowed:1;
+	unsigned int		max_lanes;
 	bool			is_intc_v1;
 	const struct cdns_pcie_ops *ops;
 };
@@ -436,6 +435,7 @@ static const struct j721e_pcie_data j721e_pcie_rc_data = {
 	.quirk_retrain_flag = false,
 	.byte_access_allowed = false,
 	.linkdown_irq_regfield = LINK_DOWN,
+	.max_lanes = 2,
 	.is_intc_v1 = true,
 	.ops = &j721e_pcie_ops,
 };
@@ -443,6 +443,7 @@ static const struct j721e_pcie_data j721e_pcie_rc_data = {
 static const struct j721e_pcie_data j721e_pcie_ep_data = {
 	.mode = PCI_MODE_EP,
 	.linkdown_irq_regfield = LINK_DOWN,
+	.max_lanes = 2,
 	.ops = &j721e_pcie_ops,
 };
 
@@ -450,6 +451,7 @@ static const struct j721e_pcie_data j7200_pcie_rc_data = {
 	.mode = PCI_MODE_RC,
 	.quirk_detect_quiet_flag = true,
 	.linkdown_irq_regfield = J7200_LINK_DOWN,
+	.max_lanes = 4,
 	.is_intc_v1 = false,
 	.byte_access_allowed = true,
 	.ops = &j7200_pcie_ops,
@@ -458,17 +460,20 @@ static const struct j721e_pcie_data j7200_pcie_rc_data = {
 static const struct j721e_pcie_data j7200_pcie_ep_data = {
 	.mode = PCI_MODE_EP,
 	.quirk_detect_quiet_flag = true,
+	.max_lanes = 4,
 };
 
 static const struct j721e_pcie_data am64_pcie_rc_data = {
 	.mode = PCI_MODE_RC,
 	.linkdown_irq_regfield = J7200_LINK_DOWN,
 	.byte_access_allowed = true,
+	.max_lanes = 1,
 };
 
 static const struct j721e_pcie_data am64_pcie_ep_data = {
 	.mode = PCI_MODE_EP,
 	.linkdown_irq_regfield = J7200_LINK_DOWN,
+	.max_lanes = 1,
 };
 
 static const struct of_device_id of_j721e_pcie_match[] = {
@@ -547,7 +552,7 @@ static int j721e_pcie_probe(struct platform_device *pdev)
 	pcie->user_cfg_base = base;
 
 	ret = of_property_read_u32(node, "num-lanes", &num_lanes);
-	if (ret || num_lanes > MAX_LANES)
+	if (ret || num_lanes > data->max_lanes)
 		num_lanes = 1;
 	pcie->num_lanes = num_lanes;
 
