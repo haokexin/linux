4f15bc0b8ceb848e2f88db2113471f4e84020b85
Larisa Grigore <larisa.grigore@nxp.com>
2022-08-22 15:26:07 +0300

    serial: linflex: Guard all clock usages for emulator
    commit f5ce644d08e7b0fda28b138a5808a5c0856448b6 from
    https://github.com/nxp-auto-linux/linux
    
    On emulator, we should also guard clock disabling.
    
    Issue: ALB-6122
    Issue: ALB-9101
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- drivers/tty/serial/fsl_linflexuart.c
+++ drivers/tty/serial/fsl_linflexuart.c
@@ -1547,8 +1547,10 @@ static int linflex_probe(struct platform_device *pdev)
 
 	ret = uart_add_one_port(&linflex_reg, sport);
 	if (ret) {
+#if !defined(CONFIG_S32CC_EMULATOR)
 		clk_disable_unprepare(lfport->clk);
 		clk_disable_unprepare(lfport->clk_ipg);
+#endif
 		goto linflex_probe_free_dma;
 	}
 
@@ -1570,8 +1572,10 @@ static int linflex_remove(struct platform_device *pdev)
 
 	uart_remove_one_port(&linflex_reg, sport);
 
+#if !defined(CONFIG_S32CC_EMULATOR)
 	clk_disable_unprepare(lfport->clk);
 	clk_disable_unprepare(lfport->clk_ipg);
+#endif
 
 	if (lfport->dma_tx_chan)
 		dma_release_channel(lfport->dma_tx_chan);
