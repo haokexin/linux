8ffe0d87e1afe8ec198ea27e5d50e9ac9d24966d
Teodor Marina <teodor.marina@nxp.com>
2022-09-06 15:03:24 +0300

    rtc: s32cc: Use rtc_class_ops structure instead of standard sysfs implementation
    commit 20162126656d741009da9a9f8601923a2a97301d from
    https://github.com/nxp-auto-linux/linux
    
    Implement rtc_class_ops callbacks for integration with user-space tools
    such as rtcwake.
    
    Issue: ALB-3505
    Issue: ALB-3506
    Issue: ALB-9114
    Issue: ALB-10867
    Signed-off-by: Teodor Marina <teodor.marina@nxp.com>
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- drivers/rtc/rtc-s32cc.c
+++ drivers/rtc/rtc-s32cc.c
@@ -19,6 +19,7 @@
 #include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
+#include <linux/rtc.h>
 
 #define RTCSUPV_OFFSET	0x0ul
 #define RTCC_OFFSET		0x4ul
@@ -45,21 +46,27 @@
 #define RTCS_APIF			BIT(13)
 #define RTCS_ROVRF			BIT(10)
 
-#define RTCVAL_VALUE	0xfff00ul
-
 #define DRIVER_NAME		"rtc_s32cc"
 #define DRIVER_VERSION	"0.1"
+#define ENABLE_WAKEUP	1
 
 /**
  * struct rtc_s32cc_priv - RTC driver private data
  * @rtc_base: rtc base address
  * @dt_irq_id: rtc interrupt id
  * @res: rtc resource
+ * @rtc_s32cc_kobj: sysfs kernel object
+ * @rtc_s32cc_attr: sysfs command attributes
+ * @pdev: platform device structure
  */
 struct rtc_s32cc_priv {
 	u8 __iomem *rtc_base;
 	int dt_irq_id;
 	struct resource *res;
+	struct kobject *rtc_s32cc_kobj;
+	struct kobj_attribute rtc_s32cc_attr;
+	struct platform_device *pdev;
+	struct rtc_device *rdev;
 };
 
 static void print_rtc(struct platform_device *pdev)
@@ -90,9 +97,67 @@ static irqreturn_t rtc_handler(int irq, void *dev)
 	return IRQ_HANDLED;
 }
 
+static int s32cc_rtc_read_time(struct device *dev, struct rtc_time *tm)
+{
+	/* Dummy reading so we appease rtc_valid_tm(); note that this means
+	 * we won't have a monotonic timestamp, in case someone wants to use
+	 * this RTC as the system timer.
+	 */
+	static struct rtc_time stm = {
+		.tm_year = 118,	/* 2018 */
+		.tm_mon = 7,	/* August */
+		.tm_mday = 10,
+		.tm_hour = 18,
+	};
+
+	if (!tm)
+		return -EINVAL;
+	*tm = stm;
+
+	return 0;
+}
+
+static int s32cc_rtc_set_time(struct device *dev, struct rtc_time *t)
+{
+	return 0;
+}
+
+static int s32cc_rtc_read_alarm(struct device *dev, struct rtc_wkalrm *t)
+{
+	return 0;
+}
+
+static int s32cc_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *t)
+{
+	return 0;
+}
+
+static int s32cc_alarm_irq_enable(struct device *dev, unsigned int enabled)
+{
+	struct rtc_s32cc_priv *priv = dev_get_drvdata(dev);
+	u32 rtcc_val;
+
+	if (!priv->dt_irq_id)
+		return -EIO;
+
+	rtcc_val = ioread32(priv->rtc_base + RTCC_OFFSET);
+	iowrite32(rtcc_val | RTCC_CNTEN | RTCC_RTCIE, priv->rtc_base + RTCC_OFFSET);
+
+	rtcc_val = ioread32(priv->rtc_base + RTCC_OFFSET);
+
+	return (rtcc_val & RTCC_RTCIE) ? 0 : -EFAULT;
+}
+
+static const struct rtc_class_ops s32cc_rtc_ops = {
+	.read_time = s32cc_rtc_read_time,
+	.set_time = s32cc_rtc_set_time,
+	.read_alarm = s32cc_rtc_read_alarm,
+	.set_alarm = s32cc_rtc_set_alarm,
+	.alarm_irq_enable = s32cc_alarm_irq_enable,
+};
+
 static int s32cc_rtc_probe(struct platform_device *pdev)
 {
-	u32 rtcc_val;
 	struct rtc_s32cc_priv *priv = NULL;
 	struct device_node *rtc_node;
 	int err = 0;
@@ -109,7 +174,7 @@ static int s32cc_rtc_probe(struct platform_device *pdev)
 	if (!priv->res) {
 		dev_err(&pdev->dev, "Failed to get resource");
 		err = -ENOMEM;
-		goto err_free_priv_data;
+		goto err_platform_get_resource;
 	}
 
 	priv->rtc_base = devm_ioremap(&pdev->dev,
@@ -119,16 +184,32 @@ static int s32cc_rtc_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Failed to map IO address 0x%016llx\n",
 			priv->res->start);
 		err = -ENOMEM;
-		goto err_release_res;
+		goto err_ioremap_nocache;
 	}
 	dev_dbg(&pdev->dev, "RTC successfully mapped to 0x%p\n",
 		priv->rtc_base);
+
+	priv->rdev = devm_rtc_device_register(&pdev->dev, "s32cc_rtc",
+					      &s32cc_rtc_ops, THIS_MODULE);
+	if (IS_ERR_OR_NULL(priv->rdev)) {
+		dev_err(&pdev->dev, "devm_rtc_device_register error %ld\n",
+			PTR_ERR(priv->rdev));
+		err = -ENXIO;
+		goto err_devm_rtc_device_register;
+	}
+
+	err = device_init_wakeup(&pdev->dev, ENABLE_WAKEUP);
+	if (err) {
+		dev_err(&pdev->dev, "device_init_wakeup err %d\n", err);
+		err = -ENXIO;
+		goto err_device_init_wakeup;
+	}
 
 	rtc_node = of_find_compatible_node(NULL, NULL, "nxp,s32cc-rtc");
 	if (!rtc_node) {
 		dev_err(&pdev->dev, "Unable to find RTC node\n");
 		err = -ENXIO;
-		goto err_release_res;
+		goto err_of_find_compatible;
 	}
 
 	priv->dt_irq_id = of_irq_get(rtc_node, 0);
@@ -136,7 +217,7 @@ static int s32cc_rtc_probe(struct platform_device *pdev)
 
 	if (priv->dt_irq_id <= 0) {
 		err = -ENXIO;
-		goto err_release_res;
+		goto err_of_irq_get;
 	}
 
 	platform_set_drvdata(pdev, priv);
@@ -147,21 +228,22 @@ static int s32cc_rtc_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Request interrupt %d failed\n",
 			priv->dt_irq_id);
 		err = -ENXIO;
-		goto err_release_res;
+		goto err_devm_request_irq;
 	}
 
-	iowrite32(RTCVAL_VALUE, priv->rtc_base + RTCVAL_OFFSET);
-	rtcc_val = ioread32(priv->rtc_base + RTCC_OFFSET);
-	iowrite32(rtcc_val | RTCC_CNTEN | RTCC_RTCIE, priv->rtc_base + RTCC_OFFSET);
-
+	priv->pdev = pdev;
 	print_rtc(pdev);
 
 	return 0;
 
-err_release_res:
+err_devm_request_irq:
+err_of_irq_get:
+err_of_find_compatible:
+err_device_init_wakeup:
+err_devm_rtc_device_register:
+err_ioremap_nocache:
 	release_resource(priv->res);
-err_free_priv_data:
-	kfree(priv);
+err_platform_get_resource:
 	return err;
 }
 
@@ -174,7 +256,6 @@ static void s32cc_rtc_remove(struct platform_device *pdev)
 	iowrite32(rtcc_val & (~RTCC_CNTEN), priv->rtc_base + RTCC_OFFSET);
 
 	release_resource(priv->res);
-	kfree(priv);
 
 	dev_info(&pdev->dev, "Removed successfully\n");
 }
