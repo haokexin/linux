4ddd557a5307eda7e408d20ba8900ab0ae703270
Roger Quadros <rogerq@kernel.org>
2022-09-08 14:31:45 +0300

    net: ethernet: ti: am65-cpsw-nuss: Fix module reloading
    commit c65624ac2bb3af48cda37f42c810770740cf2c23 from
    git://git.ti.com/ti-linux-kernel/ti-linux-kernel.git
    
    If module is removed and re-loaded it fails complaining that
    a debugfs entry is already present. This is becase we fail to
    remove the debugfs entry at module removal. Fix this.
    
    Fixes: a986420e81a7 ("HACK: net: ethernet: ti: am65-cpsw-nuss: add debugfs to cfg cut-thru params")
    Signed-off-by: Roger Quadros <rogerq@kernel.org>
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Tested-by: Vibhore Vardhan <vibhore@ti.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/net/ethernet/ti/am65-cpsw-nuss.c
+++ drivers/net/ethernet/ti/am65-cpsw-nuss.c
@@ -3079,6 +3079,7 @@ static int am65_cpsw_nuss_remove(struct platform_device *pdev)
 		return ret;
 	}
 
+	am65_cpsw_nuss_unregister_debugfs(common);
 	am65_cpsw_nuss_phylink_cleanup(common);
 	am65_cpsw_unregister_devlink(common);
 	am65_cpsw_unregister_notifiers(common);
