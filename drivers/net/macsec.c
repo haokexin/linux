c1d21d971c6e761db8582aa6c8ab5c39f25cc52c
Antoine Tenart <atenart@kernel.org>
2022-09-21 15:51:18 +0200

    net: macsec: remove the prepare flag from the MACsec offloading context
    commit 7123f00f1562e5707f5cd71ade9e130dba37f79f from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Now that the MACsec offloading preparation phase was removed from the
    MACsec core implementation as well as from drivers implementing it, we
    can safely remove the flag representing it.
    
    Change-Id: I060f44202052c851796928d47a38678690064e02
    Signed-off-by: Antoine Tenart <atenart@kernel.org>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/96387
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/macsec.c
+++ drivers/net/macsec.c
@@ -1616,7 +1616,6 @@ static int macsec_offload(int (* const func)(struct macsec_context *),
 	if (ctx->offload == MACSEC_OFFLOAD_PHY)
 		mutex_lock(&ctx->phydev->lock);
 
-	ctx->prepare = false;
 	ret = (*func)(ctx);
 
 	if (ctx->offload == MACSEC_OFFLOAD_PHY)
