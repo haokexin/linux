65a8a573075f3080a63d0b9d57552d6f7300bbdd
Amit Singh Tomar <amitsinght@marvell.com>
2022-10-21 21:50:40 +0530

    perf/marvell: get ddr speed from "marvell,ddr-speed"
    commit c07f8311144b0ff238f7aefac65a2e754e3408dc from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    At the moment, ddr speed is fetched from "octeontx_brd"
    property which is updated from U-boot (for ddr speed), but
    this would cause an issue when booting via UEFI (instead
    of U-boot), and Kernel in this case complain about missing
    ddr speed.
    
    To fix it, let's fetch ddr speed from "marvell,ddr-speed"
    property that is updated from ATF.
    
    Signed-off-by: Amit Singh Tomar <amitsinght@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/88784
    Change-Id: Ie9b2c436b3bdfc7cba2754a772c13bc19b6112de
    Signed-off-by: Gowthami Thiagarajan <gthiagarajan@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/perf/marvell_cn10k_ddr_pmu.c
+++ drivers/perf/marvell_cn10k_ddr_pmu.c
@@ -132,10 +132,9 @@
 #define CUST_WINDOW			0xA0C
 
 #define MBW_BASE			0x240000
-#define OCTTX_NODE			"octeontx_brd"
 
 bool is_probed_once;
-long ddr_speed;
+u32 ddr_speed;
 
 struct cn10k_ddr_pmu {
 	struct pmu pmu;
@@ -715,33 +714,22 @@ static int cn10k_ddr_pmu_offline_cpu(unsigned int cpu, struct hlist_node *node)
 	return 0;
 }
 
-static int cn10k_ddr_get_speed(void)
+static int cn10k_ddr_get_speed(struct device *dev)
 {
-	struct device_node *np = NULL;
-	const char *speed;
 	int ret;
 
-	np = of_find_node_by_name(NULL, OCTTX_NODE);
-	if (!np) {
-		pr_err("No board info available!\n");
-		return -ENODEV;
-	}
-
-	ret = of_property_read_string(np, "DDR-SPEED", &speed);
+	ret = device_property_read_u32(dev, "marvell,ddr-speed", &ddr_speed);
 	if (ret) {
-		pr_err("DDR-SPEED property not found\n");
+		pr_err("marvell,ddr-speed property not found\n");
 		return ret;
 	}
-
-	ret = kstrtol(speed, 0, &ddr_speed);
-	if (ret < 0)
-		return ret;
 
 	return 0;
 }
 
 static int cn10k_ddr_perf_probe(struct platform_device *pdev)
 {
+	struct device *dev = &pdev->dev;
 	struct cn10k_ddr_pmu *ddr_pmu;
 	struct resource *res;
 	void __iomem *base;
@@ -763,7 +751,7 @@ static int cn10k_ddr_perf_probe(struct platform_device *pdev)
 	ddr_pmu->mbw_base = ioremap(res->start + MBW_BASE, resource_size(res));
 
 	if (!is_probed_once) {
-		if (cn10k_ddr_get_speed())
+		if (cn10k_ddr_get_speed(dev))
 			pr_err("Couldn't fetch speed for %s\n",
 				pdev->name);
 		is_probed_once = true;
