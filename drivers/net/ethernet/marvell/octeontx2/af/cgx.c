2a98237c593240d5c3ea463314b9366b2004d8c2
Hariprasad Kelam <hkelam@marvell.com>
2022-11-21 11:54:43 +0530

    octeontx2-af: Gracefully skip the cgx_probe for unmapped devices.
    commit db039de9186b7ef5fee38a5fc9bff6b6046a075a from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Pci probe failed error messages get displayed for cgx devices not
    mapped to NIX as cgx_probe return -ENOMEM. This error message
    mislead the user. Hence avoild it by return 0 instead of -ENOMEM.
    
    Change-Id: I06316a20e977934f82d6028f6b1375585dff0f0f
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cgx.c
+++ drivers/net/ethernet/marvell/octeontx2/af/cgx.c
@@ -2076,8 +2076,8 @@ static struct mac_ops	cgx_mac_ops    = {
 static int cgx_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
 	struct device *dev = &pdev->dev;
+	int err = 0, nvec;
 	struct cgx *cgx;
-	int err, nvec;
 
 	cgx = devm_kzalloc(dev, sizeof(*cgx), GFP_KERNEL);
 	if (!cgx)
@@ -2180,10 +2180,11 @@ static void cgx_remove(struct pci_dev *pdev)
 {
 	struct cgx *cgx = pci_get_drvdata(pdev);
 
-	if (cgx) {
-		cgx_lmac_exit(cgx);
-		list_del(&cgx->cgx_list);
-	}
+	if (!cgx)
+		return;
+
+	cgx_lmac_exit(cgx);
+	list_del(&cgx->cgx_list);
 	pci_free_irq_vectors(pdev);
 	pci_release_regions(pdev);
 	pci_disable_device(pdev);
