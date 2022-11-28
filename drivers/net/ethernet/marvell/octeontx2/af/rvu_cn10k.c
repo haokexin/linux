70e92400d9e9d865f55cdbeee5a14d8092a1c12e
Pavan Nikhilesh <pbhagavatula@marvell.com>
2022-11-28 16:22:59 +0530

    octeontx2-af: cn10k: increase outstanding LMTST ops
    commit dbfb15af053f79ea9b60238c362d82dfe64f8b08 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Increase the number of outstanding store transactions issued by AP as
    a part of LMTST from default value of 1 to 16
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Change-Id: Ie950e9bb87412c21893704eed6d0f69d48118d49
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_cn10k.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_cn10k.c
@@ -569,3 +569,12 @@ void rvu_nix_block_cn10k_init(struct rvu *rvu, struct nix_hw *nix_hw)
 	cfg |= BIT_ULL(2);
 	rvu_write64(rvu, blkaddr, NIX_AF_CFG, cfg);
 }
+
+void rvu_apr_block_cn10k_init(struct rvu *rvu)
+{
+	u64 reg;
+
+	reg = rvu_read64(rvu, BLKADDR_APR, APR_AF_LMT_CFG);
+	reg |= 0xFULL << 35;
+	rvu_write64(rvu, BLKADDR_APR, APR_AF_LMT_CFG, reg);
+}
