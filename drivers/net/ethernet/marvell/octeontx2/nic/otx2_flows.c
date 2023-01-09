d3bda21f6d0783593d16c033fde500536a764217
Suman Ghosh <sumang@marvell.com>
2023-01-09 11:26:21 +0530

    octeontx2-pf: Add additional check for MCAM rules.
    commit fa4e01841084f99b23c96ddf740fb7ce7cc4abf5 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    MCAM drop rule with ether_type==802.1Q and vlan_id==0 is not supported.
    
    Signed-off-by: Suman Ghosh <sumang@marvell.com>
    Change-Id: I6d848102b76229c8deb8eded88fd9cbeeb40718e
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/94272
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_flows.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_flows.c
@@ -893,6 +893,13 @@ static int otx2_prepare_flow_request(struct ethtool_rx_flow_spec *fsp,
 			if (be16_to_cpu(fsp->m_ext.vlan_etype) != 0xFFFF)
 				return -EINVAL;
 
+			/* Drop rule with vlan_etype==802.1Q and
+			 * vlan_id==0 is not supported
+			 */
+			if (vlan_etype == ETH_P_8021Q && !fsp->m_ext.vlan_tci &&
+			    fsp->ring_cookie == RX_CLS_FLOW_DISC)
+				return -EINVAL;
+
 			vlan_etype = be16_to_cpu(fsp->h_ext.vlan_etype);
 			/* Only ETH_P_8021Q and ETH_P_802AD types supported */
 			if (vlan_etype != ETH_P_8021Q &&
