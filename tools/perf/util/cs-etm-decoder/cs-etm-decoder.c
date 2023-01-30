cf6e98bacd0b03ff6e8e4b40c4ccd8a6036a8891
Tanmay Jagdale <tanmay@marvell.com>
2023-01-29 22:08:36 -0800

    perf: cs-etm: Pass timestamp to perf front end
    commit 32ecfa39586c94868f21a1c8273b861708e74210 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    The timestamps recorded by Coresight were not being passed to
    the perf's front end during branch/instruction synthesis.
    Fix this by passing the timestamp value recorded in the tidq.
    
    Also, disable all software estimation of timestamps since
    it was causing an issue where the estimation logic had
    moved the time ahead, but the next timestamp from hardware
    was same as previous.
    
    Change-Id: I45f8b4d835e4fb48dee3b7dd2aab137885ee6bf5
    Signed-off-by: Tanmay Jagdale <tanmay@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/95678
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/102807
    Reviewed-by: Linu Cherian <lcherian@marvell.com>
    Tested-by: Linu Cherian <lcherian@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- tools/perf/util/cs-etm-decoder/cs-etm-decoder.c
+++ tools/perf/util/cs-etm-decoder/cs-etm-decoder.c
@@ -266,8 +266,12 @@ cs_etm_decoder__do_soft_timestamp(struct cs_etm_queue *etmq,
 
 	packet_queue->cs_timestamp = packet_queue->next_cs_timestamp;
 
-	/* Estimate the timestamp for the next range packet */
-	packet_queue->next_cs_timestamp += packet_queue->instr_count;
+	/* Don't update the timestamp here in SW since this
+	 * wouldn't represent an actual timestamp. It could also
+	 * lead to an issue where the cs_timestamp value is moved
+	 * beyond next actual HW timestamp or when the successive
+	 * HW timestamp is same.
+	 */
 	packet_queue->instr_count = 0;
 
 	/* Tell the front end which traceid_queue needs attention */
@@ -299,7 +303,6 @@ cs_etm_decoder__do_hard_timestamp(struct cs_etm_queue *etmq,
 		return OCSD_RESP_CONT;
 	}
 
-
 	if (!elem->timestamp) {
 		/*
 		 * Zero timestamps can be seen due to misconfiguration or hardware bugs.
@@ -327,7 +330,7 @@ cs_etm_decoder__do_hard_timestamp(struct cs_etm_queue *etmq,
 		 * which instructions started by subtracting the number of instructions
 		 * executed to the timestamp.
 		 */
-		packet_queue->cs_timestamp = elem->timestamp - packet_queue->instr_count;
+		packet_queue->cs_timestamp = elem->timestamp;
 	}
 	packet_queue->next_cs_timestamp = elem->timestamp;
 	packet_queue->instr_count = 0;
