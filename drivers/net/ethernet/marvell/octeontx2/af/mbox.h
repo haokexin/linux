47ff4ec0d159bd6c91e4785897c29ac8ea72cf66
Hariprasad Kelam <hkelam@marvell.com>
2023-01-31 13:04:05 +0530

    octeontx2-pf: Generate PPS OUT on PHC PTP_CLK_REQ_PEROUT
    commit 331ee9539f95dbfcfab1f9a2babf30b6aeafcdbf from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    PTP Hw block can output a clock sourced from internal PTP clock
    to GPIO output. This patch adds support to enable/disable this
    feature.
    
    User can enable/disable PPS by writing to the below sysfs entry
    
    echo perout.index start.sec start.nsec period.sec period.nsec >
    /sys/class/ptp/ptp0/period
    
    Change-Id: I71a4b241b791f1690e654639432ae9ed6809ed46
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/95669
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/mbox.h
+++ drivers/net/ethernet/marvell/octeontx2/af/mbox.h
@@ -1975,7 +1975,7 @@ enum ptp_op {
 	PTP_OP_GET_CLOCK = 1,
 	PTP_OP_GET_TSTMP = 2,
 	PTP_OP_SET_THRESH = 3,
-	PTP_OP_EXTTS_ON = 4,
+	PTP_OP_PPS_ON = 4,
 };
 
 struct ptp_req {
@@ -1984,7 +1984,8 @@ struct ptp_req {
 	s64 scaled_ppm;
 	u8 is_pmu;
 	u64 thresh;
-	int extts_on;
+	u64 period;
+	int pps_on;
 };
 
 struct ptp_rsp {
