ffd58e5e1d7f7f69266918e82fa877073c455652
Suman Ghosh <sumang@marvell.com>
2023-03-22 16:12:35 +0530

    Octeontx2-af: Install TC filter rules in hardware based on priority
    commit c60302d90dc93ff1b11c53f4482e3f099c2f83d0 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    As of today, hardware does not support installing tc filter
    rules based on priority. This patch fixes the issue and install
    the hardware rules based on priority. The final hardware rules
    will not be dependent on rule installation order, it will be strictly
    priority based, same as software.
    
    Signed-off-by: Suman Ghosh <sumang@marvell.com>
    Change-Id: Idc513d1a9b7ea6e79a77bb8a6622a819c2a4c02c
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/100130
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.h
@@ -359,13 +359,8 @@ struct otx2_flow_config {
 	u32			*bmap_to_dmacindex;
 	unsigned long		*dmacflt_bmap;
 	struct list_head	flow_list;
-};
-
-struct otx2_tc_info {
-	/* hash table to store TC offloaded flows */
-	struct rhashtable		flow_table;
-	struct rhashtable_params	flow_ht_params;
-	unsigned long			*tc_entries_bitmap;
+	struct list_head	flow_list_tc;
+	bool			ntuple;
 };
 
 struct dev_hw_ops {
@@ -491,7 +486,6 @@ struct otx2_nic {
 	/* NPC MCAM */
 	struct otx2_flow_config	*flow_cfg;
 	struct otx2_mac_table	*mac_table;
-	struct otx2_tc_info	tc_info;
 
 	u64			reset_count;
 	struct work_struct	reset_task;
@@ -975,7 +969,7 @@ static inline int otx2_tc_flower_rule_cnt(struct otx2_nic *pfvf)
 	if (!pfvf->flow_cfg)
 		return 0;
 
-	return bitmap_weight(pfvf->tc_info.tc_entries_bitmap, pfvf->flow_cfg->max_flows);
+	return pfvf->flow_cfg->nr_flows;
 }
 
 static inline int otx2_is_ntuple_rule_installed(struct otx2_nic *pfvf)
@@ -983,7 +977,7 @@ static inline int otx2_is_ntuple_rule_installed(struct otx2_nic *pfvf)
 	if (!pfvf->flow_cfg)
 		return false;
 
-	return (!otx2_tc_flower_rule_cnt(pfvf) && pfvf->flow_cfg->nr_flows);
+	return pfvf->flow_cfg->nr_flows;
 }
 
 /* MSI-X APIs */
@@ -1108,7 +1102,6 @@ int otx2_init_tc(struct otx2_nic *nic);
 void otx2_shutdown_tc(struct otx2_nic *nic);
 int otx2_setup_tc(struct net_device *netdev, enum tc_setup_type type,
 		  void *type_data);
-int otx2_tc_alloc_ent_bitmap(struct otx2_nic *nic);
 /* CGX/RPM DMAC filters support */
 int otx2_dmacflt_get_max_cnt(struct otx2_nic *pf);
 int otx2_dmacflt_add(struct otx2_nic *pf, const u8 *mac, u32 bit_pos);
