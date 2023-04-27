0dd40aabb2857584fdf3ab615a4bb5aa3d1fd04a
Suman Ghosh <sumang@marvell.com>
2023-04-27 12:31:28 +0530

    octeontx2: Fix klockwork issues.
    commit bf875cc90b183dff94640609cfe17b4e53bd672d from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Fix all klockwork issues reported on AF and PF/VF driver.
    
    Signed-off-by: Suman Ghosh <sumang@marvell.com>
    Change-Id: I29d2b27c2e82c36b05f4386b97b58394993e88f6
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/102540
    Reviewed-by: Bharat Bhushan <bbhushan2@marvell.com>
    Reviewed-by: Linu Cherian <lcherian@marvell.com>
    Tested-by: Linu Cherian <lcherian@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/ptp.c
+++ drivers/net/ethernet/marvell/octeontx2/af/ptp.c
@@ -439,6 +439,7 @@ static int ptp_probe(struct pci_dev *pdev,
 {
 	struct device *dev = &pdev->dev;
 	struct ptp *ptp;
+	void __iomem * const *base;
 	int err;
 
 	ptp = devm_kzalloc(dev, sizeof(*ptp), GFP_KERNEL);
@@ -457,7 +458,15 @@ static int ptp_probe(struct pci_dev *pdev,
 	if (err)
 		goto error_free;
 
-	ptp->reg_base = pcim_iomap_table(pdev)[PCI_PTP_BAR_NO];
+	base = pcim_iomap_table(pdev);
+	if (!base)
+		goto error_free;
+
+	ptp->reg_base = base[PCI_PTP_BAR_NO];
+	if (!ptp->reg_base) {
+		err = -ENODEV;
+		goto error_free;
+	}
 
 	pci_set_drvdata(pdev, ptp);
 	if (!first_ptp_block)
