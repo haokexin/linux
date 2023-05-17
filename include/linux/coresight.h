a7aaed0a11cfc5e7125f86c5957307299f772320
Linu Cherian <lcherian@marvell.com>
2023-05-17 18:18:03 +0530

    coresight: core: Add provision for panic callbacks
    commit 2a9445400128c2df7b9c8f8e83007d87d5be46a5 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Panic callback handlers allows coresight device drivers to sync
    relevant trace data and trace metadata to reserved memory
    regions so that they can be retrieved later in the subsequent
    boot or in the crashdump kernel.
    
    Change-Id: I36ac3f2e8c77571c9224e955892e6e988ac862af
    Signed-off-by: Linu Cherian <lcherian@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/104420
    Reviewed-by: Anil Kumar Reddy H <areddy3@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- include/linux/coresight.h
+++ include/linux/coresight.h
@@ -276,6 +276,7 @@ static struct coresight_dev_list (var) = {				\
 #define link_ops(csdev)		csdev->ops->link_ops
 #define helper_ops(csdev)	csdev->ops->helper_ops
 #define ect_ops(csdev)		csdev->ops->ect_ops
+#define panic_ops(csdev)	csdev->ops->panic_ops
 
 /**
  * struct coresight_ops_sink - basic operations for a sink
@@ -353,12 +354,22 @@ struct coresight_ops_ect {
 	int (*disable)(struct coresight_device *csdev);
 };
 
+/**
+ * struct coresight_ops_panic - Generic device ops for panic handing
+ *
+ * @sync	: Sync the device register state/trace data
+ */
+struct coresight_ops_panic {
+	int (*sync)(struct coresight_device *csdev);
+};
+
 struct coresight_ops {
 	const struct coresight_ops_sink *sink_ops;
 	const struct coresight_ops_link *link_ops;
 	const struct coresight_ops_source *source_ops;
 	const struct coresight_ops_helper *helper_ops;
 	const struct coresight_ops_ect *ect_ops;
+	const struct coresight_ops_panic *panic_ops;
 };
 
 #if IS_ENABLED(CONFIG_CORESIGHT)
