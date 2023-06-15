54a4e02b2405b9f287b8f6b51c54a121ed88fdc2
Dinh Nguyen <dinh.nguyen@linux.intel.com>
2023-06-15 14:07:43 -0500

    HSD 14019646348-2: add error checking when getting "platform"
    commit 92ea2e6634a737500212c5b3bda0255669da8cb7 from
    https://github.com/altera-opensource/linux-socfpga.git socfpga-6.1.55-lts.
    
    The call to of_property_read_string to get the platform name can return
    an error if the "platform" is not in the DTB, which will result in
    a kernel panic. Add the error checking around of_property_read_string().
    
    Signed-off-by: Dinh Nguyen <dinh.nguyen@linux.intel.com>
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- drivers/crypto/intel_fcs_main.c
+++ drivers/crypto/intel_fcs_main.c
@@ -3800,7 +3800,11 @@ static int fcs_driver_probe(struct platform_device *pdev)
 	if (!priv->p_data)
 		return -ENODEV;
 	
-	of_property_read_string(dev->of_node, "platform", &platform);
+	ret = of_property_read_string(dev->of_node, "platform", &platform);
+	if (ret) {
+		dev_err(dev, "can't find platform");
+		return -ENODEV;
+	}
 
 	/* Proceed only if platform is agilex as
 	 * register addresses are platform specific
@@ -3896,7 +3900,9 @@ static int fcs_driver_remove(struct platform_device *pdev)
 	int i, ret;
 	const char *platform;
 
-	of_property_read_string(dev->of_node, "platform", &platform);
+	ret = of_property_read_string(dev->of_node, "platform", &platform);
+	if (ret)
+		goto no_platform;
 
 	if (!strncmp(platform, AGILEX_PLATFORM, AGILEX_PLATFORM_STR_LEN)) {
 		msg.command = COMMAND_SMC_SVC_VERSION;
@@ -3924,6 +3930,7 @@ static int fcs_driver_remove(struct platform_device *pdev)
 		}
 	}
 
+no_platform:
 	if (priv->p_data->have_hwrng)
 		hwrng_unregister(&priv->rng);
 	misc_deregister(&priv->miscdev);
