2b2b33a6414f1d321622daf912005699a4f1f0ed
Ratheesh Kannoth <rkannoth@marvell.com>
2023-07-07 11:07:54 +0530

    octeontx-pf: Fix Call trace' while running netdev rss test
    commit 17a68c7ff7f796c08825360a843f679738cd4cca from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
     Mistake in Backporting
    
     [DEBUG] - [Log]	 [ 1716.074854] ------------[ cut here ]------------
     [DEBUG] - [Log]	 [ 1716.079478] WARNING: CPU: 5 PID: 1557 at include/net/page_pool.h:259 page_pool_put_page+0x23c/0x260
     [DEBUG] - [Log]	 [ 1716.088534] Modules linked in: br_netfilter bridge overlay ipv6
     [DEBUG] - [Log]	 [ 1716.094467] CPU: 5 PID: 1557 Comm: ethtool Not tainted 5.15.72 #1
     [DEBUG] - [Log]	 [ 1716.100561] Hardware name: Marvell OcteonTX Loki board (DT)
     [DEBUG] - [Log]	 [ 1716.106131] pstate: a0400009 (NzCv daif +PAN -UAO -TCO -DIT -SSBS BTYPE=--)
     [DEBUG] - [Log]	 [ 1716.113092] pc : page_pool_put_page+0x23c/0x260
     [DEBUG] - [Log]	 [ 1716.117623] lr : otx2_free_bufs+0x88/0xd4
     [DEBUG] - [Log]	 [ 1716.121634] sp : ffff0001030ef7f0
     [DEBUG] - [Log]	 [ 1716.124946] x29: ffff0001030ef7f0 x28: 000000000000000c x27: ffff000101cb3e40
     [DEBUG] - [Log]	 [ 1716.132086] x26: 0000000000000140 x25: ffff0001051e4980 x24: 00000000ffffffff
     [DEBUG] - [Log]	 [ 1716.139227] x23: ffff000103976c80 x22: 0000000000000880 x21: 0000000000000001
     [DEBUG] - [Log]	 [ 1716.146369] x20: ffffffc000000000 x19: ffff0001b8d42000 x18: 0000000000000000
     [DEBUG] - [Log]	 [ 1716.153510] x17: 0000000000000000 x16: 000000000000ffff x15: 0000000000000000
     [DEBUG] - [Log]	 [ 1716.160651] x14: 0000000000000004 x13: 0000000000000001 x12: 0000000000000008
     [DEBUG] - [Log]	 [ 1716.167790] x11: 0000000000000001 x10: 0000000000010000 x9 : 0000000000000002
     [DEBUG] - [Log]	 [ 1716.174932] x8 : 0000000000000000 x7 : ffff000104c9ab00 x6 : 000000000000000d
     [DEBUG] - [Log]	 [ 1716.182072] x5 : 0000000000000002 x4 : ffffffc000000028 x3 : ffffffffffffffff
     [DEBUG] - [Log]	 [ 1716.189213] x2 : 00000000ffffffff x1 : ffffffc000000000 x0 : 0000000000000000
     [DEBUG] - [Log]	 [ 1716.196355] Call trace:
     [DEBUG] - [Log]	 [ 1716.198798]  page_pool_put_page+0x23c/0x260
     [DEBUG] - [Log]	 [ 1716.202982]  otx2_free_bufs+0x88/0xd4
     [DEBUG] - [Log]	 [ 1716.206644]  otx2_cleanup_rx_cqes+0xc8/0x134
     [DEBUG] - [Log]	 [ 1716.210914]  otx2_free_hw_resources+0x88/0x3cc
     [DEBUG] - [Log]	 [ 1716.215357]  otx2_stop+0x1f8/0x350
     [DEBUG] - [Log]	 [ 1716.218758]  otx2_set_channels+0xcc/0x144
     [DEBUG] - [Log]	 [ 1716.222766]  ethnl_set_channels+0x188/0x380
     [DEBUG] - [Log]	 [ 1716.226950]  genl_family_rcv_msg_doit+0xdc/0x14c
     [DEBUG] - [Log]	 [ 1716.231567]  genl_rcv_msg+0xf4/0x1f0
     [DEBUG] - [Log]	 [ 1716.235142]  netlink_rcv_skb+0x5c/0x130
     [DEBUG] - [Log]	 [ 1716.238975]  genl_rcv+0x38/0x50
     [DEBUG] - [Log]	 [ 1716.242114]  netlink_unicast+0x2c8/0x324
     [DEBUG] - [Log]	 [ 1716.246035]  netlink_sendmsg+0x1b0/0x40c
     [DEBUG] - [Log]	 [ 1716.249956]  __sys_sendto+0x10c/0x170
     [DEBUG] - [Log]	 [ 1716.253619]  __arm64_sys_sendto+0x28/0x40
     [DEBUG] - [Log]	 [ 1716.257628]  invoke_syscall.constprop.0+0x50/0xf0
     [DEBUG] - [Log]	 [ 1716.262335]  do_el0_svc+0x50/0x150
     [DEBUG] - [Log]	 [ 1716.265735]  el0_svc+0x30/0x140
     [DEBUG] - [Log]	 [ 1716.268875]  el0t_64_sync_handler+0xe8/0x114
     [DEBUG] - [Log]	 [ 1716.273145]  el0t_64_sync+0x1a0/0x1a4
     [DEBUG] - [Log]	 [ 1716.276806] ---[ end trace cdb246a865eb63d5 ]---
    
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>
    Change-Id: I213fc7c935a514cfb1652179e3fe44b5f994bf32
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/107237
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    (cherry picked from commit 54bde19956b906ec988ae999dcdd5a211a39c62f)
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/107280
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_txrx.c
@@ -1240,6 +1240,7 @@ void otx2_cleanup_rx_cqes(struct otx2_nic *pfvf, struct otx2_cq_queue *cq, int q
 			otx2_free_rcv_seg(pfvf, cqe, cq->cq_idx);
 			continue;
 		}
+		iova = cqe->sg.seg_addr - OTX2_HEAD_ROOM;
 		otx2_free_bufs(pfvf, pool, iova, pfvf->rbsize);
 	}
 
