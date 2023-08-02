faa47f66fa37b4310240f9933bf25f83bf08abdf
Geetha sowjanya <gakula@marvell.com>
2023-08-02 12:51:22 +0530

    octeontx2-pf: Fix dangling pointers in the netdev qset
    commit f7b4c3a49dab0b1734a4b61f72ec238a5aed6df7 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    If the otx2_open() fails RQ/SQ qset memory is freed
    making the RQ/SQ dangling pointers.
    Accessing this pointers from get_ethtool_stats()
    causing driver crash.
    This patch fixes the issue by make pointers to NULL.
    
    Fixes: d45d8979840d ("octeontx2-pf: Add basic ethtool support")
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Change-Id: Id68c22de83d5696ad20de8fc225101aeb754ea06
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/109068
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -1786,6 +1786,17 @@ static void otx2_dim_work(struct work_struct *w)
 	dim->state = DIM_START_MEASURE;
 }
 
+static void otx2_free_queue_mem(struct otx2_qset *qset)
+{
+	kfree(qset->sq);
+	qset->sq = NULL;
+	kfree(qset->cq);
+	qset->cq = NULL;
+	kfree(qset->rq);
+	qset->rq = NULL;
+	kfree(qset->napi);
+}
+
 int otx2_open(struct net_device *netdev)
 {
 	struct otx2_nic *pf = netdev_priv(netdev);
@@ -1991,10 +2002,7 @@ err_disable_napi:
 	otx2_disable_napi(pf);
 	otx2_free_hw_resources(pf);
 err_free_mem:
-	kfree(qset->sq);
-	kfree(qset->cq);
-	kfree(qset->rq);
-	kfree(qset->napi);
+	otx2_free_queue_mem(qset);
 	return err;
 }
 EXPORT_SYMBOL(otx2_open);
@@ -2060,10 +2068,8 @@ int otx2_stop(struct net_device *netdev)
 		cancel_delayed_work_sync(&pf->refill_wrk[wrk].pool_refill_work);
 	devm_kfree(pf->dev, pf->refill_wrk);
 
-	kfree(qset->sq);
-	kfree(qset->cq);
-	kfree(qset->rq);
-	kfree(qset->napi);
+	otx2_free_queue_mem(qset);
+
 	/* Do not clear RQ/SQ ringsize settings */
 	memset((void *)qset + offsetof(struct otx2_qset, sqe_cnt), 0,
 	       sizeof(*qset) - offsetof(struct otx2_qset, sqe_cnt));
