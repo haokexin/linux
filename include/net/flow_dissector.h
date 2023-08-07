0fe64baabf3d12013e8a14a17b1a74ba0760e1f8
Ratheesh Kannoth <rkannoth@marvell.com>
2023-08-07 17:56:09 +0530

    net: flow_dissector: Add IPSEC dissector
    commit 00c8c0bd92e72ab977a92629273f5f9ac3b1c991 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Support for dissecting IPSEC field SPI (which is
    32bits in size) for ESP and AH packets.
    
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>
    Change-Id: I9d654f99abd55cea6f282b4304aec459f56eebdd
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/109513
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- include/net/flow_dissector.h
+++ include/net/flow_dissector.h
@@ -253,6 +253,14 @@ struct flow_dissector_key_hash {
 	u32 hash;
 };
 
+/**
+ * struct flow_dissector_key_ipsec:
+ * @spi: identifier for a ipsec connection
+ */
+struct flow_dissector_key_ipsec {
+	__be32 spi;
+};
+
 enum flow_dissector_key_id {
 	FLOW_DISSECTOR_KEY_CONTROL, /* struct flow_dissector_key_control */
 	FLOW_DISSECTOR_KEY_BASIC, /* struct flow_dissector_key_basic */
@@ -282,6 +290,7 @@ enum flow_dissector_key_id {
 	FLOW_DISSECTOR_KEY_META, /* struct flow_dissector_key_meta */
 	FLOW_DISSECTOR_KEY_CT, /* struct flow_dissector_key_ct */
 	FLOW_DISSECTOR_KEY_HASH, /* struct flow_dissector_key_hash */
+	FLOW_DISSECTOR_KEY_IPSEC, /* struct flow_dissector_key_ipsec */
 
 	FLOW_DISSECTOR_KEY_MAX,
 };
