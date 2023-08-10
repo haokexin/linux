2ef29b2cc90a8b939ab165996fa12aa8375787c6
Ratheesh Kannoth <rkannoth@marvell.com>
2023-08-10 07:52:06 +0530

    net: flow_dissector: Use 64bits for used_keys
    commit 1c82bb03593cb9152afec82db145517a71b3cffc from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    As 32bits of dissector->used_keys are exhausted,
    increase the size to 64bits.
    
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>
    Change-Id: I4defafd2f296f322e709535da2a7240ce0c41855
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/109512
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    [RQ: Replace BIT() with BIT_ULL() in some ipv6 cases.]
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- net/netfilter/nf_tables_offload.c
+++ net/netfilter/nf_tables_offload.c
@@ -35,12 +35,12 @@ void nft_flow_rule_set_addr_type(struct nft_flow_rule *flow,
 	struct nft_flow_key *mask = &match->mask;
 	struct nft_flow_key *key = &match->key;
 
-	if (match->dissector.used_keys & BIT(FLOW_DISSECTOR_KEY_CONTROL))
+	if (match->dissector.used_keys & BIT_ULL(FLOW_DISSECTOR_KEY_CONTROL))
 		return;
 
 	key->control.addr_type = addr_type;
 	mask->control.addr_type = 0xffff;
-	match->dissector.used_keys |= BIT(FLOW_DISSECTOR_KEY_CONTROL);
+	match->dissector.used_keys |= BIT_ULL(FLOW_DISSECTOR_KEY_CONTROL);
 	match->dissector.offset[FLOW_DISSECTOR_KEY_CONTROL] =
 		offsetof(struct nft_flow_key, control);
 }
