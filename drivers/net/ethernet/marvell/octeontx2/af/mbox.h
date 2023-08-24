b8970388e3ef62a5572e4ca4df365d9a9a969075
Hariprasad Kelam <hkelam@marvell.com>
2023-08-24 12:52:14 +0530

    octeontx2-pf: Tc flower offload support for MPLS
    commit 2720a510204eb332db5a3610192a6eea84793390 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch extends flower offload support for MPLS protocol.
    Though with tc framework user can match up to 7 mpls headers,
    due to hardware limitation (kpu parsing) currently driver
    supports match up to 4 mpls headers.
    
    Example command:
    - to match outer mpls header
      tc filter add dev eth0 ingress protocol mpls_uc flower mpls_label 18
      mpls_ttl 255 mpls_bos 1 mpls_tc 7 action drop
    - to match mpls header 1,4
      tc filter add dev eth0 ingress protocol mpls_uc flower mpls lse
      depth 1 label 100 tc 2 ttl 64 lse depth 4 label 101 bos 1 action drop
    - to match mpls header 1,2,3,4
      tc filter add dev eth0 ingress protocol mpls_uc flower mpls lse
      depth1 label 100 lse depth 2 label 101 lse depth 3 label 102 lse depth 4
      label 103 bos 1 action drop
    
    Change-Id: I33cbbf4d743a1837a52a84e949b846a1ef3fce3d
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/111278
    Reviewed-by: Suman Ghosh
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/mbox.h
+++ drivers/net/ethernet/marvell/octeontx2/af/mbox.h
@@ -1802,6 +1802,12 @@ struct flow_msg {
 	__be16 vlan_itci;
 	__be32 gtpu_teid;
 	__be32 gtpc_teid;
+#define OTX2_FLOWER_MASK_MPLS_LB		GENMASK(31, 12)
+#define OTX2_FLOWER_MASK_MPLS_TC		GENMASK(11, 9)
+#define OTX2_FLOWER_MASK_MPLS_BOS		BIT(8)
+#define OTX2_FLOWER_MASK_MPLS_TTL		GENMASK(7, 0)
+#define OTX2_FLOWER_MASK_MPLS_NON_TTL		GENMASK(31, 8)
+	__be32 mpls_lse[4];
 };
 
 struct npc_install_flow_req {
