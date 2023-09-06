909457515f8f5595eea9ec50e9f032b77a1e7588
Linu Cherian <lcherian@marvell.com>
2023-09-06 10:40:23 +0530

    irqchip/gic-v3: Add workaround for interrupt loss on IPI
    commit ab7b49c65b1c22228a8d80215964df39bb8b269b from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    RCU stalls has been observed during stress-ng tests on
    OcteonTx2 silicon and the reason being an interrupt loss on an IPI.
    
    As a workaround, added provision to ensure that every IPI has been
    received on the destination core and if an IPI interrupt loss is
    encountered, do resend the IPI.
    
    Identifying a lost interrupt:
    Once an interrupt is raised on an IPI line, it would be either in
    one of the below states.
       A. pending
       B. active
       C. deactivated
    
    Pending/active states can be verified from the corresponding redist
    registers. An interrupt would be in deactivated state either because its
    already processed or its been lost. So to identify the reason for case C,
    per core tx and rx counters has been introduced for each IPI line.
    
    Change-Id: I84f705a0c8b611415add3a520430cef678a3a798
    Signed-off-by: Linu Cherian <lcherian@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/89369
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -24,6 +24,7 @@
 #include <linux/irqchip/arm-gic-common.h>
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/irqchip/irq-partition-percpu.h>
+#include <linux/irqchip/irq-gic-v3-fixes.h>
 #include <linux/bitfield.h>
 #include <linux/bits.h>
 #include <linux/arm-smccc.h>
@@ -73,6 +74,7 @@ static DEFINE_STATIC_KEY_FALSE(gic_arm64_2941627_erratum);
 
 static struct gic_chip_data gic_data __read_mostly;
 static DEFINE_STATIC_KEY_TRUE(supports_deactivate_key);
+static DEFINE_STATIC_KEY_FALSE(gic_ipimiss_quirk);
 
 #define GIC_ID_NR	(1U << GICD_TYPER_ID_BITS(gic_data.rdists.gicd_typer))
 #define GIC_LINE_NR	min(GICD_TYPER_SPIS(gic_data.rdists.gicd_typer), 1020U)
@@ -136,6 +138,13 @@ static DEFINE_PER_CPU(bool, has_rss);
 #define gic_data_rdist_rd_base()	(gic_data_rdist()->rd_base)
 #define gic_data_rdist_sgi_base()	(gic_data_rdist_rd_base() + SZ_64K)
 
+#define gic_data_rdist_cpu(cpu)		\
+				(per_cpu_ptr(gic_data.rdists.rdist, cpu))
+#define gic_data_rdist_rd_base_cpu(cpu)	\
+				(gic_data_rdist_cpu(cpu)->rd_base)
+#define gic_data_rdist_sgi_base_cpu(cpu)\
+				(gic_data_rdist_rd_base_cpu(cpu) + SZ_64K)
+
 /* Our default, arbitrary priority value. Linux only uses one anyway. */
 #define DEFAULT_PMR_VALUE	0xf0
 
@@ -148,6 +157,29 @@ enum gic_intid_range {
 	LPI_RANGE,
 	__INVALID_RANGE__
 };
+
+void gic_v3_enable_ipimiss_quirk(void)
+{
+	static_branch_enable(&gic_ipimiss_quirk);
+}
+
+u32 gic_rdist_pend_reg(int cpu, int offset)
+{
+	void __iomem *base;
+
+	base = gic_data_rdist_sgi_base_cpu(cpu);
+
+	return readl_relaxed(base + GICR_ISPENDR0 + offset);
+}
+
+u32 gic_rdist_active_reg(int cpu, int offset)
+{
+	void __iomem *base;
+
+	base = gic_data_rdist_sgi_base_cpu(cpu);
+
+	return readl_relaxed(base + GICR_ISACTIVER0 + offset);
+}
 
 static enum gic_intid_range __get_intid_range(irq_hw_number_t hwirq)
 {
@@ -766,6 +798,14 @@ static void __gic_handle_irq(u32 irqnr, struct pt_regs *regs)
 	if (gic_irqnr_is_special(irqnr))
 		return;
 
+	if (static_branch_unlikely(&gic_ipimiss_quirk)) {
+		if (irqnr < 16) {
+			gic_ipi_rxcount_inc(smp_processor_id(), irqnr);
+			/* Force increment is done before deactivate */
+			wmb();
+		}
+	}
+
 	gic_complete_ack(irqnr);
 
 	if (generic_handle_domain_irq(gic_data.domain, irqnr)) {
@@ -910,6 +950,8 @@ static void __init gic_dist_init(void)
 	u64 affinity;
 	void __iomem *base = gic_data.dist_base;
 	u32 val;
+
+	gic_v3_enable_quirks(base);
 
 	/* Disable the distributor */
 	writel_relaxed(0, base + GICD_CTLR);
@@ -1315,6 +1357,10 @@ static u16 gic_compute_target_list(int *base_cpu, const struct cpumask *mask,
 	while (cpu < nr_cpu_ids) {
 		tlist |= 1 << (mpidr & 0xf);
 
+		if (static_branch_unlikely(&gic_ipimiss_quirk))
+			/* Force only one target at a time */
+			goto out;
+
 		next_cpu = cpumask_next(cpu, mask);
 		if (next_cpu >= nr_cpu_ids)
 			goto out;
@@ -1335,8 +1381,8 @@ out:
 #define MPIDR_TO_SGI_AFFINITY(cluster_id, level) \
 	(MPIDR_AFFINITY_LEVEL(cluster_id, level) \
 		<< ICC_SGI1R_AFFINITY_## level ##_SHIFT)
-
-static void gic_send_sgi(u64 cluster_id, u16 tlist, unsigned int irq)
+static void gic_send_sgi(int dest_cpu, u64 cluster_id, u16 tlist,
+			 unsigned int irq)
 {
 	u64 val;
 
@@ -1348,7 +1394,10 @@ static void gic_send_sgi(u64 cluster_id, u16 tlist, unsigned int irq)
 	       tlist << ICC_SGI1R_TARGET_LIST_SHIFT);
 
 	pr_devel("CPU%d: ICC_SGI1R_EL1 %llx\n", smp_processor_id(), val);
-	gic_write_sgi1r(val);
+	if (static_branch_unlikely(&gic_ipimiss_quirk))
+		gic_write_sgi1r_retry(dest_cpu, irq, val);
+	else
+		gic_write_sgi1r(val);
 }
 
 static void gic_ipi_send_mask(struct irq_data *d, const struct cpumask *mask)
@@ -1369,7 +1418,7 @@ static void gic_ipi_send_mask(struct irq_data *d, const struct cpumask *mask)
 		u16 tlist;
 
 		tlist = gic_compute_target_list(&cpu, mask, cluster_id);
-		gic_send_sgi(cluster_id, tlist, d->hwirq);
+		gic_send_sgi(cpu, cluster_id, tlist, d->hwirq);
 	}
 
 	/* Force the above writes to ICC_SGI1R_EL1 to be executed */
