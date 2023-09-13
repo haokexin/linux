2af5f21476f3b732adfcfac9cd4077601ec01f9e
John Ogness <john.ogness@linutronix.de>
2023-09-13 08:35:23 +0000

    printk: nbcon: Implement processing in port->lock wrapper
    Currently the port->lock wrappers nbcon_handle_port_lock(),
    nbcon_handle_port_unlock() (and their variants) only lock/unlock
    the spin_lock.
    
    If the port is an nbcon console, the wrappers must also
    acquire/release the console and mark the region as unsafe. This
    allows general port->lock synchronization to be synchronized
    with the nbcon console ownership.
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -9,6 +9,8 @@
 #include <linux/ratelimit_types.h>
 #include <linux/once_lite.h>
 
+struct uart_port;
+
 extern const char linux_banner[];
 extern const char linux_proc_banner[];
 
@@ -192,6 +194,8 @@ void show_regs_print_info(const char *log_lvl);
 extern asmlinkage void dump_stack_lvl(const char *log_lvl) __cold;
 extern asmlinkage void dump_stack(void) __cold;
 void printk_trigger_flush(void);
+extern void nbcon_handle_port_lock(struct uart_port *up);
+extern void nbcon_handle_port_unlock(struct uart_port *up);
 #else
 static inline __printf(1, 0)
 int vprintk(const char *s, va_list args)
@@ -271,6 +275,15 @@ static inline void dump_stack(void)
 static inline void printk_trigger_flush(void)
 {
 }
+
+static inline void nbcon_handle_port_lock(struct uart_port *up)
+{
+}
+
+static inline void nbcon_handle_port_unlock(struct uart_port *up)
+{
+}
+
 #endif
 
 #ifdef CONFIG_SMP
