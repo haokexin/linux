1d3e825e614f106e16235062d83cffde667f7291
Subbaraya Sundeep <sbhatta@marvell.com>
2023-10-02 11:42:09 +0530

    octeontx2-pf: Wait till detach_resources msg is complete
    commit 1ba430986ab723e61e7cc8449370f5f91c79e5a8 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    During VF driver remove, a message is sent to detach VF
    resources to PF but VF is not waiting until message is
    complete. Also mailbox interrupts need to be turned off
    after the detach resource message is complete. This patch
    fixes that problem.
    
    Fixes: e004f14f21b2 ("octeontx2-pf: Attach RVU NIX and NPA block LFs")
    Change-Id: I765aa54ce467907d079b8eb7d000d17fdb9dedd1
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/115014
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_vf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_vf.c
@@ -805,8 +805,8 @@ static void otx2vf_remove(struct pci_dev *pdev)
 	otx2_ptp_destroy(vf);
 	otx2_mcam_flow_del(vf);
 	otx2_shutdown_tc(vf);
-	otx2vf_disable_mbox_intr(vf);
 	otx2_detach_resources(&vf->mbox);
+	otx2vf_disable_mbox_intr(vf);
 	if (vf->hw.lmt_info)
 		free_percpu(vf->hw.lmt_info);
 	if (test_bit(CN10K_LMTST, &vf->hw.cap_flag))
