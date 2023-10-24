b37a1e3a13b27dff7d62f32153f364d909edd91a
John Ogness <john.ogness@linutronix.de>
2023-10-24 14:13:14 +0000

    printk: Track registered boot consoles
    Unfortunately it is not known if a boot console and a regular
    (legacy or nbcon) console use to the same hardware. For this
    reason they must not be allowed to print simultaneously.
    
    For legacy consoles this is not an issue because they are
    already synchronized with the boot consoles using the console
    lock. However nbcon consoles can be triggered separately.
    
    Add a global flag @have_boot_console to identify if any boot
    consoles are registered. This will be used in follow-up commits
    to ensure that boot consoles and nbcon consoles cannot print
    simultaneously.
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -463,6 +463,14 @@ static int console_msg_format = MSG_FORMAT_DEFAULT;
 /* syslog_lock protects syslog_* variables and write access to clear_seq. */
 static DEFINE_MUTEX(syslog_lock);
 
+/*
+ * Specifies if a boot console is registered. If boot consoles are present,
+ * nbcon consoles cannot print simultaneously and must be synchronized by
+ * the console lock. This is because boot consoles and nbcon consoles may
+ * have mapped the same hardware.
+ */
+bool have_boot_console;
+
 #ifdef CONFIG_PRINTK
 /*
  * During panic, heavy printk by other CPUs can delay the
@@ -3506,6 +3514,9 @@ void register_console(struct console *newcon)
 	if (newcon->flags & CON_NBCON)
 		nbcon_init(newcon);
 
+	if (newcon->flags & CON_BOOT)
+		have_boot_console = true;
+
 	/*
 	 * Put this console in the list - keep the
 	 * preferred driver at the head of the list.
@@ -3558,6 +3569,8 @@ EXPORT_SYMBOL(register_console);
 /* Must be called under console_list_lock(). */
 static int unregister_console_locked(struct console *console)
 {
+	bool is_boot_con = (console->flags & CON_BOOT);
+	struct console *c;
 	int res;
 
 	lockdep_assert_console_list_lock_held();
@@ -3604,6 +3617,21 @@ static int unregister_console_locked(struct console *console)
 
 	if (console->exit)
 		res = console->exit(console);
+
+	/*
+	 * If this console was a boot console, the related global flag
+	 * might need to be updated.
+	 */
+	if (is_boot_con) {
+		bool found_boot_con = false;
+
+		for_each_console(c) {
+			if (c->flags & CON_BOOT)
+				found_boot_con = true;
+		}
+		if (!found_boot_con)
+			have_boot_console = false;
+	}
 
 	return res;
 }
