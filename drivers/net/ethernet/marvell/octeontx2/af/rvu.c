f50fb560c5d34eb98e372e14ea8cd6fffa38db83
Pavan Nikhilesh <pbhagavatula@marvell.com>
2023-11-07 19:17:01 +0530

    octeontx2-af: add TIM error af interrupt handlers
    commit a029218fa0d0fabe6f2fb6198df369102b59cce3 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    Add interrupt handlers for TIM_AF_BKT_SKIP and TIM_AF_RVU_INT
    interrupts.
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Change-Id: Ifcd46c3104354fd57ffb2e15b9df0eca4e730ae9
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/116001
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu.c
@@ -3325,6 +3325,7 @@ static void rvu_unregister_interrupts(struct rvu *rvu)
 	int irq;
 
 	rvu_sso_unregister_interrupts(rvu);
+	rvu_tim_unregister_interrupts(rvu);
 	rvu_cpt_unregister_interrupts(rvu);
 	rvu_ree_unregister_interrupts(rvu);
 
@@ -3539,6 +3540,10 @@ static int rvu_register_interrupts(struct rvu *rvu)
 	rvu->irq_allocated[offset] = true;
 
 	ret = rvu_sso_register_interrupts(rvu);
+	if (ret)
+		goto fail;
+
+	ret = rvu_tim_register_interrupts(rvu);
 	if (ret)
 		goto fail;
 
