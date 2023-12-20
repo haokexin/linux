7b073c7ec06757bd13de43b62db72ba619059700
Ciprian Costea <ciprianmarian.costea@nxp.com>
2023-12-20 17:26:38 +0200

    s32cc: include: dt-bindings: Define OCOTP header
    commit e568e3bbdc63db883c8aebfbd93872661eb3c5d3 from
    https://github.com/nxp-auto-linux/linux
    
    Issue: ALB-10852
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

---
+++ include/dt-bindings/nvmem/s32cc-ocotp-nvmem.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-3-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef __DT_BINDINGS_OCOTP_NVMEM_S32CC_H
+#define __DT_BINDINGS_OCOTP_NVMEM_S32CC_H
+
+/* NRAM offset and size for TMU Fuse */
+#define TMU_FUSE_OFFSET		(0x298)
+
+#define OCOTP_CELL_SIZE		(0x4)
+
+#endif
