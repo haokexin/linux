0ad2681eb5e537583a86a20ad6eaa69bfcefc221
Larisa Grigore <larisa.grigore@nxp.com>
2023-12-22 10:26:33 +0200

    dmaengine: fsl-edma: Add new flag for S32CC channel muxing
    commit d2023e4bfbb0ba16bbb813d1e8a3075d30560328 from
    https://github.com/nxp-auto-linux/linux
    
    S32CC has the following channel muxing mapping:
    channel0 -> muxaddr + 0x3
    channel1 -> muxaddr + 0x2
    channel2 -> muxaddr + 0x1
    channel3 -> muxaddr + 0x0
    channel4 -> muxaddr + 0x7
    channel5 -> muxaddr + 0x6
    channel6 -> muxaddr + 0x5
    channel7 -> muxaddr + 0x4
    ...
    
    A new flag FSL_EDMA_DRV_REVERSED_CHANNEL_MUX was added in that scope.
    
    Issue: ALB-10740
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- drivers/dma/fsl-edma-common.c
+++ drivers/dma/fsl-edma-common.c
@@ -2,6 +2,7 @@
 //
 // Copyright (c) 2013-2014 Freescale Semiconductor, Inc
 // Copyright (c) 2017 Sysam, Angelo Dureghello  <angelo@sysam.it>
+// Copyright 2023 NXP
 
 #include <linux/dmapool.h>
 #include <linux/module.h>
@@ -186,6 +187,11 @@ static void mux_configure32(struct fsl_edma_chan *fsl_chan, void __iomem *addr,
 	iowrite32(val, addr + off * 4);
 }
 
+static unsigned int reversed_mux_mapping(u32 channel_id)
+{
+	return 4 * (channel_id / 4) + ((4 - channel_id % 4) - 1);
+}
+
 void fsl_edma_chan_mux(struct fsl_edma_chan *fsl_chan,
 		       unsigned int slot, bool enable)
 {
@@ -199,7 +205,10 @@ void fsl_edma_chan_mux(struct fsl_edma_chan *fsl_chan,
 		return;
 
 	chans_per_mux = fsl_chan->edma->n_chans / dmamux_nr;
-	ch_off = fsl_chan->vchan.chan.chan_id % chans_per_mux;
+	if (fsl_chan->edma->drvdata->flags & FSL_EDMA_DRV_REVERSED_CHANNEL_MUX)
+		ch_off = reversed_mux_mapping(ch % chans_per_mux);
+	else
+		ch_off = fsl_chan->vchan.chan.chan_id % chans_per_mux;
 
 	if (fsl_chan->edma->drvdata->flags & FSL_EDMA_DRV_MUX_SWAP)
 		ch_off += endian_diff[ch_off % 4];
