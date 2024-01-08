c096b9b09061ca4a7199870f58ba5cc056bea561
Subbaraya Sundeep <sbhatta@marvell.com>
2024-01-08 19:32:25 +0530

    octeontx2-pf: Reschedule up message if message data is non zero
    commit 524839409c11f0ba0019259e19fbf0cacf25e2b9 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    The shared mailbox data register between PF and VF indicates
    whether a message is being processed by VF. Hence if the
    register is non-zero then PF can reschedule sending link status
    message to VF (VF zeroes the register after processing a message).
    
    Change-Id: Ica43bf9fc2d39d3310ad87f0f9505ad55b4a1ff8
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/121062
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/121192
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -3202,6 +3202,7 @@ static void otx2_vf_link_event_task(struct work_struct *work)
 	struct otx2_vf_config *config;
 	struct cgx_link_info_msg *req;
 	struct mbox_msghdr *msghdr;
+	struct delayed_work *dwork;
 	struct otx2_nic *pf;
 	int vf_idx;
 
@@ -3214,6 +3215,14 @@ static void otx2_vf_link_event_task(struct work_struct *work)
 		return;
 
 	mutex_lock(&pf->mbox.lock);
+
+	dwork = &config->link_event_work;
+
+	if (!otx2_mbox_wait_for_zero(&pf->mbox_pfvf[0].mbox_up, vf_idx)) {
+		schedule_delayed_work(dwork, msecs_to_jiffies(100));
+		mutex_unlock(&pf->mbox.lock);
+		return;
+	}
 
 	msghdr = otx2_mbox_alloc_msg_rsp(&pf->mbox_pfvf[0].mbox_up, vf_idx,
 					 sizeof(*req), sizeof(struct msg_rsp));
