71b0faca6fa5558d5a208b92dfa0378bd363c7c7
Larisa Grigore <larisa.grigore@nxp.com>
2024-01-29 17:39:21 +0200

    dt-bindings: thermal: qoriq: Add possible thermal-sensors IDs
    commit 6902f587a4b9b86f5669fcaea5de84e03bc619d0 from
    https://github.com/nxp-auto-linux/linux
    
    Issue: ALB-10839
    Issue: ALB-10862
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

---
+++ include/dt-bindings/thermal/qoriq_thermal.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause) */
+/*
+ * This header provides constants for qoriq thermal bindings.
+ *
+ * Copyright 2024 NXP
+ */
+
+#ifndef _DT_BINDINGS_THERMAL_QORIQ_H
+#define _DT_BINDINGS_THERMAL_QORIQ_H
+
+#define QORIQ_SENSOR_0_IMMEDIATE_TEMP	0
+#define QORIQ_SENSOR_1_IMMEDIATE_TEMP	1
+#define QORIQ_SENSOR_2_IMMEDIATE_TEMP	2
+#define QORIQ_SENSOR_0_AVERAGE_TEMP	16
+#define QORIQ_SENSOR_1_AVERAGE_TEMP	17
+#define QORIQ_SENSOR_2_AVERAGE_TEMP	18
+
+#endif
+
