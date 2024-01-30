ffaf9c39e1a2a849fa99e2f2eeadf524b97d18e7
Larisa Grigore <larisa.grigore@nxp.com>
2024-01-30 09:44:19 +0200

    dt-bindings: thermal: qoriq: Add TMR.ALPF possible values
    commit 3ad13ceb2ec8181b8ff2d056ea16a8e3a8c448fc from
    https://github.com/nxp-auto-linux/linux
    
    Issue: ALB-10839
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>
    Signed-off-by: Zhantao Tang <zhantao.tang@windriver.com>

--- include/dt-bindings/thermal/qoriq_thermal.h
+++ include/dt-bindings/thermal/qoriq_thermal.h
@@ -15,5 +15,13 @@
 #define QORIQ_SENSOR_1_AVERAGE_TEMP	17
 #define QORIQ_SENSOR_2_AVERAGE_TEMP	18
 
+/* The average temperature is calculated using this formula:
+ * ALPF x Current_Temp + (1 - ALPF) x Average_Temp.
+ */
+#define QORIQ_TMR_ALPF_1_0	0 /* ALPF = 1.0 */
+#define QORIQ_TMR_ALPF_0_5	1 /* ALPF = 0.5 */
+#define QORIQ_TMR_ALPF_0_25	2 /* ALPF = 0.25 */
+#define QORIQ_TMR_ALPF_0_125	3 /* ALPF = 0.125 */
+
 #endif
 
