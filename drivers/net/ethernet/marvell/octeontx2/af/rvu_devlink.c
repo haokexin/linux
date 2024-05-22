27c02c4646a7c1d53ac4e73f5e7728cd04170a49
Naveen Mamindlapalli <naveenm@marvell.com>
2024-05-22 18:16:21 +0530

    octeontx2-af: dbg: Add debug prints for NIX AF interrupts
    commit daa6d1474f679574a4a453f34d1e8bcde59887e0 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch adds debug prints for NIX AF interrupts.
    
    Signed-off-by: Naveen Mamindlapalli <naveenm@marvell.com>
    Change-Id: If2fdc0a98b2c79c836d003641199ecad3e2a3204
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/130169
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Tested-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Bo Sun <Bo.Sun.CN@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_devlink.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_devlink.c
@@ -78,6 +78,7 @@ static irqreturn_t rvu_nix_af_rvu_intr_handler(int irq, void *rvu_irq)
 
 	nix_event_context = rvu_dl->rvu_nix_health_reporter->nix_event_ctx;
 	intr = rvu_read64(rvu, blkaddr, NIX_AF_RVU_INT);
+	dev_err_ratelimited(rvu->dev, "NIX_AF_RVU_INT = 0x%llx\n", intr);
 	nix_event_context->nix_af_rvu_int = intr;
 
 	/* Clear interrupts */
@@ -113,6 +114,10 @@ static irqreturn_t rvu_nix_af_rvu_gen_handler(int irq, void *rvu_irq)
 
 	nix_event_context = rvu_dl->rvu_nix_health_reporter->nix_event_ctx;
 	intr = rvu_read64(rvu, blkaddr, NIX_AF_GEN_INT);
+	if ((intr & 0x1f) == 0x10)
+		dev_info_ratelimited(rvu->dev, "NIX_AF_GEN_INT = 0x%llx\n", intr);
+	else
+		dev_err_ratelimited(rvu->dev, "NIX_AF_GEN_INT = 0x%llx\n", intr);
 	nix_event_context->nix_af_rvu_gen = intr;
 
 	/* Clear interrupts */
@@ -148,6 +153,7 @@ static irqreturn_t rvu_nix_af_rvu_err_handler(int irq, void *rvu_irq)
 
 	nix_event_context = rvu_dl->rvu_nix_health_reporter->nix_event_ctx;
 	intr = rvu_read64(rvu, blkaddr, NIX_AF_ERR_INT);
+	dev_err_ratelimited(rvu->dev, "NIX_AF_ERR_INT = 0x%llx\n", intr);
 	nix_event_context->nix_af_rvu_err = intr;
 
 	/* Clear interrupts */
@@ -182,7 +188,8 @@ static irqreturn_t rvu_nix_af_rvu_ras_handler(int irq, void *rvu_irq)
 		return IRQ_NONE;
 
 	nix_event_context = rvu_dl->rvu_nix_health_reporter->nix_event_ctx;
-	intr = rvu_read64(rvu, blkaddr, NIX_AF_ERR_INT);
+	intr = rvu_read64(rvu, blkaddr, NIX_AF_RAS);
+	dev_err_ratelimited(rvu->dev, "NIX_AF_RAS = 0x%llx\n", intr);
 	nix_event_context->nix_af_rvu_ras = intr;
 
 	/* Clear interrupts */
