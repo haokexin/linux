639ad8d38d66457db8228e82f4f7e39eb74c6bfb
Sai Krishna <saikrishnag@marvell.com>
2024-07-31 16:26:45 +0530

    octeontx2-vf: CN20K mbox REQ/ACK implementation for AF's VF
    commit 0f572eeebdf05cbf2feb9215d3a6a8107cb6fae9 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch implements the CN20k MBOX communication between PF0 and
    AF's VFs. This implementation uses separate trigger interrupts
    for request, response messages against using trigger message data in CN10K.
    
    Signed-off-by: Sai Krishna <saikrishnag@marvell.com>
    Change-Id: If73b5cf47bae852a22371b78a89f14256ea264d2
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/132761
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Bo Sun <Bo.Sun.CN@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cn20k/api.h
+++ drivers/net/ethernet/marvell/octeontx2/af/cn20k/api.h
@@ -11,9 +11,9 @@
 #include "../rvu.h"
 
 struct ng_rvu {
+	struct qmem		*vf_mbox_addr;
 	struct qmem		*pf_mbox_addr;
 	struct mbox_ops         *rvu_mbox_ops;
-	int			pf_mbox_porder;
 };
 
 struct rvu;
@@ -21,6 +21,7 @@ struct rvu;
 /* Mbox related APIs */
 int cn20k_rvu_mbox_init(struct rvu *rvu, int type, int num);
 int cn20k_register_afpf_mbox_intr(struct rvu *rvu);
+int cn20k_register_afvf_mbox_intr(struct rvu *rvu, int pf_vec_start);
 int cn20k_rvu_get_mbox_regions(struct rvu *rvu, void **mbox_addr,
 			       int num, int type, unsigned long *pf_bmap);
 void cn20k_rvu_enable_mbox_intr(struct rvu *rvu);
@@ -28,5 +29,7 @@ void cn20k_rvu_unregister_interrupts(struct rvu *rvu);
 void cn20k_free_mbox_memory(struct rvu *rvu);
 int cn20k_mbox_setup(struct otx2_mbox *mbox, struct pci_dev *pdev,
 		     void *reg_base, int direction, int ndevs);
+void cn20k_rvu_enable_afvf_intr(struct rvu *rvu, int vfs);
+void cn20k_rvu_disable_afvf_intr(struct rvu *rvu, int vfs);
 
 #endif /* CN20K_API_H */
