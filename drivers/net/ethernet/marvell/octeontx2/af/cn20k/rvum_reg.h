e1e64d4db0e8c29e4cecb259cb790d28004e9c08
Geetha sowjanya <gakula@marvell.com>
2024-08-01 13:06:30 +0530

    octeontx2-af: cn20k: Add block discovery register
    commit 4663c7241c009b01f24ca1c42edb7db82ff641a6 from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    On cn20K the "RVU_PF_BLOCK_ADDR(0..31)_DISC" registers
    are replaced with single "RVU_PF_DISC" register with
    one bit per block.
    In this patch uses new register to check whether blocks
    are implemented and also set 0th bit in RVU_PRIV_PFX/HWFVX_DISC
    register to notify the AF driver state PF/VF.
    
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Change-Id: I6889bc30d19ee99e48f3c0339480da7978b0b482
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/132757
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: Bo Sun <Bo.Sun.CN@windriver.com>

---
+++ drivers/net/ethernet/marvell/octeontx2/af/cn20k/rvum_reg.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Marvell RVU Admin Function driver
+ *
+ * Copyright (C) 2024 Marvell.
+ *
+ */
+
+#ifndef RVUM_REG_H
+#define RVUM_REG_H
+
+/* CN20K specific registers */
+#define RVU_PF_DISC			(0x100)
+#define RVU_PRIV_PFX_DISC(a)		(0x8000208 | (a) << 16)
+#define RVU_PRIV_HWVFX_DISC(a)		(0xD000000 | (a) << 12)
+#define RVU_CN20K_PRIV_HWVFX_INT_CFG(a)		(0xC000000 | (a) << 12)
+
+#endif /* RVU_REG_H */
